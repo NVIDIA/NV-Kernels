e4e14e058e9d6a4ffada965ebd410ee341b74282
James Morse <james.morse@arm.com>
2018-06-14 13:52:30 +0100

    x86/resctrl: Add helper for setting cpu default properties
    rdtgroup_rmdir_ctrl() and rdtgroup_rmdir_mon() set the per-cpu
    pqr_state for CPUs that were part of the rmdir'd group.
    
    Another architecture might not have a 'pqr_state', its hardware may
    need the values in a different format. MPAM's equivalent of rmid values
    are not unique, and always need the closid providing too.
    
    As only rdtgroup_rmdir_mon() modifies just the rmid, provide a helper
    that sets both values. This allows another architecture to convert
    the values into the format the hardware expects.
    As these values are read by __resctrl_sched_in(), but may be written by
    a different CPU without any locking, add READ/WRTE_ONCE() to avoid torn
    values.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Tushar Dave <tdave@nvidia.com>
    (cherry picked from commit 8dc8d83e19e81c65cd228f2a2391e5a30d510966 https://github.com/NVIDIA-BaseOS-6/linux-nvidia-6.5/tree/MPAM-dev)
    Signed-off-by: KobaK <kobak@nvidia.com>

--- arch/x86/kernel/cpu/resctrl/rdtgroup.c
+++ arch/x86/kernel/cpu/resctrl/rdtgroup.c
@@ -3662,7 +3662,9 @@ static int rdtgroup_rmdir_mon(struct rdtgroup *rdtgrp, cpumask_var_t tmpmask)
 
 	/* Update per cpu rmid of the moved CPUs first */
 	for_each_cpu(cpu, &rdtgrp->cpu_mask)
-		per_cpu(pqr_state.default_rmid, cpu) = prdtgrp->mon.rmid;
+		resctrl_arch_set_cpu_default_closid_rmid(cpu, rdtgrp->closid,
+							 prdtgrp->mon.rmid);
+
 	/*
 	 * Update the MSR on moved CPUs and CPUs which have moved
 	 * task running on them.
@@ -3695,6 +3697,7 @@ static int rdtgroup_ctrl_remove(struct rdtgroup *rdtgrp)
 
 static int rdtgroup_rmdir_ctrl(struct rdtgroup *rdtgrp, cpumask_var_t tmpmask)
 {
+	u32 closid, rmid;
 	int cpu;
 
 	/* Give any tasks back to the default group */
@@ -3705,10 +3708,10 @@ static int rdtgroup_rmdir_ctrl(struct rdtgroup *rdtgrp, cpumask_var_t tmpmask)
 		   &rdtgroup_default.cpu_mask, &rdtgrp->cpu_mask);
 
 	/* Update per cpu closid and rmid of the moved CPUs first */
-	for_each_cpu(cpu, &rdtgrp->cpu_mask) {
-		per_cpu(pqr_state.default_closid, cpu) = rdtgroup_default.closid;
-		per_cpu(pqr_state.default_rmid, cpu) = rdtgroup_default.mon.rmid;
-	}
+	closid = rdtgroup_default.closid;
+	rmid = rdtgroup_default.mon.rmid;
+	for_each_cpu(cpu, &rdtgrp->cpu_mask)
+		resctrl_arch_set_cpu_default_closid_rmid(cpu, closid, rmid);
 
 	/*
 	 * Update the MSR on moved CPUs and CPUs which have moved
