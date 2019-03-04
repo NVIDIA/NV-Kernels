72a5f8345a29f38fe6289fbd717023a53036c023
James Morse <james.morse@arm.com>
2019-03-04 15:15:25 +0000

    arm_mpam: resctrl: Implement resctrl_arch_reset_all_ctrls()
    We already have a helper for resetting an mpam class. Hook it up to
    resctrl_arch_reset_all_ctrls().
    
    Signed-off-by: James Morse <james.morse@arm.com>
    (cherry picked from commit 67d6d56cd1a09785e192ca99979f17391cf419c6 https://web.git.kernel.org/pub/scm/linux/kernel/git/morse/linux.git/log/?h=mpam/snapshot/v6.14-rc1)
    Signed-off-by: Koba Ko <kobak@nvidia.com>

--- drivers/platform/arm64/mpam/mpam_resctrl.c
+++ drivers/platform/arm64/mpam/mpam_resctrl.c
@@ -198,6 +198,19 @@ int mpam_resctrl_setup(void)
 	return err;
 }
 
+void resctrl_arch_reset_all_ctrls(struct rdt_resource *r)
+{
+	struct mpam_resctrl_res *res;
+
+	lockdep_assert_cpus_held();
+
+	if (!mpam_is_enabled())
+		return;
+
+	res = container_of(r, struct mpam_resctrl_res, resctrl_res);
+	mpam_reset_class_locked(res->class);
+}
+
 static void mpam_resctrl_domain_hdr_init(int cpu, struct mpam_class *class,
 					 struct mpam_component *comp,
 					 struct rdt_domain_hdr *hdr)
