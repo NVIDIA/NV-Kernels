e91b76e9b3ff4052ce82d8bfd085c26d3562a63b
Casey Schaufler <casey@schaufler-ca.com>
2020-08-17 16:02:56 -0700

    UBUNTU: SAUCE: LSM: Create and manage the lsmblob data structure.
    When more than one security module is exporting data to
    audit and networking sub-systems a single 32 bit integer
    is no longer sufficient to represent the data. Add a
    structure to be used instead.
    
    The lsmblob structure is currently an array of
    u32 "secids". There is an entry for each of the
    security modules built into the system that would
    use secids if active. The system assigns the module
    a "slot" when it registers hooks. If modules are
    compiled in but not registered there will be unused
    slots.
    
    A new lsm_id structure, which contains the name
    of the LSM and its slot number, is created. There
    is an instance for each LSM, which assigns the name
    and passes it to the infrastructure to set the slot.
    
    The audit rules data is expanded to use an array of
    security module data rather than a single instance.
    Because IMA uses the audit rule functions it is
    affected as well.
    
    Acked-by: Stephen Smalley <sds@tycho.nsa.gov>
    Acked-by: Paul Moore <paul@paul-moore.com>
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Acked-by: Andrea Righi <andrea.righi@canonical.com>
    [ saf: resolve conflicts ]
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>
    [ update to support landlock ]
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- security/integrity/ima/ima_policy.c
+++ security/integrity/ima/ima_policy.c
@@ -114,7 +114,7 @@ struct ima_rule_entry {
 	int pcr;
 	unsigned int allowed_algos; /* bitfield of allowed hash algorithms */
 	struct {
-		void *rule;	/* LSM file metadata specific */
+		void *rules[LSMBLOB_ENTRIES]; /* LSM file metadata specific */
 		char *args_p;	/* audit value */
 		int type;	/* audit type */
 	} lsm[MAX_LSM_RULES];
@@ -123,6 +123,22 @@ struct ima_rule_entry {
 	struct ima_rule_opt_list *label; /* Measure data grouped under this label */
 	struct ima_template_desc *template;
 };
+
+/**
+ * ima_lsm_isset - Is a rule set for any of the active security modules
+ * @rules: The set of IMA rules to check.
+ *
+ * If a rule is set for any LSM return true, otherwise return false.
+ */
+static inline bool ima_lsm_isset(void *rules[])
+{
+	int i;
+
+	for (i = 0; i < LSMBLOB_ENTRIES; i++)
+		if (rules[i])
+			return true;
+	return false;
+}
 
 /*
  * sanity check in case the kernels gains more hash algorithms that can
@@ -378,9 +394,11 @@ static void ima_free_rule_opt_list(struct ima_rule_opt_list *opt_list)
 static void ima_lsm_free_rule(struct ima_rule_entry *entry)
 {
 	int i;
+	int r;
 
 	for (i = 0; i < MAX_LSM_RULES; i++) {
-		ima_filter_rule_free(entry->lsm[i].rule);
+		for (r = 0; r < LSMBLOB_ENTRIES; r++)
+			ima_filter_rule_free(entry->lsm[i].rules);
 		kfree(entry->lsm[i].args_p);
 	}
 }
@@ -425,8 +443,8 @@ static struct ima_rule_entry *ima_lsm_copy_rule(struct ima_rule_entry *entry)
 
 		ima_filter_rule_init(nentry->lsm[i].type, Audit_equal,
 				     nentry->lsm[i].args_p,
-				     &nentry->lsm[i].rule);
-		if (!nentry->lsm[i].rule)
+				     &nentry->lsm[i].rules[0]);
+		if (!ima_lsm_isset(nentry->lsm[i].rules))
 			pr_warn("rule for LSM \'%s\' is undefined\n",
 				nentry->lsm[i].args_p);
 	}
@@ -451,7 +469,7 @@ static int ima_lsm_update_rule(struct ima_rule_entry *entry)
 	 * be owned by nentry.
 	 */
 	for (i = 0; i < MAX_LSM_RULES; i++)
-		ima_filter_rule_free(entry->lsm[i].rule);
+		ima_filter_rule_free(entry->lsm[i].rules);
 	kfree(entry);
 
 	return 0;
@@ -635,7 +653,7 @@ static bool ima_match_rules(struct ima_rule_entry *rule,
 		int rc = 0;
 		u32 osid;
 
-		if (!lsm_rule->lsm[i].rule) {
+		if (!ima_lsm_isset(lsm_rule->lsm[i].rules)) {
 			if (!lsm_rule->lsm[i].args_p)
 				continue;
 			else
@@ -650,14 +668,14 @@ retry:
 			security_inode_getsecid(inode, &osid);
 			rc = ima_filter_rule_match(osid, lsm_rule->lsm[i].type,
 						   Audit_equal,
-						   lsm_rule->lsm[i].rule);
+						   lsm_rule->lsm[i].rules);
 			break;
 		case LSM_SUBJ_USER:
 		case LSM_SUBJ_ROLE:
 		case LSM_SUBJ_TYPE:
 			rc = ima_filter_rule_match(secid, lsm_rule->lsm[i].type,
 						   Audit_equal,
-						   lsm_rule->lsm[i].rule);
+						   lsm_rule->lsm[i].rules);
 			break;
 		default:
 			break;
@@ -680,7 +698,7 @@ retry:
 out:
 	if (rule_reinitialized) {
 		for (i = 0; i < MAX_LSM_RULES; i++)
-			ima_filter_rule_free(lsm_rule->lsm[i].rule);
+			ima_filter_rule_free(lsm_rule->lsm[i].rules);
 		kfree(lsm_rule);
 	}
 	return result;
@@ -1128,7 +1146,7 @@ static int ima_lsm_rule_init(struct ima_rule_entry *entry,
 {
 	int result;
 
-	if (entry->lsm[lsm_rule].rule)
+	if (ima_lsm_isset(entry->lsm[lsm_rule].rules))
 		return -EINVAL;
 
 	entry->lsm[lsm_rule].args_p = match_strdup(args);
@@ -1138,8 +1156,8 @@ static int ima_lsm_rule_init(struct ima_rule_entry *entry,
 	entry->lsm[lsm_rule].type = audit_type;
 	result = ima_filter_rule_init(entry->lsm[lsm_rule].type, Audit_equal,
 				      entry->lsm[lsm_rule].args_p,
-				      &entry->lsm[lsm_rule].rule);
-	if (!entry->lsm[lsm_rule].rule) {
+				      &entry->lsm[lsm_rule].rules[0]);
+	if (!ima_lsm_isset(entry->lsm[lsm_rule].rules)) {
 		pr_warn("rule for LSM \'%s\' is undefined\n",
 			entry->lsm[lsm_rule].args_p);
 
@@ -2075,7 +2093,7 @@ int ima_policy_show(struct seq_file *m, void *v)
 
 	/* Do not print rules with inactive LSM labels */
 	for (i = 0; i < MAX_LSM_RULES; i++) {
-		if (entry->lsm[i].args_p && !entry->lsm[i].rule) {
+		if (entry->lsm[i].args_p && !entry->lsm[i].rules) {
 			rcu_read_unlock();
 			return 0;
 		}
@@ -2223,7 +2241,7 @@ int ima_policy_show(struct seq_file *m, void *v)
 	}
 
 	for (i = 0; i < MAX_LSM_RULES; i++) {
-		if (entry->lsm[i].rule) {
+		if (ima_lsm_isset(entry->lsm[i].rules)) {
 			switch (i) {
 			case LSM_OBJ_USER:
 				seq_printf(m, pt(Opt_obj_user),
