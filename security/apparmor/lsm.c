648d77951e97fb1fbc3053dea48160251737299b
Casey Schaufler <casey@schaufler-ca.com>
2020-08-20 10:40:08 -0700

    UBUNTU: SAUCE: LSM: Specify which LSM to display
    Create a new entry "display" in the procfs attr directory for
    controlling which LSM security information is displayed for a
    process. A process can only read or write its own display value.
    
    The name of an active LSM that supplies hooks for
    human readable data may be written to "display" to set the
    value. The name of the LSM currently in use can be read from
    "display". At this point there can only be one LSM capable
    of display active. A helper function lsm_task_display() is
    provided to get the display slot for a task_struct.
    
    Setting the "display" requires that all security modules using
    setprocattr hooks allow the action. Each security module is
    responsible for defining its policy.
    
    AppArmor hook provided by John Johansen <john.johansen@canonical.com>
    SELinux hook provided by Stephen Smalley <sds@tycho.nsa.gov>
    
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Acked-by: Stephen Smalley <sds@tycho.nsa.gov>
    Acked-by: Paul Moore <paul@paul-moore.com>
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Acked-by: Andrea Righi <andrea.righi@canonical.com>
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- security/apparmor/lsm.c
+++ security/apparmor/lsm.c
@@ -659,6 +659,27 @@ static int apparmor_getprocattr(struct task_struct *task, const char *name,
 	return error;
 }
 
+
+static int profile_display_lsm(struct aa_profile *profile,
+			       struct common_audit_data *sa)
+{
+	struct aa_ruleset *rules = list_first_entry(&profile->rules,
+						    typeof(*rules), list);
+	struct aa_perms perms = { };
+	unsigned int state;
+
+	state = RULE_MEDIATES(rules, AA_CLASS_DISPLAY_LSM);
+	if (state) {
+		perms = *aa_lookup_perms(&rules->policy, state);
+		aa_apply_modes_to_perms(profile, &perms);
+		aad(sa)->label = &profile->label;
+
+		return aa_check_perms(profile, &perms, AA_MAY_WRITE, sa, NULL);
+	}
+
+	return 0;
+}
+
 static int apparmor_setprocattr(const char *name, void *value,
 				size_t size)
 {
@@ -670,6 +691,19 @@ static int apparmor_setprocattr(const char *name, void *value,
 
 	if (size == 0)
 		return -EINVAL;
+
+	/* LSM infrastructure does actual setting of display if allowed */
+	if (!strcmp(name, "display")) {
+		struct aa_profile *profile;
+		struct aa_label *label;
+
+		aad(&sa)->info = "set display lsm";
+		label = begin_current_label_crit_section();
+		error = fn_for_each_confined(label, profile,
+					     profile_display_lsm(profile, &sa));
+		end_current_label_crit_section(label);
+		return error;
+	}
 
 	/* AppArmor requires that the buffer must be null terminated atm */
 	if (args[size - 1] != '\0') {
