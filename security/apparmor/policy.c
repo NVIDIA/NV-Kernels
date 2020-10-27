4d23d63a4bee99fc887e452a2fbe215222a63717
Paolo Pisati <paolo.pisati@canonical.com>
2020-10-27 09:45:23 +0000

    UBUNTU: SAUCE: apparmor: rename kzfree() to kfree_sensitive()
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- security/apparmor/policy.c
+++ security/apparmor/policy.c
@@ -257,7 +257,7 @@ void aa_free_profile(struct aa_profile *profile)
 	kfree_sensitive(profile->rename);
 
 	free_attachment(&profile->attach);
-	kzfree(profile->net_compat);
+	kfree_sensitive(profile->net_compat);
 
 	/*
 	 * at this point there are no tasks that can have a reference
