fcbacf2fc5da79ef163253b0428bd924d5ac6cb8
James Morse <james.morse@arm.com>
2021-02-10 18:11:20 +0000

    NVIDIA: SAUCE: arm_mpam: Allow configuration to be applied and restored during cpu online
    When CPUs come online the original configuration should be restored.
    Once the maximum partid is known, allocate an configuration array for
    each component, and reprogram each RIS configuration from this.
    
    The MPAM spec describes how multiple controls can interact. To prevent
    this happening by accident, always reset controls that don't have a
    valid configuration. This allows the same helper to be used for
    configuration and reset.
    
    CC: Dave Martin <Dave.Martin@arm.com>
    Signed-off-by: James Morse <james.morse@arm.com>
    (cherry picked from commit 860ff949b5d61359a16c7c1cc0488f26887a1ceb https://git.kernel.org/pub/scm/linux/kernel/git/morse/linux.git)
    Signed-off-by: Fenghua Yu <fenghuay@nvidia.com>

--- drivers/platform/arm64/mpam/mpam_internal.h
+++ drivers/platform/arm64/mpam/mpam_internal.h
@@ -190,11 +190,7 @@ struct mpam_props {
 	u16			num_mbwu_mon;
 };
 
-static inline bool mpam_has_feature(enum mpam_device_features feat,
-				    struct mpam_props *props)
-{
-	return (1 << feat) & props->features;
-}
+#define mpam_has_feature(_feat, x)	((1 << (_feat)) & (x)->features)
 
 static inline void mpam_set_feature(enum mpam_device_features feat,
 				    struct mpam_props *props)
@@ -225,6 +221,17 @@ struct mpam_class {
 	struct mpam_garbage	garbage;
 };
 
+struct mpam_config {
+	/* Which configuration values are valid. 0 is used for reset */
+	mpam_features_t		features;
+
+	u32	cpbm;
+	u32	mbw_pbm;
+	u16	mbw_max;
+
+	struct mpam_garbage	garbage;
+};
+
 struct mpam_component {
 	u32			comp_id;
 
@@ -232,6 +239,12 @@ struct mpam_component {
 	struct list_head	vmsc;
 
 	cpumask_t		affinity;
+
+	/*
+	 * Array of configuration values, indexed by partid.
+	 * Read from cpuhp callbacks, hold the cpuhp lock when writing.
+	 */
+	struct mpam_config	*cfg;
 
 	/* member of mpam_class:components */
 	struct list_head	class_list;
@@ -296,6 +309,9 @@ extern u8 mpam_pmg_max;
 /* Scheduled work callback to enable mpam once all MSC have been probed */
 void mpam_enable(struct work_struct *work);
 void mpam_disable(struct work_struct *work);
+
+int mpam_apply_config(struct mpam_component *comp, u16 partid,
+		      struct mpam_config *cfg);
 
 int mpam_get_cpumask_from_cache_id(unsigned long cache_id, u32 cache_level,
 				   cpumask_t *affinity);
