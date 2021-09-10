b40226ea3893be00ec196551017fec740058e48b
James Morse <james.morse@arm.com>
2021-09-10 12:00:01 +0100

    arm_mpam: Add helper to reset saved mbwu state
    resctrl expects to reset the bandwidth counters when the filesystem
    is mounted.
    
    To allow this, add a helper that clears the saved mbwu state. Instead
    of cross calling to each CPU that can access the component MSC to
    write to the counter, set a flag that causes it to be zero'd on the
    the next read. This is easily done by forcing a configuration update.
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- drivers/platform/arm64/mpam/mpam_internal.h
+++ drivers/platform/arm64/mpam/mpam_internal.h
@@ -295,10 +295,12 @@ struct mon_cfg {
 
 /*
  * Changes to enabled and cfg are protected by the msc->lock.
- * Changes to prev_val and correction are protected by the msc's mon_sel_lock.
+ * Changes to reset_on_next_read, prev_val and correction are protected by the
+ * msc's mon_sel_lock.
  */
 struct msmon_mbwu_state {
 	bool		enabled;
+	bool		reset_on_next_read;
 	struct mon_cfg	cfg;
 
 	/* The value last read from the hardware. Used to detect overflow. */
@@ -408,6 +410,7 @@ int mpam_apply_config(struct mpam_component *comp, u16 partid,
 
 int mpam_msmon_read(struct mpam_component *comp, struct mon_cfg *ctx,
 		    enum mpam_device_features, u64 *val);
+void mpam_msmon_reset_mbwu(struct mpam_component *comp, struct mon_cfg *ctx);
 
 int mpam_get_cpumask_from_cache_id(unsigned long cache_id, u32 cache_level,
 				   cpumask_t *affinity);
