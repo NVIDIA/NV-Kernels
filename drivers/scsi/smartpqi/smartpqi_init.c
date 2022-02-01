1541d6d4df41739f3a90df63b4ad36793f6e9332
Don Brace <don.brace@microchip.com>
2022-02-01 15:47:58 -0600

    scsi: smartpqi: Add PCI IDs
    BugLink: https://bugs.launchpad.net/bugs/1998643
    
    Add in new ZTE controllers:
                                        VID  / DID  / SVID / SDID
                                        ----   ----   ----   ----
    ZTE SmartROC3100 RS241-18i 2G       9005 / 028F / 1CF2 / 5449
    ZTE SmartROC3100 RS242-18i 4G       9005 / 028F / 1CF2 / 544A
    ZTE SmartIOC2100 RS243-18i          9005 / 028F / 1CF2 / 544B
    ZTE SmartROC3100 RM241B-18i 2G      9005 / 028F / 1CF2 / 544D
    ZTE SmartROC3100 RM242B-18i 4G      9005 / 028F / 1CF2 / 544E
    ZTE SmartIOC2100 RM243B-18i         9005 / 028F / 1CF2 / 544F
    
    Add PCI ID for 1100-24i controller:
                                        VID  / DID  / SVID / SDID
                                        ----   ----   ----   ----
    HBA 1100-24i                        9005 / 028F / 9005 / 1304
    
    Add PCI IDs for HPE and Adaptec devices:
                                        VID  / DID  / SVID / SDID
                                        ----   ----   ----   ----
    Adaptec Smart HBA 2200-8io /e       9005 / 028F / 9005 / 1463
    Adaptec Smart HBA 2200-16io /e      9005 / 028F / 9005 / 14C2
    HPE SR308i-p Gen11                  9005 / 028F / 1590 / 0382
    HPE SR308i-o Gen11                  9005 / 028F / 1590 / 0383
    HPE SR932i-p Gen11                  9005 / 028F / 1590 / 0381
    
    Add PCI IDs for Inspur controllers:
                                        VID  / DID  / SVID / SDID
                                        ----   ----   ----   ----
    INSPUR RS0800M5H24i                 9005 / 028F / 1BD4 / 006B
    INSPUR RS0800M5E8I                  9005 / 028F / 1BD4 / 006C
    INSPUR RS0800M5H8I                  9005 / 028F / 1BD4 / 006D
    INSPUR RS0804M5R16i                 9005 / 028F / 1BD4 / 006F
    INSPUR RS0800M5E16i                 9005 / 028F / 1BD4 / 0070
    INSPUR RS0800M5H16i                 9005 / 028F / 1BD4 / 0071
    INSPUR RS0800M5E16i                 9005 / 028F / 1BD4 / 0072
    NT RAID 3100-24i                    9005 / 028F / 1F0C / 3161
    
    Add HPE and Adaptec OROC PCI IDs:
                                        VID  / DID  / SVID / SDID
                                        ----   ----   ----   ----
    HPE SR216i-o Gen11                  9005 / 028F / 9005 / 036F
    Adaptec SmartRAID 3284-16io /e/uC   9005 / 028F / 9005 / 1473
    Adaptec SmartRAID 3254-16io /e      9005 / 028F / 9005 / 1474
    
    Add PCI IDs for new channel controllers:
                                        VID  / DID  / SVID / SDID
                                        ----   ----   ----   ----
    Adaptec SmartRAID 3254-8i /e        9005 / 028F / 9005 / 14A4
    Adaptec SmartRAID 3252-8i /e        9005 / 028F / 9005 / 14A5
    Adaptec SmartRAID 3204-8i /e        9005 / 028F / 9005 / 14A6
    
    Align PCI IDs with OOB driver. Easier to check for differences.
    
    Link: https://lore.kernel.org/r/164375207802.440833.15947108153078495425.stgit@brunhilda.pdev.net
    Reviewed-by: Scott Benesh <scott.benesh@microchip.com>
    Reviewed-by: Scott Teel <scott.teel@microchip.com>
    Co-developed-by: Mike McGowen <Mike.McGowen@microchip.com>
    Signed-off-by: Mike McGowen <Mike.McGowen@microchip.com>
    Co-developed-by: Murthy Bhat <Murthy.Bhat@microchip.com>
    Signed-off-by: Murthy Bhat <Murthy.Bhat@microchip.com>
    Co-developed-by: Sagar Biradar <sagar.biradar@microchip.com>
    Signed-off-by: Sagar Biradar <sagar.biradar@microchip.com>
    Signed-off-by: Don Brace <don.brace@microchip.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    
    (cherry picked from commit c57ee4ccb3584fea91e62cc4a7e60600130e75a8)
    Signed-off-by: Michael Reed <Michael.Reed@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- drivers/scsi/smartpqi/smartpqi_init.c
+++ drivers/scsi/smartpqi/smartpqi_init.c
@@ -8950,10 +8950,6 @@ static const struct pci_device_id pqi_pci_id_table[] = {
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
-			       0x193d, 0x8460)
-	},
-	{
-		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
 			       0x193d, 0x1104)
 	},
 	{
@@ -9047,6 +9043,34 @@ static const struct pci_device_id pqi_pci_id_table[] = {
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
 			       0x1bd4, 0x0054)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1bd4, 0x006b)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1bd4, 0x006c)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1bd4, 0x006d)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1bd4, 0x006f)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1bd4, 0x0070)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1bd4, 0x0071)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1bd4, 0x0072)
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
@@ -9206,6 +9230,10 @@ static const struct pci_device_id pqi_pci_id_table[] = {
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       PCI_VENDOR_ID_ADAPTEC2, 0x1304)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
 			       PCI_VENDOR_ID_ADAPTEC2, 0x1380)
 	},
 	{
@@ -9266,6 +9294,10 @@ static const struct pci_device_id pqi_pci_id_table[] = {
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       PCI_VENDOR_ID_ADAPTEC2, 0x1463)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
 			       PCI_VENDOR_ID_ADAPTEC2, 0x1470)
 	},
 	{
@@ -9275,6 +9307,14 @@ static const struct pci_device_id pqi_pci_id_table[] = {
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
 			       PCI_VENDOR_ID_ADAPTEC2, 0x1472)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       PCI_VENDOR_ID_ADAPTEC2, 0x1473)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       PCI_VENDOR_ID_ADAPTEC2, 0x1474)
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
@@ -9302,6 +9342,18 @@ static const struct pci_device_id pqi_pci_id_table[] = {
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       PCI_VENDOR_ID_ADAPTEC2, 0x14a4)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       PCI_VENDOR_ID_ADAPTEC2, 0x14a5)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       PCI_VENDOR_ID_ADAPTEC2, 0x14a6)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
 			       PCI_VENDOR_ID_ADAPTEC2, 0x14b0)
 	},
 	{
@@ -9315,6 +9367,10 @@ static const struct pci_device_id pqi_pci_id_table[] = {
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
 			       PCI_VENDOR_ID_ADAPTEC2, 0x14c1)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       PCI_VENDOR_ID_ADAPTEC2, 0x14c2)
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
@@ -9422,6 +9478,22 @@ static const struct pci_device_id pqi_pci_id_table[] = {
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1590, 0x036f)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1590, 0x0381)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1590, 0x0382)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1590, 0x0383)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
 			       0x1d8d, 0x0800)
 	},
 	{
@@ -9446,6 +9518,10 @@ static const struct pci_device_id pqi_pci_id_table[] = {
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1f0c, 0x3161)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
 			       0x1cf2, 0x5445)
 	},
 	{
@@ -9455,6 +9531,30 @@ static const struct pci_device_id pqi_pci_id_table[] = {
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
 			       0x1cf2, 0x5447)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1cf2, 0x5449)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1cf2, 0x544a)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1cf2, 0x544b)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1cf2, 0x544d)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1cf2, 0x544e)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1cf2, 0x544f)
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
