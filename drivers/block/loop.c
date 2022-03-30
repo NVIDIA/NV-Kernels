560736b092b0e62d67c7a5fb918f18af2b48382a
Christoph Hellwig <hch@lst.de>
2022-03-30 07:29:14 +0200

    loop: suppress uevents while reconfiguring the device
    BugLink: https://bugs.launchpad.net/bugs/2023233
    
    [ Upstream commit 498ef5c777d9c89693b70cc453b40c392120ea1b ]
    
    Currently, udev change event is generated for a loop device before the
    device is ready for IO. Due to serialization on lo->lo_mutex in
    lo_open() this does not matter because anybody is able to open the
    device and do IO only after the configuration is finished. However this
    synchronization in lo_open() is going away so make sure userspace
    reacting to the change event will see the new device state by generating
    the event only when the device is setup.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Link: https://lore.kernel.org/r/20220330052917.2566582-13-hch@lst.de
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Stable-dep-of: bb430b694226 ("loop: LOOP_CONFIGURE: send uevents for partitions")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Luke Nowakowski-Krijger <luke.nowakowskikrijger@canonical.com>

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -746,6 +746,10 @@ static int loop_change_fd(struct loop_device *lo, struct block_device *bdev,
 
 	if (!file)
 		return -EBADF;
+
+	/* suppress uevents while reconfiguring the device */
+	dev_set_uevent_suppress(disk_to_dev(lo->lo_disk), 1);
+
 	is_loop = is_loop_device(file);
 	error = loop_global_lock_killable(lo, is_loop);
 	if (error)
@@ -811,7 +815,12 @@ static int loop_change_fd(struct loop_device *lo, struct block_device *bdev,
 		fput(old_virt_file);
 	if (partscan)
 		loop_reread_partitions(lo);
-	return 0;
+
+	error = 0;
+done:
+	/* enable and uncork uevent now that we are done */
+	dev_set_uevent_suppress(disk_to_dev(lo->lo_disk), 0);
+	return error;
 
 out_err:
 	loop_global_unlock(lo, is_loop);
@@ -819,7 +828,7 @@ out_putf:
 	fput(file);
 	if (virt_file)
 		fput(virt_file);
-	return error;
+	goto done;
 }
 
 /*
@@ -1252,6 +1261,9 @@ static int loop_configure(struct loop_device *lo, fmode_t mode,
 		get_file(file);
 	}
 
+	/* suppress uevents while reconfiguring the device */
+	dev_set_uevent_suppress(disk_to_dev(lo->lo_disk), 1);
+
 	/*
 	 * If we don't hold exclusive handle for the device, upgrade to it
 	 * here to avoid changing device under exclusive owner.
@@ -1359,7 +1371,12 @@ static int loop_configure(struct loop_device *lo, fmode_t mode,
 		loop_reread_partitions(lo);
 	if (!(mode & FMODE_EXCL))
 		bd_abort_claiming(bdev, loop_configure);
-	return 0;
+
+	error = 0;
+done:
+	/* enable and uncork uevent now that we are done */
+	dev_set_uevent_suppress(disk_to_dev(lo->lo_disk), 0);
+	return error;
 
 out_unlock:
 	loop_global_unlock(lo, is_loop);
@@ -1372,7 +1389,7 @@ out_putf:
 		fput(virt_file);
 	/* This is safe: open() is still holding a reference. */
 	module_put(THIS_MODULE);
-	return error;
+	goto done;
 }
 
 static int __loop_clr_fd(struct loop_device *lo, bool release)
