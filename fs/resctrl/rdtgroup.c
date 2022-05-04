542f085d0a61d1646c8c5b1f1f6b14d05ffe72dd
James Morse <james.morse@arm.com>
2022-05-04 17:41:28 +0100

    drivers/perf: Add PMU to support reading resctrl counters via perf
    Resctrl exposes the cache opccupancy and bandwidth counters to user space
    via files. Each control and monitor group has a copy of these files, and
    can read the counter by reading the file.
    
    MPAM needs to allocate a hardware monitor to do the counting work, and
    there may not be enough for every control and monitor group to have one.
    MPAM needs some indication of which counters are currently in use, it
    needs to be able to reject additional users if there are not enough
    monitors.
    
    Add a PMU driver for resctrl. This never touches the hardware directly,
    it only uses the resctrl API to retrieve the counter values. The PMU
    supports the same events as resctrl. The event parameters are the domain
    id, which user-space already understands, and the control or monitor
    group id. The group id is a 64bit field provided by resctrl that can
    be used in the kernel to retrieve the group. Currently this encodes
    the CLOSID and RMID.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: KobaK <kobak@nvidia.com>

--- fs/resctrl/rdtgroup.c
+++ fs/resctrl/rdtgroup.c
@@ -20,6 +20,7 @@
 #include <linux/iommu.h>
 #include <linux/sysfs.h>
 #include <linux/kernfs.h>
+#include <linux/platform_device.h>
 #include <linux/random.h>
 #include <linux/seq_buf.h>
 #include <linux/seq_file.h>
@@ -71,6 +72,8 @@ struct dentry *debugfs_resctrl;
 DEFINE_STATIC_KEY_FALSE(resctrl_abi_playground);
 
 u64 resctrl_id_obsfucation;
+
+static struct platform_device *pmu_pdev;
 
 void rdt_last_cmd_clear(void)
 {
@@ -4167,6 +4170,11 @@ int resctrl_init(void)
 
 	resctrl_id_obsfucation = get_random_u64();
 
+	if (resctrl_is_mbm_enabled()) {
+		pmu_pdev = platform_device_register_simple("resctrl_pmu", 0,
+							   NULL, 0);
+	}
+
 	return 0;
 
 cleanup_mountpoint:
@@ -4179,6 +4187,8 @@ cleanup_root:
 
 void resctrl_exit(void)
 {
+	platform_device_put(pmu_pdev);
+	pmu_pdev = NULL;
 	debugfs_remove_recursive(debugfs_resctrl);
 	unregister_filesystem(&rdt_fs_type);
 	sysfs_remove_mount_point(fs_kobj, "resctrl");
