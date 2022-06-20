f3a66a8df71b0cfd8da185191d6787806b7596f5
Steven Price <steven.price@arm.com>
2022-06-20 11:24:00 +0100

    NVIDIA: SAUCE: KVM: arm64: Handle realm VCPU load
    When loading a realm VCPU much of the work is handled by the RMM so only
    some of the actions are required. Rearrange kvm_arch_vcpu_load()
    slightly so we can bail out early for a realm guest.
    
    Change-Id: Id0e61c6767a0e0fce76f8f3cd6eaa733624a6e70
    Signed-off-by: Steven Price <steven.price@arm.com>
    [cherry-picked: https://lore.kernel.org/all/20241004152804.72508-23-steven.price@arm.com/]
    Signed-off-by: Ian May <ianm@nvidia.com>

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -657,10 +657,6 @@ void kvm_arch_vcpu_load(struct kvm_vcpu *vcpu, int cpu)
 
 	kvm_vgic_load(vcpu);
 	kvm_timer_vcpu_load(vcpu);
-	if (has_vhe())
-		kvm_vcpu_load_vhe(vcpu);
-	kvm_arch_vcpu_load_fp(vcpu);
-	kvm_vcpu_pmu_restore_guest(vcpu);
 	if (kvm_arm_is_pvtime_enabled(&vcpu->arch))
 		kvm_make_request(KVM_REQ_RECORD_STEAL, vcpu);
 
@@ -675,6 +671,15 @@ void kvm_arch_vcpu_load(struct kvm_vcpu *vcpu, int cpu)
 		vcpu->arch.hcr_el2 |= HCR_TWI;
 
 	vcpu_set_pauth_traps(vcpu);
+
+	/* No additional state needs to be loaded on Realmed VMs */
+	if (vcpu_is_rec(vcpu))
+		return;
+
+	if (has_vhe())
+		kvm_vcpu_load_vhe(vcpu);
+	kvm_arch_vcpu_load_fp(vcpu);
+	kvm_vcpu_pmu_restore_guest(vcpu);
 
 	kvm_arch_vcpu_load_debug_state_flags(vcpu);
 
