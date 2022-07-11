77fa63c92acef92dd175585825f8bf92cc1b0c66
Oleg Nesterov <oleg@redhat.com>
2022-07-11 18:16:25 +0200

    fix race between exit_itimers() and /proc/pid/timers
    BugLink: https://bugs.launchpad.net/bugs/1988351
    
    commit d5b36a4dbd06c5e8e36ca8ccc552f679069e2946 upstream.
    
    As Chris explains, the comment above exit_itimers() is not correct,
    we can race with proc_timers_seq_ops. Change exit_itimers() to clear
    signal->posix_timers with ->siglock held.
    
    Cc: <stable@vger.kernel.org>
    Reported-by: chris@accessvector.net
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1302,7 +1302,7 @@ int begin_new_exec(struct linux_binprm * bprm)
 	spin_lock_irq(&me->sighand->siglock);
 	posix_cpu_timers_exit(me);
 	spin_unlock_irq(&me->sighand->siglock);
-	exit_itimers(me->signal);
+	exit_itimers(me);
 	flush_itimer_signals();
 #endif
 
