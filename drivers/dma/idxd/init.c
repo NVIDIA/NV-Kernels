9d34e591c0f8e0a2e42d0cab4e4f2a1617c3989c
Dave Jiang <dave.jiang@intel.com>
2022-09-17 09:12:19 -0700

    dmanegine: idxd: reformat opcap output to match bitmap_parse() input
    To make input and output consistent and prepping for the per WQ operation
    configuration support, change the output of opcap display to match the
    input that is expected by bitmap_parse() helper function. The output will
    be a bitmap with field width as the number of bits using the %*pb format
    specifier for printk() family.
    
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>
    Co-developed-by: Fenghua Yu <fenghua.yu@intel.com>
    Signed-off-by: Fenghua Yu <fenghua.yu@intel.com>
    Link: https://lore.kernel.org/r/20220917161222.2835172-3-fenghua.yu@intel.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    
    CVE-2024-21823
    Security-Prereq-Of: dmaengine: idxd: add a write() method for
                        applications to submit work
    (backported from commit a8563a33a5e26064061f2fb34215c97f0e2995f4)
    [smb: Open code bitmap_zalloc_node() to avoid 7529cc7fbd9c]
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- drivers/dma/idxd/init.c
+++ drivers/dma/idxd/init.c
@@ -437,6 +437,19 @@ static void idxd_read_table_offsets(struct idxd_device *idxd)
 	dev_dbg(dev, "IDXD Perfmon Offset: %#x\n", idxd->perfmon_offset);
 }
 
+static void multi_u64_to_bmap(unsigned long *bmap, u64 *val, int count)
+{
+	int i, j, nr;
+
+	for (i = 0, nr = 0; i < count; i++) {
+		for (j = 0; j < BITS_PER_LONG_LONG; j++) {
+			if (val[i] & BIT(j))
+				set_bit(nr, bmap);
+			nr++;
+		}
+	}
+}
+
 static void idxd_read_caps(struct idxd_device *idxd)
 {
 	struct device *dev = &idxd->pdev->dev;
@@ -491,6 +504,7 @@ static void idxd_read_caps(struct idxd_device *idxd)
 				IDXD_OPCAP_OFFSET + i * sizeof(u64));
 		dev_dbg(dev, "opcap[%d]: %#llx\n", i, idxd->hw.opcap.bits[i]);
 	}
+	multi_u64_to_bmap(idxd->opcap_bmap, &idxd->hw.opcap.bits[0], 4);
 }
 
 static struct idxd_device *idxd_alloc(struct pci_dev *pdev, struct idxd_driver_data *data)
@@ -511,6 +525,14 @@ static struct idxd_device *idxd_alloc(struct pci_dev *pdev, struct idxd_driver_d
 	idxd->id = ida_alloc(&idxd_ida, GFP_KERNEL);
 	if (idxd->id < 0)
 		return NULL;
+
+	idxd->opcap_bmap = kmalloc_array_node(BITS_TO_LONGS(IDXD_MAX_OPCAP_BITS),
+					      sizeof(unsigned long), GFP_KERNEL | __GFP_ZERO,
+					      dev_to_node(dev));
+	if (!idxd->opcap_bmap) {
+		ida_free(&idxd_ida, idxd->id);
+		return NULL;
+	}
 
 	device_initialize(conf_dev);
 	conf_dev->parent = dev;
