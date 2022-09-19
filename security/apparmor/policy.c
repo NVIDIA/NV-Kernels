cc3130f5c494622018bd9f1f3f6d9ca8e503f409
John Johansen <john.johansen@canonical.com>
2022-09-18 23:55:00 -0700

    UBUNTU: SAUCE: apparmor5.0.0 [20/57]: apparmor: prompt: add the ability for profiles to have a learning cache
    BugLink: http://bugs.launchpad.net/bugs/2028253
    
    To support a better complain mode allow caching learned entries off
    of the profile. This can be used to dedup complain messages and
    also as a basis for bulk delivery of complain messages to userspace
    through a non audit logging interface.
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    (cherry picked from https://gitlab.com/jjohansen/apparmor-kernel)
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- security/apparmor/policy.c
+++ security/apparmor/policy.c
@@ -358,6 +358,7 @@ void aa_free_profile(struct aa_profile *profile)
 	kfree_sensitive(profile->hash);
 	aa_put_profile_loaddata(profile->rawdata);
 	aa_label_destroy(&profile->label);
+	aa_audit_cache_destroy(&profile->learning_cache);
 
 	kfree_sensitive(profile);
 }
@@ -408,6 +409,8 @@ struct aa_profile *aa_alloc_profile(const char *hname, struct aa_proxy *proxy,
 	profile->label.vec[0] = profile;
 
 	profile->signal = SIGKILL;
+	aa_audit_cache_init(&profile->learning_cache);
+
 	/* refcount released by caller */
 	return profile;
 
