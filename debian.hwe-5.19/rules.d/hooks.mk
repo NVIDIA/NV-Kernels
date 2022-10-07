5886f8f614f78288cceeda1dfa3861e882a493d0
Dimitri John Ledkov <dimitri.ledkov@canonical.com>
2022-10-07 13:11:28 +0100

    UBUNTU: Start new release
    Ignore: yes
    Signed-off-by: Dimitri John Ledkov <dimitri.ledkov@canonical.com>

--- debian.hwe-5.19/rules.d/hooks.mk
+++ debian.hwe-5.19/rules.d/hooks.mk
@@ -1,6 +1,5 @@
 do_enforce_all = true
-do_libc_dev_package = false
-do_doc_package      = false
-do_tools_common     = false
-do_tools_host       = false
-gcc = gcc-12
+do_libc_dev_package	= false
+do_doc_package		= false
+do_tools_common		= false
+do_tools_host		= false
