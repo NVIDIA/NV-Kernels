f835bac2ca09988a1ae7f32e82a298945eaea1a3
Andrea Righi <andrea.righi@canonical.com>
2023-02-11 15:24:36 +0100

    UBUNTU: SAUCE: rust: properly detect the version of libclang used by bindgen
    BugLink: https://bugs.launchpad.net/bugs/2007654
    
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- scripts/rust_is_available.sh
+++ scripts/rust_is_available.sh
@@ -102,9 +102,7 @@ fi
 # Check that the `libclang` used by the Rust bindings generator is suitable.
 bindgen_libclang_version=$( \
 	LC_ALL=C "$BINDGEN" $(dirname $0)/rust_is_available_bindgen_libclang.h 2>&1 >/dev/null \
-		| grep -F 'clang version ' \
-		| grep -oE '[0-9]+\.[0-9]+\.[0-9]+' \
-		| head -n 1 \
+		| sed -ne 's/.*clang version \([0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/p'
 )
 bindgen_libclang_min_version=$($min_tool_version llvm)
 bindgen_libclang_cversion=$(get_canonical_version $bindgen_libclang_version)
