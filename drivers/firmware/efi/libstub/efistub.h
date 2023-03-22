7576840b0e71863e4dfefacf5f183c216ca8e5c9
Ard Biesheuvel <ardb@kernel.org>
2023-03-22 15:26:21 +0100

    efi/libstub: smbios: Drop unused 'recsize' parameter
    BugLink: https://bugs.launchpad.net/bugs/2015741
    
    We no longer use the recsize argument for locating the string table in
    an SMBIOS record, so we can drop it from the internal API.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- drivers/firmware/efi/libstub/efistub.h
+++ drivers/firmware/efi/libstub/efistub.h
@@ -1102,13 +1102,12 @@ struct efi_smbios_type4_record {
 };
 
 #define efi_get_smbios_string(__record, __type, __name) ({		\
-	int size = sizeof(struct efi_smbios_type ## __type ## _record);	\
 	int off = offsetof(struct efi_smbios_type ## __type ## _record,	\
 			   __name);					\
-	__efi_get_smbios_string((__record), __type, off, size);		\
+	__efi_get_smbios_string((__record), __type, off);		\
 })
 
 const u8 *__efi_get_smbios_string(const struct efi_smbios_record *record,
-				  u8 type, int offset, int recsize);
+				  u8 type, int offset);
 
 #endif
