b9517dd8c6bf2dee97c0fcfff0f2b2ad272cf3ca
Steven Price <steven.price@arm.com>
2023-04-06 15:28:07 +0100

    arm_pmu: Provide a mechanism for disabling the physical IRQ
    Arm CCA assigns the physical PMU device to the guest running in realm
    world, however the IRQs are routed via the host. To enter a realm guest
    while a PMU IRQ is pending it is necessary to block the physical IRQ to
    prevent an immediate exit. Provide a mechanism in the PMU driver for KVM
    to control the physical IRQ.
    
    Signed-off-by: Steven Price <steven.price@arm.com>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Signed-off-by: Shanker Donthineni <sdonthineni@nvidia.com>

--- include/linux/perf/arm_pmu.h
+++ include/linux/perf/arm_pmu.h
@@ -186,6 +186,7 @@ void kvm_host_pmu_init(struct arm_pmu *pmu);
 #endif
 
 bool arm_pmu_irq_is_nmi(void);
+void arm_pmu_set_phys_irq(bool enable);
 
 /* Internal functions only for core arm_pmu code */
 struct arm_pmu *armpmu_alloc(void);
@@ -195,6 +196,10 @@ int armpmu_request_irq(struct arm_pmu * __percpu *armpmu, int irq, int cpu);
 void armpmu_free_irq(struct arm_pmu * __percpu *armpmu, int irq, int cpu);
 
 #define ARMV8_PMU_PDEV_NAME "armv8-pmu"
+
+#else /* CONFIG_ARM_PMU */
+
+static inline void arm_pmu_set_phys_irq(bool enable) {}
 
 #endif /* CONFIG_ARM_PMU */
 
