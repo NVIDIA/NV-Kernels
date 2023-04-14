c0ec092c25ed2a58ff48dd0b69e4b151f3e43288
John Johansen <john.johansen@canonical.com>
2023-04-14 00:24:47 -0700

    UBUNTU: SAUCE: (no-up) apparmor: fix: add missing failure check in compute_xmatch_perms
    This fixes a patch in the out of tree apparmor prompting patchset that
    ubuntu carries.
    
    Add check for failure to allocate the permission table.
    
    BugLink: http://bugs.launchpad.net/bugs/2017903
    Fixes: caa9f579ca72 ("apparmor: isolate policy backwards compatibility to its own file")
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Acked-by: Andrea Righi <andrea.righi@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- security/apparmor/policy_compat.c
+++ security/apparmor/policy_compat.c
@@ -182,6 +182,8 @@ static struct aa_perms *compute_xmatch_perms(struct aa_dfa *xmatch,
 	state_count = xmatch->tables[YYTD_ID_BASE]->td_lolen;
 	/* DFAs are restricted from having a state_count of less than 2 */
 	perms = kvcalloc(state_count, sizeof(struct aa_perms), GFP_KERNEL);
+	if (!perms)
+		return NULL;
 	*size = state_count;
 
 	/* zero init so skip the trap state (state == 0) */
