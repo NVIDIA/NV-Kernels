c242212c1540c86d44b7ea7088eaf3cb5bca045d
Steven Price <steven.price@arm.com>
2023-05-05 16:04:16 +0100

    NVIDIA: SAUCE: kvm: rme: Hide KVM_CAP_READONLY_MEM for realm guests
    For protected memory read only isn't supported. While it may be possible
    to support read only for unprotected memory, this isn't supported at the
    present time.
    
    Change-Id: Id0964bfdaf17da1b6afd707c83380c124dbfc453
    Signed-off-by: Steven Price <steven.price@arm.com>
    [cherry-picked: https://lore.kernel.org/all/20241004152804.72508-35-steven.price@arm.com/]
    Signed-off-by: Ian May <ianm@nvidia.com>

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -375,7 +375,6 @@ int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
 	case KVM_CAP_ONE_REG:
 	case KVM_CAP_ARM_PSCI:
 	case KVM_CAP_ARM_PSCI_0_2:
-	case KVM_CAP_READONLY_MEM:
 	case KVM_CAP_MP_STATE:
 	case KVM_CAP_IMMEDIATE_EXIT:
 	case KVM_CAP_VCPU_EVENTS:
@@ -389,6 +388,7 @@ int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
 	case KVM_CAP_COUNTER_OFFSET:
 		r = 1;
 		break;
+	case KVM_CAP_READONLY_MEM:
 	case KVM_CAP_SET_GUEST_DEBUG:
 		r = !kvm_is_realm(kvm);
 		break;
