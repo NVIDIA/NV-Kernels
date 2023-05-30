fc2bad796e50a5506ab439c20c9380dbf8f00578
Mark Rutland <mark.rutland@arm.com>
2023-05-30 12:03:27 +0100

    arm64: module: mandate MODULE_PLTS
    BugLink: https://bugs.launchpad.net/bugs/2024857
    
    Contemporary kernels and modules can be relatively large, especially
    when common debug options are enabled. Using GCC 12.1.0, a v6.3-rc7
    defconfig kernel is ~38M, and with PROVE_LOCKING + KASAN_INLINE enabled
    this expands to ~117M. Shanker reports [1] that the NVIDIA GPU driver
    alone can consume 110M of module space in some configurations.
    
    Both KASLR and ARM64_ERRATUM_843419 select MODULE_PLTS, so anyone
    wanting a kernel to have KASLR or run on Cortex-A53 will have
    MODULE_PLTS selected. This is the case in defconfig and distribution
    kernels (e.g. Debian, Android, etc).
    
    Practically speaking, this means we're very likely to need MODULE_PLTS
    and while it's almost guaranteed that MODULE_PLTS will be selected, it
    is possible to disable support, and we have to maintain some awkward
    special cases for such unusual configurations.
    
    This patch removes the MODULE_PLTS config option, with the support code
    always enabled if MODULES is selected. This results in a slight
    simplification, and will allow for further improvement in subsequent
    patches.
    
    For any config which currently selects MODULE_PLTS, there will be no
    functional change as a result of this patch.
    
    [1] https://lore.kernel.org/linux-arm-kernel/159ceeab-09af-3174-5058-445bc8dcf85b@nvidia.com/
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Reviewed-by: Ard Biesheuvel <ardb@kernel.org>
    Cc: Shanker Donthineni <sdonthineni@nvidia.com>
    Cc: Will Deacon <will@kernel.org>
    Tested-by: Shanker Donthineni <sdonthineni@nvidia.com>
    Link: https://lore.kernel.org/r/20230530110328.2213762-6-mark.rutland@arm.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    [tdave: merge conflict in arch/arm64/kernel/ftrace.c and Makefile]
    (cherry picked from commit ea3752ba9685b47db4571ddaee39344cf2b0bf45 linux-next)
    Signed-off-by: Tushar Dave <tdave@nvidia.com>
    Acked-by: Brad Figg <bfigg@nvidia.com>
    Acked-by: Ian May <ian.may@canonical.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Signed-off-by: Brad Figg <bfigg@nvidia.com>

--- arch/arm64/kernel/ftrace.c
+++ arch/arm64/kernel/ftrace.c
@@ -107,7 +107,7 @@ int ftrace_update_ftrace_func(ftrace_func_t func)
 
 static struct plt_entry *get_ftrace_plt(struct module *mod, unsigned long addr)
 {
-#ifdef CONFIG_ARM64_MODULE_PLTS
+#ifdef CONFIG_MODULES
 	struct plt_entry *plt = mod->arch.ftrace_trampolines;
 
 	if (addr == FTRACE_ADDR)
@@ -145,7 +145,7 @@ static bool ftrace_find_callable_addr(struct dyn_ftrace *rec,
 	 * must use a PLT to reach it. We can only place PLTs for modules, and
 	 * only when module PLT support is built-in.
 	 */
-	if (!IS_ENABLED(CONFIG_ARM64_MODULE_PLTS))
+	if (!IS_ENABLED(CONFIG_MODULES))
 		return false;
 
 	/*
@@ -249,10 +249,8 @@ int ftrace_make_nop(struct module *mod, struct dyn_ftrace *rec,
 	 *
 	 * Note: 'mod' is only set at module load time.
 	 */
-	if (!IS_ENABLED(CONFIG_DYNAMIC_FTRACE_WITH_ARGS) &&
-	    IS_ENABLED(CONFIG_ARM64_MODULE_PLTS) && mod) {
+	if (!IS_ENABLED(CONFIG_DYNAMIC_FTRACE_WITH_ARGS) && mod)
 		return aarch64_insn_patch_text_nosync((void *)pc, new);
-	}
 
 	if (!ftrace_find_callable_addr(rec, mod, &addr))
 		return -EINVAL;
