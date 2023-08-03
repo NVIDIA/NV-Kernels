30d493216407a5c528393795861c29130b52b536
Steven Price <steven.price@arm.com>
2023-08-03 11:22:48 +0100

    NVIDIA: VR: CCA: SAUCE: arm64: RMI: Enable PMU support with a realm guest
    Use the PMU registers from the RmiRecExit structure to identify when an
    overflow interrupt is due and inject it into the guest. Also hook up the
    configuration option for enabling the PMU within the guest.
    
    When entering a realm guest with a PMU interrupt pending, it is
    necessary to disable the physical interrupt. Otherwise when the RMM
    restores the PMU state the physical interrupt will trigger causing an
    immediate exit back to the host. The guest is expected to acknowledge
    the interrupt causing a host exit (to update the GIC state) which gives
    the opportunity to re-enable the physical interrupt before the next PMU
    event.
    
    Number of PMU counters is configured by the VMM by writing to PMCR.N.
    
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Signed-off-by: Steven Price <steven.price@arm.com>
    (cherry picked from commit f0b492646d4fb199deb17fd69e29493040b76cd5 https://gitlab.arm.com/linux-arm/linux-cca)
    Signed-off-by: Ian May <ianm@nvidia.com>
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Jamie Nguyen <jamien@nvidia.com>
    Acked-by: Nirmoy Das <nirmoyd@nvidia.com>
    Acked-by: Carol L Soto <csoto@nvidia.com>
    Signed-off-by: Jamie Nguyen <jamien@nvidia.com>

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -14,6 +14,7 @@
 #include <linux/vmalloc.h>
 #include <linux/fs.h>
 #include <linux/mman.h>
+#include <linux/perf/arm_pmu.h>
 #include <linux/sched.h>
 #include <linux/kvm.h>
 #include <linux/kvm_irqfd.h>
@@ -1266,6 +1267,8 @@ int kvm_arch_vcpu_ioctl_run(struct kvm_vcpu *vcpu)
 	run->exit_reason = KVM_EXIT_UNKNOWN;
 	run->flags = 0;
 	while (ret > 0) {
+		bool pmu_stopped = false;
+
 		/*
 		 * Check conditions before entering the guest
 		 */
@@ -1290,6 +1293,11 @@ int kvm_arch_vcpu_ioctl_run(struct kvm_vcpu *vcpu)
 
 		if (kvm_vcpu_has_pmu(vcpu))
 			kvm_pmu_flush_hwstate(vcpu);
+
+		if (vcpu_is_rec(vcpu) && kvm_pmu_get_irq_level(vcpu)) {
+			pmu_stopped = true;
+			arm_pmu_set_phys_irq(false);
+		}
 
 		local_irq_disable();
 
@@ -1398,6 +1406,9 @@ int kvm_arch_vcpu_ioctl_run(struct kvm_vcpu *vcpu)
 		kvm_nested_sync_hwstate(vcpu);
 
 		preempt_enable();
+
+		if (pmu_stopped)
+			arm_pmu_set_phys_irq(true);
 
 		/*
 		 * The ARMv8 architecture doesn't give the hypervisor
