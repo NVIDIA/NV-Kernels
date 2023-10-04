e8ae6593cbec63bf99c0c1e1a446817188e81eb3
Jason Gunthorpe <jgg@nvidia.com>
2023-10-04 09:08:32 -0300

    iommu: Fix return code in iommu_group_alloc_default_domain()
    This function returns NULL on errors, not ERR_PTR.
    
    Fixes: 1c68cbc64fe6 ("iommu: Add IOMMU_DOMAIN_PLATFORM")
    Reported-by: Dan Carpenter <dan.carpenter@linaro.org>
    Link: https://lore.kernel.org/r/8fb75157-6c81-4a9c-9992-d73d49902fa8@moroto.mountain
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    Link: https://lore.kernel.org/r/0-v2-ee2bae9af0f2+96-iommu_ga_err_ptr_jgg@nvidia.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    (cherry picked from commit b85b4f30846bb169c114e99ceee17cc119f02a4b)
    Signed-off-by: Koba Ko <kobak@nvidia.com>

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -1607,7 +1607,7 @@ iommu_group_alloc_default_domain(struct iommu_group *group, int req_type)
 	 */
 	if (ops->default_domain) {
 		if (req_type != ops->default_domain->type)
-			return ERR_PTR(-EINVAL);
+			return NULL;
 		return ops->default_domain;
 	}
 
