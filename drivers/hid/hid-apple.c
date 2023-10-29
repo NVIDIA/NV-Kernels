958417a895b9d82c1d1c69c83a7b5c093a5fd153
Yihong Cao <caoyihong4@outlook.com>
2023-10-30 01:05:38 +0800

    HID: apple: add Jamesdonkey and A3R to non-apple keyboards list
    BugLink: https://bugs.launchpad.net/bugs/2051924
    
    [ Upstream commit 113f736655e4f20633e107d731dd5bd097d5938c ]
    
    Jamesdonkey A3R keyboard is identified as "Jamesdonkey A3R" in wired
    mode, "A3R-U" in wireless mode and "A3R" in bluetooth mode. Adding them
    to non-apple keyboards fixes function key.
    
    Signed-off-by: Yihong Cao <caoyihong4@outlook.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Manuel Diewald <manuel.diewald@canonical.com>
    Signed-off-by: Roxana Nicolescu <roxana.nicolescu@canonical.com>

--- drivers/hid/hid-apple.c
+++ drivers/hid/hid-apple.c
@@ -343,7 +343,9 @@ static const struct apple_non_apple_keyboard non_apple_keyboards[] = {
 	{ "SONiX USB DEVICE" },
 	{ "Keychron" },
 	{ "AONE" },
-	{ "GANSS" }
+	{ "GANSS" },
+	{ "Jamesdonkey" },
+	{ "A3R" },
 };
 
 static bool apple_is_non_apple_keyboard(struct hid_device *hdev)
