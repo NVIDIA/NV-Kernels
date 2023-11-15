c32c2a5fe91a5973b2637e61fba85d8c62d69102
Robin Murphy <robin.murphy@arm.com>
2023-11-15 18:25:44 +0000

    iommu: Avoid more races around device probe
    BugLink: https://bugs.launchpad.net/bugs/2051366
    
    commit a2e7e59a94269484a83386972ca07c22fd188854 upstream.
    
    It turns out there are more subtle races beyond just the main part of
    __iommu_probe_device() itself running in parallel - the dev_iommu_free()
    on the way out of an unsuccessful probe can still manage to trip up
    concurrent accesses to a device's fwspec. Thus, extend the scope of
    iommu_probe_device_lock() to also serialise fwspec creation and initial
    retrieval.
    
    Reported-by: Zhenhua Huang <quic_zhenhuah@quicinc.com>
    Link: https://lore.kernel.org/linux-iommu/e2e20e1c-6450-4ac5-9804-b0000acdf7de@quicinc.com/
    Fixes: 01657bc14a39 ("iommu: Avoid races around device probe")
    Signed-off-by: Robin Murphy <robin.murphy@arm.com>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Reviewed-by: André Draszik <andre.draszik@linaro.org>
    Tested-by: André Draszik <andre.draszik@linaro.org>
    Link: https://lore.kernel.org/r/16f433658661d7cadfea51e7c65da95826112a2b.1700071477.git.robin.murphy@arm.com
    Cc: stable@vger.kernel.org
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Manuel Diewald <manuel.diewald@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -333,12 +333,13 @@ static u32 dev_iommu_get_max_pasids(struct device *dev)
 	return min_t(u32, max_pasids, dev->iommu->iommu_dev->max_pasids);
 }
 
+DEFINE_MUTEX(iommu_probe_device_lock);
+
 static int __iommu_probe_device(struct device *dev, struct list_head *group_list)
 {
 	const struct iommu_ops *ops = dev->bus->iommu_ops;
 	struct iommu_device *iommu_dev;
 	struct iommu_group *group;
-	static DEFINE_MUTEX(iommu_probe_device_lock);
 	int ret;
 
 	if (!ops)
@@ -350,10 +351,9 @@ static int __iommu_probe_device(struct device *dev, struct list_head *group_list
 	 * probably be able to use device_lock() here to minimise the scope,
 	 * but for now enforcing a simple global ordering is fine.
 	 */
-	mutex_lock(&iommu_probe_device_lock);
+	lockdep_assert_held(&iommu_probe_device_lock);
 	if (!dev_iommu_get(dev)) {
-		ret = -ENOMEM;
-		goto err_unlock;
+		return -ENOMEM;
 	}
 
 	if (!try_module_get(ops->owner)) {
@@ -384,7 +384,6 @@ static int __iommu_probe_device(struct device *dev, struct list_head *group_list
 	mutex_unlock(&group->mutex);
 	iommu_group_put(group);
 
-	mutex_unlock(&iommu_probe_device_lock);
 	iommu_device_link(iommu_dev, dev);
 
 	return 0;
@@ -399,9 +398,6 @@ out_module_put:
 err_free:
 	dev_iommu_free(dev);
 
-err_unlock:
-	mutex_unlock(&iommu_probe_device_lock);
-
 	return ret;
 }
 
@@ -411,7 +407,9 @@ int iommu_probe_device(struct device *dev)
 	struct iommu_group *group;
 	int ret;
 
+	mutex_lock(&iommu_probe_device_lock);
 	ret = __iommu_probe_device(dev, NULL);
+	mutex_unlock(&iommu_probe_device_lock);
 	if (ret)
 		goto err_out;
 
@@ -1721,7 +1719,9 @@ static int probe_iommu_group(struct device *dev, void *data)
 		return 0;
 	}
 
+	mutex_lock(&iommu_probe_device_lock);
 	ret = __iommu_probe_device(dev, group_list);
+	mutex_unlock(&iommu_probe_device_lock);
 	if (ret == -ENODEV)
 		ret = 0;
 
