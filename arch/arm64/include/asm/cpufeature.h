d0d1ce486d2d2f5115b29aaf467e15c67b2a42e7
Ryan Roberts <ryan.roberts@arm.com>
2023-11-27 11:17:30 +0000

    arm64: Add ARM64_HAS_LPA2 CPU capability
    Expose FEAT_LPA2 as a capability so that we can take advantage of
    alternatives patching in the hypervisor.
    
    Although FEAT_LPA2 presence is advertised separately for stage1 and
    stage2, the expectation is that in practice both stages will either
    support or not support it. Therefore, we combine both into a single
    capability, allowing us to simplify the implementation. KVM requires
    support in both stages in order to use LPA2 since the same library is
    used for hyp stage 1 and guest stage 2 pgtables.
    
    Reviewed-by: Oliver Upton <oliver.upton@linux.dev>
    Signed-off-by: Ryan Roberts <ryan.roberts@arm.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20231127111737.1897081-6-ryan.roberts@arm.com
    (cherry picked from commit b1366d21daaebb8e474e4169c5e557fbb37bfdc0)
    Signed-off-by: Koba Ko <kobak@nvidia.com>

--- arch/arm64/include/asm/cpufeature.h
+++ arch/arm64/include/asm/cpufeature.h
@@ -831,6 +831,11 @@ static inline bool system_supports_tlb_range(void)
 		cpus_have_const_cap(ARM64_HAS_TLB_RANGE);
 }
 
+static inline bool system_supports_lpa2(void)
+{
+	return cpus_have_final_cap(ARM64_HAS_LPA2);
+}
+
 int do_emulate_mrs(struct pt_regs *regs, u32 sys_reg, u32 rt);
 bool try_emulate_mrs(struct pt_regs *regs, u32 isn);
 
