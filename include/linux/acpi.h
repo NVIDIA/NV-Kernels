603457f1e55a42106b41777931dab6062608e856
James Morse <james.morse@arm.com>
2023-12-04 14:33:19 +0000

    ACPI / PPTT: Add a helper to fill a cpumask from a processor container
    The PPTT describes CPUs and caches, as well as processor containers.
    The ACPI table for MPAM describes the set of CPUs that can access an MSC
    with the UID of a processor container.
    
    Add a helper to find the processor container by its id, then walk
    the possible CPUs to fill a cpumask with the CPUs that have this
    processor container as a parent.
    
    CC: Dave Martin <dave.martin@arm.com>
    Reviewed-by: Sudeep Holla <sudeep.holla@arm.com>
    Signed-off-by: James Morse <james.morse@arm.com>

--- include/linux/acpi.h
+++ include/linux/acpi.h
@@ -1541,6 +1541,7 @@ int find_acpi_cpu_topology(unsigned int cpu, int level);
 int find_acpi_cpu_topology_cluster(unsigned int cpu);
 int find_acpi_cpu_topology_package(unsigned int cpu);
 int find_acpi_cpu_topology_hetero_id(unsigned int cpu);
+int acpi_pptt_get_cpus_from_container(u32 acpi_cpu_id, cpumask_t *cpus);
 #else
 static inline int acpi_pptt_cpu_is_thread(unsigned int cpu)
 {
@@ -1559,6 +1560,11 @@ static inline int find_acpi_cpu_topology_package(unsigned int cpu)
 	return -EINVAL;
 }
 static inline int find_acpi_cpu_topology_hetero_id(unsigned int cpu)
+{
+	return -EINVAL;
+}
+static inline int acpi_pptt_get_cpus_from_container(u32 acpi_cpu_id,
+						    cpumask_t *cpus)
 {
 	return -EINVAL;
 }
