145d57b83352af036415143ed8592774b69f5efe
Emil Renner Berthing <emil.renner.berthing@canonical.com>
2023-12-04 13:14:49 +0100

    UBUNTU: [Packaging] enable riscv64 builds
    Add ABI files, sync riscv64.mk with linux-riscv and add riscv64 to the
    arch list.
    
    Signed-off-by: Emil Renner Berthing <emil.renner.berthing@canonical.com>
    Acked-by: Dimitri John Ledkov <dimitri.ledkov@canonical.com>
    Signed-off-by: Dimitri John Ledkov <dimitri.ledkov@canonical.com>

--- debian.master/rules.d/riscv64.mk
+++ debian.master/rules.d/riscv64.mk
@@ -1,7 +1,7 @@
 human_arch	= RISC-V
 build_arch	= riscv
 defconfig	= defconfig
-flavours        = generic
+flavours	= generic
 build_image	= Image
 kernel_file	= arch/$(build_arch)/boot/Image
 install_file	= vmlinuz
@@ -9,10 +9,10 @@ install_file	= vmlinuz
 vdso		= vdso_install
 no_dumpfile	= true
 
-do_flavour_image_package = false
-do_tools	= false
-do_tools_common	= false
-do_extras_package = false
-do_source_package = false
-do_flavour_header_package = false
-do_common_headers_indep = false
+do_extras_package	= true
+do_tools_usbip		= true
+do_tools_cpupower	= true
+do_tools_perf		= true
+do_tools_perf_jvmti	= true
+do_tools_bpftool	= true
+do_dtbs			= true
