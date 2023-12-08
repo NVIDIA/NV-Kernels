3a799ad767efaad6a16dc55f2aecadda5cd0b567
SeongJae Park <sj@kernel.org>
2023-12-08 17:50:18 +0000

    mm/damon/core: make damon_start() waits until kdamond_fn() starts
    BugLink: https://bugs.launchpad.net/bugs/2052499
    
    commit 6376a824595607e99d032a39ba3394988b4fce96 upstream.
    
    The cleanup tasks of kdamond threads including reset of corresponding
    DAMON context's ->kdamond field and decrease of global nr_running_ctxs
    counter is supposed to be executed by kdamond_fn().  However, commit
    0f91d13366a4 ("mm/damon: simplify stop mechanism") made neither
    damon_start() nor damon_stop() ensure the corresponding kdamond has
    started the execution of kdamond_fn().
    
    As a result, the cleanup can be skipped if damon_stop() is called fast
    enough after the previous damon_start().  Especially the skipped reset
    of ->kdamond could cause a use-after-free.
    
    Fix it by waiting for start of kdamond_fn() execution from
    damon_start().
    
    Link: https://lkml.kernel.org/r/20231208175018.63880-1-sj@kernel.org
    Fixes: 0f91d13366a4 ("mm/damon: simplify stop mechanism")
    Signed-off-by: SeongJae Park <sj@kernel.org>
    Reported-by: Jakub Acs <acsjakub@amazon.de>
    Cc: Changbin Du <changbin.du@intel.com>
    Cc: Jakub Acs <acsjakub@amazon.de>
    Cc: <stable@vger.kernel.org> # 5.15.x
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: SeongJae Park <sj@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Manuel Diewald <manuel.diewald@canonical.com>
    Signed-off-by: Roxana Nicolescu <roxana.nicolescu@canonical.com>

--- include/linux/damon.h
+++ include/linux/damon.h
@@ -8,6 +8,7 @@
 #ifndef _DAMON_H_
 #define _DAMON_H_
 
+#include <linux/completion.h>
 #include <linux/memcontrol.h>
 #include <linux/mutex.h>
 #include <linux/time64.h>
@@ -508,6 +509,8 @@ struct damon_ctx {
 /* private: internal use only */
 	struct timespec64 last_aggregation;
 	struct timespec64 last_ops_update;
+	/* for waiting until the execution of the kdamond_fn is started */
+	struct completion kdamond_started;
 
 /* public: */
 	struct task_struct *kdamond;
