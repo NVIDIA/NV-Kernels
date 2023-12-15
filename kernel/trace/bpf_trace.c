04489285e9b5144c5669f904516da947be3edec4
Hou Tao <houtao1@huawei.com>
2023-12-15 18:07:04 +0800

    bpf: Limit the number of uprobes when attaching program to multiple uprobes
    BugLink: https://bugs.launchpad.net/bugs/2056403
    
    [ Upstream commit 8b2efe51ba85ca83460941672afac6fca4199df6 ]
    
    An abnormally big cnt may be passed to link_create.uprobe_multi.cnt,
    and it will trigger the following warning in kvmalloc_node():
    
    	if (unlikely(size > INT_MAX)) {
    		WARN_ON_ONCE(!(flags & __GFP_NOWARN));
    		return NULL;
    	}
    
    Fix the warning by limiting the maximal number of uprobes in
    bpf_uprobe_multi_link_attach(). If the number of uprobes is greater than
    MAX_UPROBE_MULTI_CNT, the attachment will return -E2BIG.
    
    Fixes: 89ae89f53d20 ("bpf: Add multi uprobe link")
    Reported-by: Xingwei Lee <xrivendell7@gmail.com>
    Signed-off-by: Hou Tao <houtao1@huawei.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Jiri Olsa <jolsa@kernel.org>
    Acked-by: Andrii Nakryiko <andrii@kernel.org>
    Closes: https://lore.kernel.org/bpf/CABOYnLwwJY=yFAGie59LFsUsBAgHfroVqbzZ5edAXbFE3YiNVA@mail.gmail.com
    Link: https://lore.kernel.org/bpf/20231215100708.2265609-2-houtao@huaweicloud.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Portia Stephens <portia.stephens@canonical.com>
    Signed-off-by: Roxana Nicolescu <roxana.nicolescu@canonical.com>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -40,6 +40,8 @@
 #define bpf_event_rcu_dereference(p)					\
 	rcu_dereference_protected(p, lockdep_is_held(&bpf_event_mutex))
 
+#define MAX_UPROBE_MULTI_CNT (1U << 20)
+
 #ifdef CONFIG_MODULES
 struct bpf_trace_module {
 	struct module *module;
@@ -2814,6 +2816,8 @@ int bpf_kprobe_multi_link_attach(const union bpf_attr *attr, struct bpf_prog *pr
 	cnt = attr->link_create.kprobe_multi.cnt;
 	if (!cnt)
 		return -EINVAL;
+	if (cnt > MAX_UPROBE_MULTI_CNT)
+		return -E2BIG;
 
 	size = cnt * sizeof(*addrs);
 	addrs = kvmalloc_array(cnt, sizeof(*addrs), GFP_KERNEL);
