e621ee988fce0c420c29540cbacdf993e469a1bf
Hou Tao <houtao1@huawei.com>
2023-12-15 18:07:05 +0800

    bpf: Limit the number of kprobes when attaching program to multiple kprobes
    BugLink: https://bugs.launchpad.net/bugs/2056403
    
    [ Upstream commit d6d1e6c17cab2dcb7b8530c599f00e7de906d380 ]
    
    An abnormally big cnt may also be assigned to kprobe_multi.cnt when
    attaching multiple kprobes. It will trigger the following warning in
    kvmalloc_node():
    
    	if (unlikely(size > INT_MAX)) {
    	    WARN_ON_ONCE(!(flags & __GFP_NOWARN));
    	    return NULL;
    	}
    
    Fix the warning by limiting the maximal number of kprobes in
    bpf_kprobe_multi_link_attach(). If the number of kprobes is greater than
    MAX_KPROBE_MULTI_CNT, the attachment will fail and return -E2BIG.
    
    Fixes: 0dcac2725406 ("bpf: Add multi kprobe link")
    Signed-off-by: Hou Tao <houtao1@huawei.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Jiri Olsa <jolsa@kernel.org>
    Acked-by: Andrii Nakryiko <andrii@kernel.org>
    Link: https://lore.kernel.org/bpf/20231215100708.2265609-3-houtao@huaweicloud.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Portia Stephens <portia.stephens@canonical.com>
    Signed-off-by: Roxana Nicolescu <roxana.nicolescu@canonical.com>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -41,6 +41,7 @@
 	rcu_dereference_protected(p, lockdep_is_held(&bpf_event_mutex))
 
 #define MAX_UPROBE_MULTI_CNT (1U << 20)
+#define MAX_KPROBE_MULTI_CNT (1U << 20)
 
 #ifdef CONFIG_MODULES
 struct bpf_trace_module {
@@ -2812,6 +2813,8 @@ int bpf_kprobe_multi_link_attach(const union bpf_attr *attr, struct bpf_prog *pr
 	usyms = u64_to_user_ptr(attr->link_create.kprobe_multi.syms);
 	if (!!uaddrs == !!usyms)
 		return -EINVAL;
+	if (cnt > MAX_KPROBE_MULTI_CNT)
+		return -E2BIG;
 
 	cnt = attr->link_create.kprobe_multi.cnt;
 	if (!cnt)
