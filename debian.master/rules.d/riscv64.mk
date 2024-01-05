275ec10505376857b3e8f2771886df1f6eafc486
Andrea Righi <andrea.righi@canonical.com>
2024-01-05 12:24:48 +0100

    UBUNTU: [Packaging] temporarily disable riscv64 builds
    Disable riscv64 build for now, since the ABI files are completely
    removed every time we start a new kernel release.
    
    Moreover, building riscv64 in generic consistently increase the overall
    build time of the packages (3 days or more), slowing down the testing
    process of the new published kernels.
    
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- debian.master/rules.d/riscv64.mk
+++ debian.master/rules.d/riscv64.mk
@@ -1,18 +1,18 @@
-human_arch	= RISC-V
-build_arch	= riscv
-defconfig	= defconfig
-flavours	= generic
-build_image	= Image
-kernel_file	= arch/$(build_arch)/boot/Image
-install_file	= vmlinuz
+human_arch     = RISC-V
+build_arch     = riscv
+defconfig      = defconfig
+flavours       = generic
+build_image    = Image
+kernel_file    = arch/$(build_arch)/boot/Image
+install_file   = vmlinuz
 
-vdso		= vdso_install
-no_dumpfile	= true
+vdso           = vdso_install
+no_dumpfile    = true
 
-do_extras_package	= true
-do_tools_usbip		= true
-do_tools_cpupower	= true
-do_tools_perf		= true
-do_tools_perf_jvmti	= true
-do_tools_bpftool	= true
-do_dtbs			= true
+do_extras_package      = false
+do_tools_usbip         = false
+do_tools_cpupower      = false
+do_tools_perf          = false
+do_tools_perf_jvmti    = false
+do_tools_bpftool       = false
+do_dtbs                = false
