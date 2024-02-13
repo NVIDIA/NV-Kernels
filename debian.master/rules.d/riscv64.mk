b213ac78294df6b99353459d5509bb912c32df7e
Paolo Pisati <paolo.pisati@canonical.com>
2024-02-13 23:02:18 +0100

    UBUNTU: [Packaging] riscv64: disable building unnecessary binary debs
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- debian.master/rules.d/riscv64.mk
+++ debian.master/rules.d/riscv64.mk
@@ -9,6 +9,12 @@ install_file   = vmlinuz
 vdso           = vdso_install
 no_dumpfile    = true
 
+do_flavour_image_package = false
+do_tools                 = false
+do_tools_common          = false
+do_source_package        = false
+do_flavour_header_package = false
+do_common_headers_indep = false
 do_extras_package      = false
 do_tools_usbip         = false
 do_tools_cpupower      = false
