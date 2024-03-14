48357b9b6d27526f72e6fa098ded2d2319e5e85d
Andrea Righi <andrea.righi@canonical.com>
2024-03-14 06:56:39 +0100

    UBUNTU: [Packaging] enable perf python module
    BugLink: https://bugs.launchpad.net/bugs/2051560
    
    The perf python module is required by some tools (e.g., tuned) and we
    are not currently providing it.
    
    Enable it to be able to support tools that require this module.
    
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- debian.master/rules.d/ppc64el.mk
+++ debian.master/rules.d/ppc64el.mk
@@ -12,6 +12,7 @@ do_tools_usbip    = true
 do_tools_cpupower = true
 do_tools_perf	  = true
 do_tools_perf_jvmti = true
+do_tools_perf_python = true
 do_tools_bpftool  = true
 
 #do_flavour_image_package = false
