3f54afdf5058b06ed38bf019862c7233bcb4d4e9
James Morse <james.morse@arm.com>
2024-03-15 17:36:02 +0000

    NVIDIA: SAUCE: fs/restrl: Allow the overflow handler to be disabled
    BugLink: https://bugs.launchpad.net/bugs/2122432
    
    Resctrl has an overflow handler that runs on each domain every second
    to ensure that any overflow of the hardware counter is accounted for.
    MPAM can have counters as large as 63 bits, in which case there is no
    need to check for overflow.
    Call the new arch helpers to determine this.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    (cherry picked from commit 7120f602ee83c2fb75517b137efd41b83777ba76 https://git.kernel.org/pub/scm/linux/kernel/git/morse/linux.git)
    Signed-off-by: Fenghua Yu <fenghuay@nvidia.com>
    Acked-by: Jamie Nguyen <jamien@nvidia.com>
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    
    (cherry picked from commit 3bd638c7e02cac36a20061cc47817067c47fecab noble:linux-nvidia-6.17)
    Signed-off-by: Jacob Martin <jacob.martin@canonical.com>

--- fs/resctrl/monitor.c
+++ fs/resctrl/monitor.c
@@ -920,8 +920,10 @@ void mbm_setup_overflow_handler(struct rdt_l3_mon_domain *dom, unsigned long del
 	/*
 	 * When a domain comes online there is no guarantee the filesystem is
 	 * mounted. If not, there is no need to catch counter overflow.
+	 * Some architecture may have ~64bit counters, and can ignore overflow.
 	 */
-	if (!resctrl_mounted || !resctrl_arch_mon_capable())
+	if (!resctrl_mounted || !resctrl_arch_mon_capable() ||
+	    !resctrl_arch_mon_can_overflow())
 		return;
 	cpu = cpumask_any_housekeeping(&dom->hdr.cpu_mask, exclude_cpu);
 	dom->mbm_work_cpu = cpu;
