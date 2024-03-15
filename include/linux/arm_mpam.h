6b9e4f104b7c99fe03890370b731e6d2a0613dfc
James Morse <james.morse@arm.com>
2024-03-15 17:32:53 +0000

    NVIDIA: SAUCE: arm_mpam: resctrl: Determine if any exposed counter can overflow
    BugLink: https://bugs.launchpad.net/bugs/2154527
    
    Resctrl has an overflow handler that runs on each domain every second
    to ensure that any overflow of the hardware counter is accounted for.
    MPAM can have counters as large as 63 bits, in which case there is no
    need to check for overflow.
    To allow the overflow handler to be disabled, determine if an overflow
    can happen. If a class is not implemented, or has the 63bit counter,
    it can't overflow.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    (cherry picked from commit 0f6aefdf5164dd6be3bd8c6cd82b6257fadbeab2 https://github.com/NVIDIA/NV-Kernels 24.04_linux-nvidia-6.17-next)
    [fenghuay:
      - Resolve minor conflicts in `drivers/resctrl/mpam_resctrl.c`;
    ]
    Signed-off-by: Fenghua Yu <fenghuay@nvidia.com>
    Acked-by: Jamie Nguyen <jamien@nvidia.com>
    Acked-by: Carol L Soto <csoto@nvidia.com>
    Signed-off-by: Brad Figg <bfigg@nvidia.com>

--- include/linux/arm_mpam.h
+++ include/linux/arm_mpam.h
@@ -53,6 +53,7 @@ static inline int mpam_ris_create(struct mpam_msc *msc, u8 ris_idx,
 
 bool resctrl_arch_alloc_capable(void);
 bool resctrl_arch_mon_capable(void);
+bool resctrl_arch_mon_can_overflow(void);
 
 void resctrl_arch_set_cpu_default_closid(int cpu, u32 closid);
 void resctrl_arch_set_closid_rmid(struct task_struct *tsk, u32 closid, u32 rmid);
