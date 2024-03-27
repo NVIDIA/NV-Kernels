9aa12066aed524548359cec850ca422d6e2fda9b
Jason Gunthorpe <jgg@nvidia.com>
2024-03-27 15:07:49 -0300

    iommu/arm-smmu-v3: Do not allow a SVA domain to be set on the wrong PASID
    BugLink: https://bugs.launchpad.net/bugs/2076435
    
    [ Upstream commit fdc69d39e77f88264ee6e8174ff9aaf0953aecd9 ]
    
    The SVA code is wired to assume that the SVA is programmed onto the
    mm->pasid. The current core code always does this, so it is fine.
    
    Add a check for clarity.
    
    Tested-by: Nicolin Chen <nicolinc@nvidia.com>
    Tested-by: Shameer Kolothum <shameerali.kolothum.thodi@huawei.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    Link: https://lore.kernel.org/r/3-v6-228e7adf25eb+4155-smmuv3_newapi_p2_jgg@nvidia.com
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Portia Stephens <portia.stephens@canonical.com>
    Signed-off-by: Roxana Nicolescu <roxana.nicolescu@canonical.com>

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3-sva.c
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3-sva.c
@@ -575,6 +575,9 @@ static int arm_smmu_sva_set_dev_pasid(struct iommu_domain *domain,
 	int ret = 0;
 	struct mm_struct *mm = domain->mm;
 
+	if (mm_get_enqcmd_pasid(mm) != id)
+		return -EINVAL;
+
 	mutex_lock(&sva_lock);
 	ret = __arm_smmu_sva_bind(dev, id, mm);
 	mutex_unlock(&sva_lock);
