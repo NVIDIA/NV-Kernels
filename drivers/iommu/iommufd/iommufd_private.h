621c0ae5b6f7ff1f3a1b18ad00fe06f92fa8a342
Nicolin Chen <nicolinc@nvidia.com>
2024-04-04 20:50:46 +0000

    iommufd: Add IOMMUFD_OBJ_VQUEUE and IOMMUFD_CMD_VQUEUE_ALLOC
    Introduce a new IOMMUFD_OBJ_VQUEU to represent a virtual command queue
    instance. And add a new ioctl for user space to allocate it.
    
    As an initial version, ddd IOMMU_VQUEUE_DATA_TEGRA241_CMDQV to the enum
    iommu_vqueue_data_type and the corresponding iommu_vqueue_tegra241_cmdqv
    data structure.
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>

--- drivers/iommu/iommufd/iommufd_private.h
+++ drivers/iommu/iommufd/iommufd_private.h
@@ -623,6 +623,8 @@ int iommufd_viommu_alloc_ioctl(struct iommufd_ucmd *ucmd);
 void iommufd_viommu_destroy(struct iommufd_object *obj);
 int iommufd_viommu_set_vdev_id(struct iommufd_ucmd *ucmd);
 int iommufd_viommu_unset_vdev_id(struct iommufd_ucmd *ucmd);
+int iommufd_vqueue_alloc_ioctl(struct iommufd_ucmd *ucmd);
+void iommufd_vqueue_destroy(struct iommufd_object *obj);
 
 #ifdef CONFIG_IOMMUFD_TEST
 int iommufd_test(struct iommufd_ucmd *ucmd);
