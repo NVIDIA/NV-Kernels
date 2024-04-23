c2dc6d162fdd93655f3587145631971d8bf2580c
Matthew Wilcox (Oracle) <willy@infradead.org>
2024-04-23 23:55:34 +0100

    memory-failure: remove calls to page_mapping()
    This is mostly just inlining page_mapping() into the two callers.
    
    Link: https://lkml.kernel.org/r/20240423225552.4113447-4-willy@infradead.org
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Reviewed-by: Sidhartha Kumar <sidhartha.kumar@oracle.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Acked-by: Miaohe Lin <linmiaohe@huawei.com>
    Cc: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit 89f5c54b228181713f1c00b27b360b29643cdfa6)
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Kai-Heng Feng <kaihengf@nvidia.com>
    Acked-by: Koba Ko <kobak@nvidia.com>
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -217,6 +217,7 @@ EXPORT_SYMBOL_GPL(hwpoison_filter_flags_value);
 
 static int hwpoison_filter_dev(struct page *p)
 {
+	struct folio *folio = page_folio(p);
 	struct address_space *mapping;
 	dev_t dev;
 
@@ -224,7 +225,7 @@ static int hwpoison_filter_dev(struct page *p)
 	    hwpoison_filter_dev_minor == ~0U)
 		return 0;
 
-	mapping = page_mapping(p);
+	mapping = folio_mapping(folio);
 	if (mapping == NULL || mapping->host == NULL)
 		return -EINVAL;
 
@@ -1094,7 +1095,8 @@ out:
  */
 static int me_pagecache_dirty(struct page_state *ps, struct page *p)
 {
-	struct address_space *mapping = page_mapping(p);
+	struct folio *folio = page_folio(p);
+	struct address_space *mapping = folio_mapping(folio);
 
 	SetPageError(p);
 	/* TBD: print more information about the file. */
