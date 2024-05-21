aed9791a593ef512ee035bb3f19abf680fc3f894
Andre Przywara <andre.przywara@arm.com>
2024-05-21 11:26:16 +0100

    NVIDIA: VR: SAUCE: firmware: arm_scmi: add MPAM-FB SCMI protocol stub
    The Arm MPAM Firmware-backed (Fb) Profile describes an SCMI based
    protocol to access "Memory System Components" (MSCs) in an "Memory System
    Resource Partitioning And Monitoring" (MPAM) enabled system.
    Although this SCMI protocol follows the usual protocol properties, it
    will not be described in the SCMI specifications. Also since ACPI based
    systems will need to use this MPAM-fb profile, we do not follow the
    usual way of describing each protocol function as a function in the SCMI
    framework system.
    Instead there is one generic transport function, that takes a
    preformatted buffer and transfers this to the MSC agent.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>
    Signed-off-by: James Morse <james.morse@arm.com>
    (cherry picked from commit 262ebed25a96271fbeb1fc0b3128d68203202aca https://github.com/NVIDIA/NV-Kernels 24.04_linux-nvidia-6.17-next)
    [fenghuay:
    - Update to new version APIs in scmi_mpam_protocol_init();
    ]
    Signed-off-by: Fenghua Yu <fenghuay@nvidia.com>

--- drivers/resctrl/mpam_devices.c
+++ drivers/resctrl/mpam_devices.c
@@ -2058,7 +2058,6 @@ static struct mpam_msc *do_mpam_msc_drv_probe(struct platform_device *pdev)
 	u32 tmp;
 	char name[20];
 	struct mpam_msc *msc;
-	struct resource *msc_res;
 	struct device *dev = &pdev->dev;
 
 	lockdep_assert_held(&mpam_list_lock);
@@ -2108,6 +2107,7 @@ static struct mpam_msc *do_mpam_msc_drv_probe(struct platform_device *pdev)
 
 	if (msc->iface == MPAM_IFACE_MMIO) {
 		void __iomem *io;
+		struct resource *msc_res;
 
 		io = devm_platform_get_and_ioremap_resource(pdev, 0,
 							    &msc_res);
