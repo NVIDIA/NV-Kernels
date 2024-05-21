c2ff5f855eec7812e910deb2b71636ad81cab28f
Nicolin Chen <nicolinc@nvidia.com>
2024-05-21 01:16:11 +0000

    iommu: Pass in a viommu pointer to domain_alloc_user op
    With a viommu object wrapping a potentially shareable S2 domain, a nested
    domain should be allocated by associating to a viommu instead. Driver can
    store this viommu pointer somewhere, so as to later use it calling viommu
    helpers for virtual device ID lookup and viommu invalidation.
    
    For drivers without a viommu support, keep the parent domain input, which
    should be just viommu->hwpt->common.domain otherwise.
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -3980,6 +3980,7 @@ static struct iommu_domain *intel_iommu_domain_alloc(unsigned type)
 static struct iommu_domain *
 intel_iommu_domain_alloc_user(struct device *dev, u32 flags,
 			      struct iommu_domain *parent,
+			      struct iommufd_viommu *viommu,
 			      const struct iommu_user_data *user_data)
 {
 	struct device_domain_info *info = dev_iommu_priv_get(dev);
