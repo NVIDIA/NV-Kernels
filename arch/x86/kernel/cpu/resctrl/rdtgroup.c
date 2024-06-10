a0deefb12a539ad91ced2e5ba7ec0749d7477cbf
James Morse <james.morse@arm.com>
2024-06-10 18:27:40 +0100

    x86/resctrl: resctrl_exit() teardown resctrl but leave the mount point
    resctrl_exit() was intended for use when the 'resctrl' module was unloaded.
    resctrl can't be built as a module, and the kernfs helpers are not exported
    so this is unlikely to change. MPAM has an error interrupt which indicates
    the MPAM driver has gone haywire. Should this occur tasks could run with
    the wrong control values, leading to bad performance for important tasks.
    The MPAM driver needs a way to tell resctrl that no further configuration
    should be attempted.
    
    Using resctrl_exit() for this leaves the system in a funny state as
    resctrl is still mounted, but cannot be un-mounted because the sysfs
    directory that is typically used has been removed. Dave Martin suggests
    this may cause systemd trouble in the future as not all filesystems
    can be unmounted.
    
    Add calls to remove all the files and directories in resctrl, and
    remove the sysfs_remove_mount_point() call that leaves the system
    in a funny state. When triggered, this causes all the resctrl files
    to disappear. resctrl can be unmounted, but not mounted again.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Tested-by: Carl Worth <carl@os.amperecomputing.com> # arm64
    Tested-by: Shaopeng Tan <tan.shaopeng@jp.fujitsu.com>
    Reviewed-by: Shaopeng Tan <tan.shaopeng@jp.fujitsu.com>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    ---
    Changes since v5:
     * Serialise rdtgroup_destroy_root() against umount().
     * Check rdtgroup_default.kn to protect against duplicate calls.
    (cherry picked from commit caeb88195b07ffd592b20434129d3b589663e85e https://web.git.kernel.org/pub/scm/linux/kernel/git/morse/linux.git/log/?h=mpam/snapshot/v6.14-rc1)
    Signed-off-by: Koba Ko <kobak@nvidia.com>

--- arch/x86/kernel/cpu/resctrl/rdtgroup.c
+++ arch/x86/kernel/cpu/resctrl/rdtgroup.c
@@ -4092,8 +4092,12 @@ static int rdtgroup_setup_root(struct rdt_fs_context *ctx)
 
 static void rdtgroup_destroy_root(void)
 {
-	kernfs_destroy_root(rdt_root);
-	rdtgroup_default.kn = NULL;
+	lockdep_assert_held(&rdtgroup_mutex);
+
+	if (rdtgroup_default.kn) {
+		kernfs_destroy_root(rdt_root);
+		rdtgroup_default.kn = NULL;
+	}
 }
 
 static void __init rdtgroup_setup_default(void)
@@ -4371,9 +4375,12 @@ cleanup_mountpoint:
 
 void __exit resctrl_exit(void)
 {
+	mutex_lock(&rdtgroup_mutex);
+	rdtgroup_destroy_root();
+	mutex_unlock(&rdtgroup_mutex);
+
 	debugfs_remove_recursive(debugfs_resctrl);
 	unregister_filesystem(&rdt_fs_type);
-	sysfs_remove_mount_point(fs_kobj, "resctrl");
 
 	resctrl_mon_resource_exit();
 }
