074417f6d67e852ccdd50e598ac657d46825c0e9
Shanker Donthineni <sdonthineni@nvidia.com>
2024-08-12 22:39:25 -0500

    NVIDIA: SAUCE: irqchip/gic-v3: Allow unused SGIs for drivers/modules
    BugLink: https://bugs.launchpad.net/bugs/2114230
    
    The commit 897e9e60c016 ("firmware: arm_ffa: Initial support for scheduler
    receiver interrupt") adds support for SGI interrupts in the FFA driver.
    However, the validation for SGIs in the GICv3 is too strict, causing the
    driver probe to fail.
    
    This patch relaxes the SGI validation check, allowing callers to use SGIs
    if the requested SGI number is greater than or equal to MAX_IPI, which
    fixes the TFA driver probe failure.
    
    This issue is observed on NVIDIA server platform with FFA-v1.1.
    
     PTP clock support registered
     EDAC MC: Ver: 3.0.0
     ARM FF-A: Driver version 1.1
     ARM FF-A: Firmware version 1.1 found
     GICv3: [Firmware Bug]: Illegal GSI8 translation request
     ARM FF-A: Failed to create IRQ mapping!
     ARM FF-A: Notification setup failed -61, not enabled
     ARM FF-A: Failed to register driver sched callback -95
     scmi_core: SCMI protocol bus registered
    
    This patch was sent in arm mailing list for upstream but it got
    rejected.
    
    https://patchwork.kernel.org/project/linux-arm-kernel/patch/20240813033925.925947-1-sdonthineni@nvidia.com/
    
    The proper fix requires some kind of mechanism by which a
    SGI can be requested by module but that needs discussion with arm and
    it will take time. This patch will break only if MAX_IPI value gets
    changed. This patch adds a BUILD_BUG_ON() to catch that situation.
    Once proper solution is concluded then this patch will be reverted.
    
    Signed-off-by: Shanker Donthineni <sdonthineni@nvidia.com>
    Signed-off-by: Abhishek Sahu <abhsahu@nvidia.com>
    Acked-by: Carol L Soto <csoto@nvidia.com>
    Acked-by: Jamie Nguyen <jamien@nvidia.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Signed-off--by: Brad Figg <bfigg@nvidia.com>

--- arch/arm64/include/asm/arch_gicv3.h
+++ arch/arm64/include/asm/arch_gicv3.h
@@ -188,5 +188,22 @@ static inline bool gic_has_relaxed_pmr_sync(void)
 	return cpus_have_cap(ARM64_HAS_GIC_PRIO_RELAXED_SYNC);
 }
 
+enum ipi_msg_type {
+	IPI_RESCHEDULE,
+	IPI_CALL_FUNC,
+	IPI_CPU_STOP,
+	IPI_CPU_STOP_NMI,
+	IPI_TIMER,
+	IPI_IRQ_WORK,
+	NR_IPI,
+	/*
+	 * Any enum >= NR_IPI and < MAX_IPI is special and not tracable
+	 * with trace_ipi_*
+	 */
+	IPI_CPU_BACKTRACE = NR_IPI,
+	IPI_KGDB_ROUNDUP,
+	MAX_IPI
+};
+
 #endif /* __ASSEMBLY__ */
 #endif /* __ASM_ARCH_GICV3_H */
