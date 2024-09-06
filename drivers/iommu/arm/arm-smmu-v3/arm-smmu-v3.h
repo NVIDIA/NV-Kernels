dc151cb608ccb1f830f71b3ec917252131018638
Jason Gunthorpe <jgg@nvidia.com>
2024-09-06 12:47:49 -0300

    iommu/arm-smmu-v3: Add types for each level of the 2 level stream table
    Add types struct arm_smmu_strtab_l1 and l2 to represent the HW layout of
    the descriptors, and use them in most places, following patches will get
    the remaing places. The size of the l1 and l2 HW allocations are
    sizeof(struct arm_smmu_strtab_l1/2).
    
    This provides some more clarity than having raw __le64 *'s and sizes
    computed via macros.
    
    Remove STRTAB_L1_DESC_DWORDS.
    
    Tested-by: Nicolin Chen <nicolinc@nvidia.com>
    Reviewed-by: Nicolin Chen <nicolinc@nvidia.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    Link: https://lore.kernel.org/r/2-v4-6416877274e1+1af-smmuv3_tidy_jgg@nvidia.com
    Signed-off-by: Will Deacon <will@kernel.org>
    (cherry picked from commit abb4f9d323a8d53870cc842d3c5024f71c2d4951
    linux)
    Signed-off-by: Koba Ko <kobak@nvidia.com>

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.h
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.h
@@ -204,7 +204,6 @@
  */
 #define STRTAB_SPLIT			8
 
-#define STRTAB_L1_DESC_DWORDS		1
 #define STRTAB_L1_DESC_SPAN		GENMASK_ULL(4, 0)
 #define STRTAB_L1_DESC_L2PTR_MASK	GENMASK_ULL(51, 6)
 
@@ -215,6 +214,13 @@ struct arm_smmu_ste {
 };
 
 #define STRTAB_NUM_L2_STES		(1 << STRTAB_SPLIT)
+struct arm_smmu_strtab_l2 {
+	struct arm_smmu_ste stes[STRTAB_NUM_L2_STES];
+};
+
+struct arm_smmu_strtab_l1 {
+	__le64 l2ptr;
+};
 #define STRTAB_MAX_L1_ENTRIES		(1 << 17)
 
 static inline u32 arm_smmu_strtab_l1_idx(u32 sid)
@@ -597,7 +603,7 @@ struct arm_smmu_priq {
 
 /* High-level stream table and context descriptor structures */
 struct arm_smmu_strtab_l1_desc {
-	struct arm_smmu_ste		*l2ptr;
+	struct arm_smmu_strtab_l2	*l2ptr;
 };
 
 struct arm_smmu_ctx_desc {
