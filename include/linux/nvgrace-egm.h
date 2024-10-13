78903d032932d9adacf01efd27a3123c42bdec9a
Ankit Agrawal <ankita@nvidia.com>
2024-10-13 04:53:38 +0000

    NVIDIA: SAUCE: vfio/nvgrace-egm: Move the egm header file to include
    BugLink: https://bugs.launchpad.net/bugs/2119656
    
    nvgrace-egm exposes the API register_egm_node & unregister_egm_node
    to manage EGM (Extended GPU Memory) present on the system.
    
    To allow out-of-tree driver such as nvidia-vgpu-vfio make use of them,
    move the declaration to a new nvgrace-egm.h in include.
    
    Signed-off-by: Ankit Agrawal <ankita@nvidia.com>
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Kai-Heng Feng <kaihengf@nvidia.com>
    Acked-by: Koba Ko <kobak@nvidia.com>
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>
    (cherry picked from commit bed340f2023f22192893e9121834ee3ce252edd1 https://github.com/NVIDIA/NV-Kernels/tree/24.04_linux-nvidia-adv-6.8-next)
    Signed-off-by: Koba Ko <kobak@nvidia.com>
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Carol L. Soto <csoto@nvidia.com>
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>
    (cherry picked from commit a9616639ce81799f5b3133c47c25f8d875728f4f https://github.com/NVIDIA/NV-Kernels/tree/24.04_linux-nvidia-adv-6.11-next)
    Signed-off-by: Nirmoy Das <nirmoyd@nvidia.com>
    Acked-by: Carol L Soto <csoto@nvidia.com>
    Acked-by: Matt Ochs <mochs@nvidia.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Signed-off--by: Brad Figg <bfigg@nvidia.com>

--- include/linux/nvgrace-egm.h
+++ include/linux/nvgrace-egm.h
@@ -1,12 +1,12 @@
-// SPDX-License-Identifier: GPL-2.0-only
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 /*
  * Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved
  */
 
-#ifndef NVGRACE_EGM_H
-#define NVGRACE_EGM_H
+#ifndef _NVGRACE_EGM_H
+#define _NVGRACE_EGM_H
 
 int register_egm_node(struct pci_dev *pdev);
 void unregister_egm_node(int egm_node);
 
-#endif /* NVGRACE_EGM_H */
+#endif /* _NVGRACE_EGM_H */
