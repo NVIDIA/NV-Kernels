bf614ae4ab07da8662aaf31a64a9ec4785ea2621
Jason Gunthorpe <jgg@ziepe.ca>
2024-10-28 09:38:00 +0000

    iommu: Introduce iommu_paging_domain_alloc_flags()
    Currently drivers calls iommu_paging_domain_alloc(dev) to get an
    UNMANAGED domain. This is not sufficient to support PASID with
    UNMANAGED domain as some HW like AMD requires certain page table type
    to support PASIDs.
    
    Also the domain_alloc_paging op only passes device as param for domain
    allocation. This is not sufficient for AMD driver to decide the right
    page table.
    
    Instead of extending ops->domain_alloc_paging() it was decided to
    enhance ops->domain_alloc_user() so that caller can pass various
    additional flags.
    
    Hence add iommu_paging_domain_alloc_flags() API which takes flags as
    parameter. Caller can pass additional parameter to indicate type of
    domain required, etc. iommu_paging_domain_alloc_flags() internally calls
    appropriate callback function to allocate a domain.
    
    Signed-off-by: Jason Gunthorpe <jgg@ziepe.ca>
    [Added description - Vasant]
    Signed-off-by: Vasant Hegde <vasant.hegde@amd.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Yi Liu <yi.l.liu@intel.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Link: https://lore.kernel.org/r/20241028093810.5901-3-vasant.hegde@amd.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    (backported from commit 20858d4ebb423826b7a978d54cde195f51d87e20)
    [mochs: minor context cleanup]
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -559,8 +559,6 @@ iommu_copy_struct_from_full_user_array(void *kdst, size_t kdst_entry_size,
  *                the caller iommu_domain_alloc() returns.
  * @domain_alloc_user: Allocate an iommu domain corresponding to the input
  *                     parameters as defined in include/uapi/linux/iommufd.h.
- *                     Unlike @domain_alloc, it is called only by IOMMUFD and
- *                     must fully initialize the new domain before return.
  *                     Upon success, if the @user_data is valid and the @parent
  *                     points to a kernel-managed domain, the new domain must be
  *                     IOMMU_DOMAIN_NESTED type; otherwise, the @parent must be
@@ -855,9 +853,13 @@ static inline void iommu_iotlb_gather_init(struct iommu_iotlb_gather *gather)
 extern int bus_iommu_probe(const struct bus_type *bus);
 extern bool device_iommu_capable(struct device *dev, enum iommu_cap cap);
 extern bool iommu_group_has_isolated_msi(struct iommu_group *group);
-struct iommu_domain *iommu_paging_domain_alloc(struct device *dev);
 struct iommu_group *iommu_group_get_from_kobj(struct kobject *group_kobj);
 extern struct iommu_group *iommu_group_get_by_id(int id);
+struct iommu_domain *iommu_paging_domain_alloc_flags(struct device *dev, unsigned int flags);
+static inline struct iommu_domain *iommu_paging_domain_alloc(struct device *dev)
+{
+	return iommu_paging_domain_alloc_flags(dev, 0);
+}
 extern void iommu_domain_free(struct iommu_domain *domain);
 extern int iommu_attach_device(struct iommu_domain *domain,
 			       struct device *dev);
@@ -1159,6 +1161,12 @@ struct iommu_dirty_ops {};
 static inline bool device_iommu_capable(struct device *dev, enum iommu_cap cap)
 {
 	return false;
+}
+
+struct iommu_domain *iommu_paging_domain_alloc_flags(struct device *dev,
+						     unsigned int flags)
+{
+	return ERR_PTR(-ENODEV);
 }
 
 static inline struct iommu_domain *iommu_paging_domain_alloc(struct device *dev)
