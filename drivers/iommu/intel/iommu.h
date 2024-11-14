a3bbfcc2afc23faca871eecc67bceccd69c2db7d
Jason Gunthorpe <jgg@nvidia.com>
2024-11-14 15:55:30 -0400

    iommu: Add ops->domain_alloc_nested()
    It turns out all the drivers that are using this immediately call into
    another function, so just make that function directly into the op. This
    makes paging=NULL for domain_alloc_user and we can remove the argument in
    the next patch.
    
    The function mirrors the similar op in the viommu that allocates a nested
    domain on top of the viommu's nesting parent. This version supports cases
    where a viommu is not being used.
    
    Link: https://patch.msgid.link/r/1-v1-c252ebdeb57b+329-iommu_paging_flags_jgg@nvidia.com
    Reviewed-by: Lu Baolu <baolu.lu@linux.intel.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    (backported from commit 64214c2b95364d26cdff045d8bbefd37380edbe1)
    [mochs: minor context cleanup]
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>

--- drivers/iommu/intel/iommu.h
+++ drivers/iommu/intel/iommu.h
@@ -1115,8 +1115,10 @@ void domain_update_iommu_cap(struct dmar_domain *domain);
 int dmar_ir_support(void);
 
 void iommu_flush_write_buffer(struct intel_iommu *iommu);
-struct iommu_domain *intel_nested_domain_alloc(struct iommu_domain *parent,
-					       const struct iommu_user_data *user_data);
+struct iommu_domain *
+intel_iommu_domain_alloc_nested(struct device *dev, struct iommu_domain *parent,
+				u32 flags,
+				const struct iommu_user_data *user_data);
 struct device *device_rbtree_find(struct intel_iommu *iommu, u16 rid);
 
 enum cache_tag_type {
