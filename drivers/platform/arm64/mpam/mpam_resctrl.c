8c6c0c46dd7819f4a2c62f067ed1ef23e7cc5836
James Morse <james.morse@arm.com>
2024-11-18 18:45:50 +0000

    fs/resctrl: Group all the MBA specific properties in a separate struct
    struct resctrl_membw combines parameters that are related to the control
    value, and parameters that are specific to the MBA resource.
    
    To allow the control value parsing and management code to be re-used for
    other resources, it needs to be separated from the MBA resource.
    
    Add struct resctrl_mba that holds all the parameters that are specific
    to the MBA resource.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    (cherry picked from commit 77573d5b8a88c78e3956531a3443d19f37f6fbc8 https://web.git.kernel.org/pub/scm/linux/kernel/git/morse/linux.git/log/?h=mpam/snapshot%2bextras/v6.14-rc1)
    Signed-off-by: Koba Ko <kobak@nvidia.com>

--- drivers/platform/arm64/mpam/mpam_resctrl.c
+++ drivers/platform/arm64/mpam/mpam_resctrl.c
@@ -1033,8 +1033,8 @@ static int mpam_resctrl_control_init(struct mpam_resctrl_res *res,
 		r->schema_fmt = RESCTRL_SCHEMA_RANGE;
 		r->ctrl_scope = RESCTRL_L3_CACHE;
 
-		r->membw.delay_linear = true;
-		r->membw.throttle_mode = THREAD_THROTTLE_UNDEFINED;
+		r->mba.delay_linear = true;
+		r->mba.throttle_mode = THREAD_THROTTLE_UNDEFINED;
 		r->membw.min_bw = get_mba_granularity(cprops);
 		r->membw.max_bw = MAX_MBA_BW;
 		r->membw.bw_gran = get_mba_granularity(cprops);
