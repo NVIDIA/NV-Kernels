486ccff29755bf78ad80b4733497d6cea8c15693
James Morse <james.morse@arm.com>
2024-11-20 12:21:25 +0000

    NVIDIA: SAUCE: fs/resctrl: Add a schema format to the schema, allowing it to be different
    BugLink: https://bugs.launchpad.net/bugs/2154527
    
    parse_bw() doesn't bother checking the bounds it is given if the
    resource is in use by mba_sc. This is because the values parsed from
    user-space are not the same as those the control should take.
    To make this disparity easier to work with, a second different copy
    of the schema format is needed, which would need a version of
    resctrl_get_default_ctrl(). This would let the resctrl change the
    schema format presented to user-space, provided it converts it to match
    what the architecture code expects.
    Add a second schema format for use with mba_sc. The membw properties
    are copied and the schema version is used. When mba_sc is enabled
    the schema copy of these properties is modified.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    (cherry picked from commit 225d28eb849877c6b97dcdc466d8e1aa67978272 https://github.com/NVIDIA/NV-Kernels 24.04_linux-nvidia-6.17-next)
    [fenghuay:
      - Resolve minor conflicts in `fs/resctrl/ctrlmondata.c`;
      - Resolve minor conflicts in `include/linux/arm_mpam.h`;
    ]
    Signed-off-by: Fenghua Yu <fenghuay@nvidia.com>
    Acked-by: Jamie Nguyen <jamien@nvidia.com>
    Acked-by: Carol L Soto <csoto@nvidia.com>
    Signed-off-by: Brad Figg <bfigg@nvidia.com>

--- include/linux/arm_mpam.h
+++ include/linux/arm_mpam.h
@@ -79,10 +79,8 @@ static inline void resctrl_arch_enable_alloc(void) { }
 static inline void resctrl_arch_disable_alloc(void) { }
 
 struct resctrl_schema;
-
-struct rdt_resource;
 static inline u32 resctrl_arch_round_bw(u32 val,
-					const struct rdt_resource *r __always_unused)
+					const struct resctrl_schema *s __always_unused)
 {
 	/*
 	 * Do nothing: for MPAM, resctrl_arch_update_one() has the necessary
