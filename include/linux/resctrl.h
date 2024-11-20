1db7f4a7a00482d9d792ddaec7c6b4bd354a80f5
James Morse <james.morse@arm.com>
2024-11-20 15:15:54 +0000

    NVIDIA: SAUCE: fs/resctrl: Add specific schema types for 'range'
    BugLink: https://bugs.launchpad.net/bugs/2122432
    
    Resctrl allows the architecture code to specify the schema format for a
    control. Controls can either take a bitmap, or some kind of number.
    If user-space doesn't know what a control is by its name, it could be
    told the schema format. 'Some kind of number' isn't useful as the
    difference between a percentage and a value in MB/s affects how these
    would be programmed, even if resctrl's parsing code doesn't need to
    care.
    Add the types resctrl already has in addition to 'range'. This
    allows architectures to move over before 'range' is removed. These
    new schema formats are parsed the same, but will additionally affect
    which files are visible.
    Schema formats with a double underscore should not be considered
    portable between architectures, and are likely to be described to
    user-space as 'platform defined'. AMDs MBA resource is configured
    with an absolute bandwidth measured in multiples of one eighth of
    a GB per second. resctrl needs to be aware of this platform
    defined format to ensure the existing 'MB' files continue to be
    shown.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    (cherry picked from commit 50eed7f84b62a3a4f539c579035ab72ec150cf19 https://git.kernel.org/pub/scm/linux/kernel/git/morse/linux.git)
    Signed-off-by: Fenghua Yu <fenghuay@nvidia.com>
    Acked-by: Jamie Nguyen <jamien@nvidia.com>
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>

--- include/linux/resctrl.h
+++ include/linux/resctrl.h
@@ -271,10 +271,16 @@ enum resctrl_scope {
  * enum resctrl_schema_fmt - The format user-space provides for a schema.
  * @RESCTRL_SCHEMA_BITMAP:	The schema is a bitmap in hex.
  * @RESCTRL_SCHEMA_RANGE:	The schema is a decimal number.
+ * @RESCTRL_SCHEMA_PERCENT:	The schema is a percentage.
+ * @RESCTRL_SCHEMA_MBPS:	The schema ia a MBps value.
+ * @RESCTRL_SCHEMA__AMD_MBA:	The schema value is MBA for AMD platforms.
  */
 enum resctrl_schema_fmt {
 	RESCTRL_SCHEMA_BITMAP,
 	RESCTRL_SCHEMA_RANGE,
+	RESCTRL_SCHEMA_PERCENT,
+	RESCTRL_SCHEMA_MBPS,
+	RESCTRL_SCHEMA__AMD_MBA,
 };
 
 /**
@@ -406,6 +412,9 @@ static inline u32 resctrl_get_resource_default_ctrl(struct rdt_resource *r)
 	case RESCTRL_SCHEMA_BITMAP:
 		return BIT_MASK(r->cache.cbm_len) - 1;
 	case RESCTRL_SCHEMA_RANGE:
+	case RESCTRL_SCHEMA_PERCENT:
+	case RESCTRL_SCHEMA_MBPS:
+	case RESCTRL_SCHEMA__AMD_MBA:
 		return r->membw.max_bw;
 	}
 
@@ -423,6 +432,9 @@ static inline u32 resctrl_get_schema_default_ctrl(struct resctrl_schema *s)
 	case RESCTRL_SCHEMA_BITMAP:
 		return resctrl_get_resource_default_ctrl(s->res);
 	case RESCTRL_SCHEMA_RANGE:
+	case RESCTRL_SCHEMA_PERCENT:
+	case RESCTRL_SCHEMA_MBPS:
+	case RESCTRL_SCHEMA__AMD_MBA:
 		return s->membw.max_bw;
 	}
 
