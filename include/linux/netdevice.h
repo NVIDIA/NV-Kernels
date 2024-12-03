c2f174680bc184fef3b18e1480ff41a563d47a40
Toke Høiland-Jørgensen <toke@redhat.com>
2024-12-03 18:37:29 +0100

    xdp: register system page pool as an XDP memory model
    BugLink: https://bugs.launchpad.net/bugs/2155660
    
    [ Upstream commit e77d9aee951341119be16a991fcfc76d1154d22a ]
    
    To make the system page pool usable as a source for allocating XDP
    frames, we need to register it with xdp_reg_mem_model(), so that page
    return works correctly. This is done in preparation for using the system
    page_pool to convert XDP_PASS XSk frames to skbs; for the same reason,
    make the per-cpu variable non-static so we can access it from other
    source files as well (but w/o exporting).
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>
    Signed-off-by: Alexander Lobakin <aleksander.lobakin@intel.com>
    Link: https://patch.msgid.link/20241203173733.3181246-7-aleksander.lobakin@intel.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Stable-dep-of: 6f1a9140ecda ("net: add xmit recursion limit to tunnel xmit functions")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    [cremfuelled: adjust context]
    Signed-off-by: Alice C. Munduruca <alice.munduruca@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -3351,6 +3351,7 @@ static inline void input_queue_tail_incr_save(struct softnet_data *sd,
 }
 
 DECLARE_PER_CPU_ALIGNED(struct softnet_data, softnet_data);
+DECLARE_PER_CPU(struct page_pool *, system_page_pool);
 
 static inline int dev_recursion_level(void)
 {
