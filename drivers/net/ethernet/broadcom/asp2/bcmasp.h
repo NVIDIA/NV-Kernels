e57cfd7faf78bcdd2261089a6c89e0c6ac92fe76
Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
2025-01-14 16:50:57 +0000

    net: bcm: asp2: convert to phylib managed EEE
    BugLink: https://bugs.launchpad.net/bugs/2155660
    
    [ Upstream commit 21f56ad1b21131eb2c9c16e11ccb28f77b5addc4 ]
    
    Convert the Broadcom ASP2 driver to use phylib managed EEE support.
    
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
    Reviewed-by: Florian Fainelli <florian.fainelli@broadcom.com>
    Tested-by: Florian Fainelli <florian.fainelli@broadcom.com>
    Link: https://patch.msgid.link/E1tXk81-000r4x-TS@rmk-PC.armlinux.org.uk
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Stable-dep-of: cbfa5be2bf64 ("net: bcmasp: fix double free of WoL irq")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Alice C. Munduruca <alice.munduruca@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- drivers/net/ethernet/broadcom/asp2/bcmasp.h
+++ drivers/net/ethernet/broadcom/asp2/bcmasp.h
@@ -345,8 +345,6 @@ struct bcmasp_intf {
 	/* Used if per intf wol irq */
 	int				wol_irq;
 	unsigned int			wol_irq_enabled:1;
-
-	struct ethtool_eee		eee;
 };
 
 #define NUM_NET_FILTERS				32
@@ -596,5 +594,4 @@ int bcmasp_netfilt_get_all_active(struct bcmasp_intf *intf, u32 *rule_locs,
 
 void bcmasp_netfilt_suspend(struct bcmasp_intf *intf);
 
-void bcmasp_eee_enable_set(struct bcmasp_intf *intf, bool enable);
 #endif
