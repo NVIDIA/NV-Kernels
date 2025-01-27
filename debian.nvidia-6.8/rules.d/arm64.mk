ed72b95f0e6a8ea629968b1b3e7931e3f3550f7d
Jacob Martin <jacob.martin@canonical.com>
2025-01-27 16:26:42 -0600

    UBUNTU: [Packaging] nvidia-6.8: Do not build perf with CORESIGHT=1
    BugLink: https://bugs.launchpad.net/bugs/2093957
    
    The version of libopencsd-dev in Jammy is not new enough to support
    building perf with CoreSight support. Disable it on this backport
    kernel.
    
    Signed-off-by: Jacob Martin <jacob.martin@canonical.com>

--- debian.nvidia-6.8/rules.d/arm64.mk
+++ debian.nvidia-6.8/rules.d/arm64.mk
@@ -16,6 +16,7 @@ do_tools_cpupower = true
 do_tools_perf   = true
 do_tools_perf_jvmti = true
 do_tools_perf_python = true
+do_tools_perf_coresight = false
 do_tools_bpftool = true
 do_tools_rtla = false
 
