01d6280f660d9daadde587fa91ac2ddd7d4e3adf
Jianbo Liu <jianbol@nvidia.com>
2025-02-03 23:35:07 +0200

    net/mlx5: Add devcom component for the clock shared by functions
    BugLink: https://bugs.launchpad.net/bugs/2124107
    
    Add new devcom component for hardware clock. When it is running in
    real time mode, the functions are grouped by the identify they query.
    
    According to firmware document, the clock identify size is 64 bits, so
    it's safe to memcpy to component key, as the key size is also 64 bits.
    
    Signed-off-by: Jianbo Liu <jianbol@nvidia.com>
    Reviewed-by: Carolina Jubran <cjubran@nvidia.com>
    Reviewed-by: Dragos Tatulea <dtatulea@nvidia.com>
    Signed-off-by: Tariq Toukan <tariqt@nvidia.com>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    (cherry picked from commit 574998cf3b3f59afa9e3a6bbb609d9d4eb2023b4)
    Signed-off-by: Benjamin Poirier <bpoirier@nvidia.com>
    Acked-by: Valentine Fatiev <valentinef@nvidia.com>
    Acked-by: Aya Levin <ayal@nvidia.com>
    Tested-by: Rola Dabbah <rdabbah@nvidia.com>
    Tested-by: Shmuel Gutman <sgutman@nvidia.com>

--- include/linux/mlx5/driver.h
+++ include/linux/mlx5/driver.h
@@ -680,6 +680,7 @@ struct mlx5_rsvd_gids {
 };
 
 struct mlx5_clock;
+struct mlx5_clock_dev_state;
 struct mlx5_dm;
 struct mlx5_fw_tracer;
 struct mlx5_vxlan;
@@ -764,6 +765,7 @@ struct mlx5_core_dev {
 	struct mlx5_fpga_device *fpga;
 #endif
 	struct mlx5_clock       *clock;
+	struct mlx5_clock_dev_state *clock_state;
 	struct mlx5_ib_clock_info  *clock_info;
 	struct mlx5_fw_tracer   *tracer;
 	struct mlx5_rsc_dump    *rsc_dump;
