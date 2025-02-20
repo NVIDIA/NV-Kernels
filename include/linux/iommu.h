788cb56e7fdaa978b11d06f63a9218d259aa2288
Jason Gunthorpe <jgg@nvidia.com>
2025-02-19 17:31:37 -0800

    genirq/msi: Refactor iommu_dma_compose_msi_msg()
    BugLink: https://bugs.launchpad.net/bugs/2119656
    
    The two-step process to translate the MSI address involves two functions,
    iommu_dma_prepare_msi() and iommu_dma_compose_msi_msg().
    
    Previously iommu_dma_compose_msi_msg() needed to be in the iommu layer as
    it had to dereference the opaque cookie pointer. Now, the previous patch
    changed the cookie pointer into an integer so there is no longer any need
    for the iommu layer to be involved.
    
    Further, the call sites of iommu_dma_compose_msi_msg() all follow the same
    pattern of setting an MSI message address_hi/lo to non-translated and then
    immediately calling iommu_dma_compose_msi_msg().
    
    Refactor iommu_dma_compose_msi_msg() into msi_msg_set_addr() that directly
    accepts the u64 version of the address and simplifies all the callers.
    
    Move the new helper to linux/msi.h since it has nothing to do with iommu.
    
    Aside from refactoring, this logically prepares for the next patch, which
    allows multiple implementation options for iommu_dma_prepare_msi(). So, it
    does not make sense to have the iommu_dma_compose_msi_msg() in dma-iommu.c
    as it no longer provides the only iommu_dma_prepare_msi() implementation.
    
    Link: https://patch.msgid.link/r/eda62a9bafa825e9cdabd7ddc61ad5a21c32af24.1740014950.git.nicolinc@nvidia.com
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    (cherry picked from commit 9349887e93009331e751854843f73a086bef4018)
    Signed-off-by: Nirmoy Das <nirmoyd@nvidia.com>
    Acked-by: Carol L Soto <csoto@nvidia.com>
    Acked-by: Matt Ochs <mochs@nvidia.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Signed-off--by: Brad Figg <bfigg@nvidia.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -1510,7 +1510,6 @@ static inline void iommu_debugfs_setup(void) {}
 int iommu_get_msi_cookie(struct iommu_domain *domain, dma_addr_t base);
 
 int iommu_dma_prepare_msi(struct msi_desc *desc, phys_addr_t msi_addr);
-void iommu_dma_compose_msi_msg(struct msi_desc *desc, struct msi_msg *msg);
 
 #else /* CONFIG_IOMMU_DMA */
 
@@ -1526,11 +1525,6 @@ static inline int iommu_dma_prepare_msi(struct msi_desc *desc, phys_addr_t msi_a
 {
 	return 0;
 }
-
-static inline void iommu_dma_compose_msi_msg(struct msi_desc *desc, struct msi_msg *msg)
-{
-}
-
 #endif	/* CONFIG_IOMMU_DMA */
 
 /*
