f229ae81664c2fa02765eb5baf335cda8f0dbace
Abhishek Sahu <abhsahu@nvidia.com>
2025-04-24 10:27:44 +0000

    NVIDIA: SAUCE: Bluetooth: btusb: Add new VID/PID 13d3/3630
    This PID will be used by NVIDIA DGX Spark GB10 Grace Blackwell chip.
    
    Signed-off-by: Abhishek Sahu <abhsahu@nvidia.com>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -748,6 +748,9 @@ static const struct usb_device_id quirks_table[] = {
 	{ USB_DEVICE(0x13d3, 0x3609), .driver_info = BTUSB_MEDIATEK |
 						     BTUSB_WIDEBAND_SPEECH |
 						     BTUSB_VALID_LE_STATES },
+	{ USB_DEVICE(0x13d3, 0x3630), .driver_info = BTUSB_MEDIATEK |
+						     BTUSB_WIDEBAND_SPEECH |
+						     BTUSB_VALID_LE_STATES },
 
 	/* Additional Realtek 8723AE Bluetooth devices */
 	{ USB_DEVICE(0x0930, 0x021d), .driver_info = BTUSB_REALTEK },
