80df2e9839307e05339fdb1d131f17223c88f04a
Tushar Dave <tdave@nvidia.com>
2025-05-19 18:19:37 -0700

    iommu: Skip PASID validation for devices without PASID capability
    BugLink: https://bugs.launchpad.net/bugs/2100340
    
    Generally PASID support requires ACS settings that usually create
    single device groups, but there are some niche cases where we can get
    multi-device groups and still have working PASID support. The primary
    issue is that PCI switches are not required to treat PASID tagged TLPs
    specially so appropriate ACS settings are required to route all TLPs to
    the host bridge if PASID is going to work properly.
    
    pci_enable_pasid() does check that each device that will use PASID has
    the proper ACS settings to achieve this routing.
    
    However, no-PASID devices can be combined with PASID capable devices
    within the same topology using non-uniform ACS settings. In this case
    the no-PASID devices may not have strict route to host ACS flags and
    end up being grouped with the PASID devices.
    
    This configuration fails to allow use of the PASID within the iommu
    core code which wrongly checks if the no-PASID device supports PASID.
    
    Fix this by ignoring no-PASID devices during the PASID validation. They
    will never issue a PASID TLP anyhow so they can be ignored.
    
    Fixes: c404f55c26fc ("iommu: Validate the PASID in iommu_attach_device_pasid()")
    Cc: stable@vger.kernel.org
    (backported from commit b3f6fcd8404f9f92262303369bb877ec5d188a81)
    [old domain doesn't exist in earlier kernel - tdave]
    Signed-off-by: Tushar Dave <tdave@nvidia.com>
    Reviewed-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Vasant Hegde <vasant.hegde@amd.com>
    Link: https://lore.kernel.org/r/20250520011937.3230557-1-tdave@nvidia.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Acked-by: Jamie Nguyen <jamien@nvidia.com>
    Acked-by: Carol L Soto <csoto@nvidia.com>
    Acked-by: Abdur Rahman <abdur.rahman@canonical.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Signed-off--by: Brad Figg <bfigg@nvidia.com>

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -3337,10 +3337,12 @@ static int __iommu_set_group_pasid(struct iommu_domain *domain,
 	int ret;
 
 	for_each_group_device(group, device) {
-		ret = domain->ops->set_dev_pasid(domain, device->dev,
-						 pasid, NULL);
-		if (ret)
-			goto err_revert;
+		if (device->dev->iommu->max_pasids > 0) {
+			ret = domain->ops->set_dev_pasid(domain, device->dev,
+							 pasid, NULL);
+			if (ret)
+				goto err_revert;
+		}
 	}
 
 	return 0;
@@ -3350,7 +3352,8 @@ err_revert:
 	for_each_group_device(group, device) {
 		if (device == last_gdev)
 			break;
-		iommu_remove_dev_pasid(device->dev, pasid, domain);
+		if (device->dev->iommu->max_pasids > 0)
+			iommu_remove_dev_pasid(device->dev, pasid, domain);
 	}
 	return ret;
 }
@@ -3361,8 +3364,10 @@ static void __iommu_remove_group_pasid(struct iommu_group *group,
 {
 	struct group_device *device;
 
-	for_each_group_device(group, device)
-		iommu_remove_dev_pasid(device->dev, pasid, domain);
+	for_each_group_device(group, device) {
+		if (device->dev->iommu->max_pasids > 0)
+			iommu_remove_dev_pasid(device->dev, pasid, domain);
+	}
 }
 
 /*
@@ -3399,7 +3404,13 @@ int iommu_attach_device_pasid(struct iommu_domain *domain,
 
 	mutex_lock(&group->mutex);
 	for_each_group_device(group, device) {
-		if (pasid >= device->dev->iommu->max_pasids) {
+		/*
+		 * Skip PASID validation for devices without PASID support
+		 * (max_pasids = 0). These devices cannot issue transactions
+		 * with PASID, so they don't affect group's PASID usage.
+		 */
+		if ((device->dev->iommu->max_pasids > 0) &&
+		    (pasid >= device->dev->iommu->max_pasids)) {
 			ret = -EINVAL;
 			goto out_unlock;
 		}
