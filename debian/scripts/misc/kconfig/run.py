7684608a2ff27146c7f4d834bc814bb361b05928
Mehmet Basaran <mehmet.basaran@canonical.com>
2025-06-16 09:28:31 +0300

    UBUNTU: [Packaging] update annotations scripts
    BugLink: https://bugs.launchpad.net/bugs/1786013
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- debian/scripts/misc/kconfig/run.py
+++ debian/scripts/misc/kconfig/run.py
@@ -2,11 +2,11 @@
 # Manage Ubuntu kernel .config and annotations
 # Copyright © 2022 Canonical Ltd.
 
-import sys
-import os
 import argparse
 import json
-from signal import signal, SIGPIPE, SIG_DFL
+import os
+import sys
+from signal import SIG_DFL, SIGPIPE, signal
 
 try:
     from argcomplete import autocomplete
@@ -17,9 +17,8 @@ except ModuleNotFoundError:
 
 
 from kconfig.annotations import Annotation, KConfig  # noqa: E402 Import not at top of file
-from kconfig.utils import autodetect_annotations, arg_fail  # noqa: E402 Import not at top of file
-from kconfig.version import VERSION, ANNOTATIONS_FORMAT_VERSION  # noqa: E402 Import not at top of file
-
+from kconfig.utils import arg_fail, autodetect_annotations  # noqa: E402 Import not at top of file
+from kconfig.version import ANNOTATIONS_FORMAT_VERSION, VERSION  # noqa: E402 Import not at top of file
 
 SKIP_CONFIGS = (
     # CONFIG_VERSION_SIGNATURE is dynamically set during the build
