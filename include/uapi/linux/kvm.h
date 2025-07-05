2923ecbaeeb89f98c2cfa61c2d3d60e900776392
Ankit Agrawal <ankita@nvidia.com>
2025-07-05 07:17:17 +0000

    KVM: arm64: Expose new KVM cap for cacheable PFNMAP
    BugLink: https://bugs.launchpad.net/bugs/2116207
    
    Introduce a new KVM capability to expose to the userspace whether
    cacheable mapping of PFNMAP is supported.
    
    The ability to safely do the cacheable mapping of PFNMAP is contingent
    on S2FWB and ARM64_HAS_CACHE_DIC. S2FWB allows KVM to avoid flushing
    the D cache, ARM64_HAS_CACHE_DIC allows KVM to avoid flushing the icache
    and turns icache_inval_pou() into a NOP. The cap would be false if
    those requirements are missing and is checked by making use of
    kvm_arch_supports_cacheable_pfnmap.
    
    This capability would allow userspace to discover the support.
    It could for instance be used by userspace to prevent live-migration
    across FWB and non-FWB hosts.
    
    CC: Catalin Marinas <catalin.marinas@arm.com>
    CC: Jason Gunthorpe <jgg@nvidia.com>
    CC: Oliver Upton <oliver.upton@linux.dev>
    CC: David Hildenbrand <david@redhat.com>
    Suggested-by: Marc Zyngier <maz@kernel.org>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Tested-by: Donald Dutile <ddutile@redhat.com>
    Signed-off-by: Ankit Agrawal <ankita@nvidia.com>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Link: https://lore.kernel.org/r/20250705071717.5062-7-ankita@nvidia.com
    Signed-off-by: Oliver Upton <oliver.upton@linux.dev>
    (backported from commit f55ce5a6cd33211c8cc5bce0554b6ac710a6a28b linux-next)
    [mochs: minor context conflicts in docs and the header file]
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Carol L Soto <csoto@nvidia.com>
    Acked-by: Jamie Nguyen <jamien@nvidia.com>
    Acked-by: Nirmoy Das <nirmoyd@nvidia.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Signed-off--by: Brad Figg <bfigg@nvidia.com>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -929,6 +929,7 @@ struct kvm_enable_cap {
 #define KVM_CAP_PRE_FAULT_MEMORY 236
 #define KVM_CAP_X86_APIC_BUS_CYCLES_NS 237
 #define KVM_CAP_X86_GUEST_MODE 238
+#define KVM_CAP_ARM_CACHEABLE_PFNMAP_SUPPORTED 243
 
 struct kvm_irq_routing_irqchip {
 	__u32 irqchip;
