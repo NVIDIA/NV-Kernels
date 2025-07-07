4fd13d30e9e4d6dd5b3aa8abdd0451fd9ae21de3
Jason Gunthorpe <jgg@nvidia.com>
2025-07-07 11:23:44 -0300

    NVIDIA: SAUCE: PCI: Add pci_mfd_isolation()
    BugLink: https://bugs.launchpad.net/bugs/2116967
    
    Introduce a new ACS related quirk that indicates a single function in a
    MFD is isolated from other functions in the MFD. In otherwords the
    function has no internal loopback to other functions.
    
    This is different from the existing ACS quirk since ACS only indicates if
    the function's egress can reach other functions in the MFD for P2P, while
    leaving the ingress question up to the ACS of the other
    functions. pci_mfd_isolation() reports both the ingress and egress
    behavior together, if both are blocked then it can return true.
    
    This quirk matches the historical behavior of the
    pci_dev_specific_acs_enabled() quirk that would allow 'adding' ACS to a
    single function to force that function into its own iommu_group. Many
    quirks were added on this assumption to give specific functions their own
    IOMMU groups. For example some Intel systems have a NIC in a MFD with
    other functions and no ACS capabilities. The NIC has been quirked, while
    the rest of the MFD functions are unquirked.
    
    Add an internal flag, PCI_ACS_QUIRK_ACS_ISOLATED, in the upper 16 bits of
    the acs_flags. If the quirk implementation sees the flag and decides the
    function should be isolated, then it can return success. The additional
    flag automatically makes the existing quirks all return failure as it is
    never masked off.
    
    Have pci_quirk_mf_endpoint_acs() support the flag with some expectation
    this will grown down the road.
    
    Apply the MFD isolation override to a general case of host bridges inside
    MFDs that match some AMD systems:
    
    00:07.0 Host bridge: Advanced Micro Devices, Inc. [AMD] Starship/Matisse PCIe Dummy Host Bridge
            Control: I/O- Mem- BusMaster- SpecCycle- MemWINV- VGASnoop- ParErr- Stepping- SERR- FastB2B- DisINTx-
            Status: Cap- 66MHz- UDF- FastB2B- ParErr- DEVSEL=fast >TAbort- <TAbort- <MAbort- >SERR- <PERR- INTx-
            NUMA node: 3
            IOMMU group: 52
    
    00:07.1 PCI bridge: Advanced Micro Devices, Inc. [AMD] Starship/Matisse Internal PCIe GPP Bridge 0 to bus[E:B] (prog-if 00 [Normal decode])
    	[..]
            Bus: primary=00, secondary=01, subordinate=01, sec-latency=0
    	[..]
            Capabilities: [2a0 v1] Access Control Services
    
    The logic being that a host bridge with no mmio has no downstream devices
    and cannot source or sink any P2P MMIO operations, thus it does not
    contribute to the ACS isolation calculation for the MFD.
    
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Tushar Dave <tdave@nvidia.com>
    Acked-by: Nirmoy Das <nirmoyd@nvidia.com>
    Acked-by: Carol L Soto <csoto@nvidia.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Signed-off--by: Brad Figg <bfigg@nvidia.com>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -1261,6 +1261,7 @@ void pci_reachable_set(struct pci_dev *start, struct pci_reachable_set *devfns,
 		       bool (*reachable)(struct pci_dev *deva,
 					 struct pci_dev *devb));
 enum pci_bus_isolation pci_bus_isolated(struct pci_bus *bus);
+bool pci_mfd_isolated(struct pci_dev *dev);
 
 int pci_dev_present(const struct pci_device_id *ids);
 
@@ -2041,6 +2042,9 @@ pci_reachable_set(struct pci_dev *start, struct pci_reachable_set *devfns,
 
 static inline enum pci_bus_isolation pci_bus_isolated(struct pci_bus *bus)
 { return PCIE_NON_ISOLATED; }
+
+static inline bool pci_mfd_isolated(struct pci_dev *dev)
+{ return false; }
 
 static inline int pci_dev_present(const struct pci_device_id *ids)
 { return 0; }
