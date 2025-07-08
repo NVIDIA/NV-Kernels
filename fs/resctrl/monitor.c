702bdaa52eb5c126c5007c35539328338a245271
James Morse <james.morse@arm.com>
2025-07-08 17:02:24 +0100

    NVIDIA: SAUCE: fs/resctrl: Take memory hotplug lock whenever taking CPU hotplug lock
    BugLink: https://bugs.launchpad.net/bugs/2122432
    
    resctrl takes the read side CPU hotplug lock whenever it is working
    with the list of domains. This prevents a CPU being brought online
    and the list being modified while resctrl is walking the list, or
    picking CPUs from the CPU masks.
    If resctrl domains for CPU-less NUMA nodes are to be supported, this
    would not be enough to prevent the domain list form being modified as
    a NUMA node can come online with only memory.
    Take the memory hotplug lock whenever the CPU hotplug lock is taken.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    (cherry picked from commit f5a082989a5f40b9b95515d68b230f8125648fdb https://git.kernel.org/pub/scm/linux/kernel/git/morse/linux.git)
    Signed-off-by: Fenghua Yu <fenghuay@nvidia.com>
    Acked-by: Jamie Nguyen <jamien@nvidia.com>
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>

--- fs/resctrl/monitor.c
+++ fs/resctrl/monitor.c
@@ -18,6 +18,7 @@
 #define pr_fmt(fmt)	"resctrl: " fmt
 
 #include <linux/cpu.h>
+#include <linux/memory_hotplug.h>
 #include <linux/resctrl.h>
 #include <linux/sizes.h>
 #include <linux/slab.h>
@@ -763,6 +764,7 @@ void cqm_handle_limbo(struct work_struct *work)
 	struct rdt_mon_domain *d;
 
 	cpus_read_lock();
+	get_online_mems();
 	mutex_lock(&rdtgroup_mutex);
 
 	d = container_of(work, struct rdt_mon_domain, cqm_limbo.work);
@@ -777,6 +779,7 @@ void cqm_handle_limbo(struct work_struct *work)
 	}
 
 	mutex_unlock(&rdtgroup_mutex);
+	put_online_mems();
 	cpus_read_unlock();
 }
 
@@ -810,6 +813,7 @@ void mbm_handle_overflow(struct work_struct *work)
 	struct rdt_resource *r;
 
 	cpus_read_lock();
+	get_online_mems();
 	mutex_lock(&rdtgroup_mutex);
 
 	/*
@@ -843,6 +847,7 @@ void mbm_handle_overflow(struct work_struct *work)
 
 out_unlock:
 	mutex_unlock(&rdtgroup_mutex);
+	put_online_mems();
 	cpus_read_unlock();
 }
 
