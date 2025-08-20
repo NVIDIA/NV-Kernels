0c28591faf430e2a9bd1749b96f273018847c595
Steven Price <steven.price@arm.com>
2025-08-20 15:55:34 +0100

    NVIDIA: VR: SAUCE: KVM: arm64: Support timers in realm RECs
    BugLink: https://bugs.launchpad.net/bugs/2139249
    
    The RMM keeps track of the timer while the realm REC is running, but on
    exit to the normal world KVM is responsible for handling the timers.
    
    The RMM doesn't provide a mechanism to set the counter offset, so don't
    expose KVM_CAP_COUNTER_OFFSET for a realm VM.
    
    A later patch adds the support for propagating the timer values from the
    exit data structure and calling kvm_realm_timers_update().
    
    Reviewed-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Signed-off-by: Steven Price <steven.price@arm.com>
    (cherry picked from https://lore.kernel.org/20250820145606.180644-15-steven.price@arm.com/)
    Signed-off-by: Ian May <ianm@nvidia.com>
    Acked-by: Jamie Nguyen <jamien@nvidia.com>
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Acked-by: Abdur Rahman <abdur.rahman@canonical.com>
    Signed-off-by: Ian May <ianm@nvidia.com>

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -355,11 +355,11 @@ int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
 	case KVM_CAP_PTP_KVM:
 	case KVM_CAP_ARM_SYSTEM_SUSPEND:
 	case KVM_CAP_IRQFD_RESAMPLE:
-	case KVM_CAP_COUNTER_OFFSET:
 	case KVM_CAP_ARM_WRITABLE_IMP_ID_REGS:
 	case KVM_CAP_ARM_SEA_TO_USER:
 		r = 1;
 		break;
+	case KVM_CAP_COUNTER_OFFSET:
 	case KVM_CAP_SET_GUEST_DEBUG:
 		r = !kvm_is_realm(kvm);
 		break;
