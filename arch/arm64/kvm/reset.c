00d75796dba0db9d7268aae1dab7dd1b3f04f212
Steven Price <steven.price@arm.com>
2025-08-20 15:55:31 +0100

    NVIDIA: VR: SAUCE: arm64: RME: Allocate/free RECs to match vCPUs
    BugLink: https://bugs.launchpad.net/bugs/2139249
    
    The RMM maintains a data structure known as the Realm Execution Context
    (or REC). It is similar to struct kvm_vcpu and tracks the state of the
    virtual CPUs. KVM must delegate memory and request the structures are
    created when vCPUs are created, and suitably tear down on destruction.
    
    RECs must also be supplied with addition pages - auxiliary (or AUX)
    granules - for storing the larger registers state (e.g. for SVE). The
    number of AUX granules for a REC depends on the parameters with which
    the Realm was created - the RMM makes this information available via the
    RMI_REC_AUX_COUNT call performed after creating the Realm Descriptor (RD).
    
    Note that only some of register state for the REC can be set by KVM, the
    rest is defined by the RMM (zeroed). The register state then cannot be
    changed by KVM after the REC is created (except when the guest
    explicitly requests this e.g. by performing a PSCI call). The RMM also
    requires that the VMM creates RECs in ascending order of the MPIDR.
    
    See Realm Management Monitor specification (DEN0137) for more information:
    https://developer.arm.com/documentation/den0137/
    
    Signed-off-by: Steven Price <steven.price@arm.com>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    (cherry picked from https://lore.kernel.org/20250820145606.180644-12-steven.price@arm.com/)
    Signed-off-by: Ian May <ianm@nvidia.com>

--- arch/arm64/kvm/reset.c
+++ arch/arm64/kvm/reset.c
@@ -137,6 +137,11 @@ int kvm_arm_vcpu_finalize(struct kvm_vcpu *vcpu, int feature)
 			return -EPERM;
 
 		return kvm_vcpu_finalize_sve(vcpu);
+	case KVM_ARM_VCPU_REC:
+		if (!kvm_is_realm(vcpu->kvm) || !vcpu_is_rec(vcpu))
+			return -EINVAL;
+
+		return kvm_create_rec(vcpu);
 	}
 
 	return -EINVAL;
@@ -145,6 +150,11 @@ int kvm_arm_vcpu_finalize(struct kvm_vcpu *vcpu, int feature)
 bool kvm_arm_vcpu_is_finalized(struct kvm_vcpu *vcpu)
 {
 	if (vcpu_has_sve(vcpu) && !kvm_arm_vcpu_sve_finalized(vcpu))
+		return false;
+
+	if (kvm_is_realm(vcpu->kvm) &&
+	    !(vcpu_is_rec(vcpu) && kvm_arm_rec_finalized(vcpu) &&
+	      READ_ONCE(vcpu->kvm->arch.realm.state) == REALM_STATE_ACTIVE))
 		return false;
 
 	return true;
@@ -161,6 +171,7 @@ void kvm_arm_vcpu_destroy(struct kvm_vcpu *vcpu)
 	free_page((unsigned long)vcpu->arch.ctxt.vncr_array);
 	kfree(vcpu->arch.vncr_tlb);
 	kfree(vcpu->arch.ccsidr);
+	kvm_destroy_rec(vcpu);
 }
 
 static void kvm_vcpu_reset_sve(struct kvm_vcpu *vcpu)
