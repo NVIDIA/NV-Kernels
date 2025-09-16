3487f0004c60704cf775664ff425556157d71778
Alice C. Munduruca <alice.munduruca@canonical.com>
2025-09-16 15:15:57 -0400

    UBUNTU: SAUCE: Revert "sch_htb: make htb_deactivate() idempotent"
    BugLink: https://bugs.launchpad.net/bugs/2127864
    
    This reverts commit d0f4c9c4a964a8f166fbf7d37dec9fa39ccf3e94.
    
    The upstream stable update v5.15.190 contains a backport for a fix which
    we independently applied. (upstream commit 3769478610135) Given that it
    contains changes missing from our backport, we should revert it and
    apply the one from upstream.
    
    Signed-off-by: Alice C. Munduruca <alice.munduruca@canonical.com>
    Signed-off-by: Edoardo Canepa <edoardo.canepa@canonical.com>

--- net/sched/sch_htb.c
+++ net/sched/sch_htb.c
@@ -345,8 +345,7 @@ static void htb_add_to_wait_tree(struct htb_sched *q,
  */
 static inline void htb_next_rb_node(struct rb_node **n)
 {
-	if (*n)
-		*n = rb_next(*n);
+	*n = rb_next(*n);
 }
 
 /**
@@ -607,8 +606,8 @@ static inline void htb_activate(struct htb_sched *q, struct htb_class *cl)
  */
 static inline void htb_deactivate(struct htb_sched *q, struct htb_class *cl)
 {
-	if (!cl->prio_activity)
-		return;
+	WARN_ON(!cl->prio_activity);
+
 	htb_deactivate_prios(q, cl);
 	cl->prio_activity = 0;
 }
@@ -1763,7 +1762,8 @@ static int htb_delete(struct Qdisc *sch, unsigned long arg,
 	if (cl->parent)
 		cl->parent->children--;
 
-	htb_deactivate(q, cl);
+	if (cl->prio_activity)
+		htb_deactivate(q, cl);
 
 	if (cl->cmode != HTB_CAN_SEND)
 		htb_safe_rb_erase(&cl->pq_node,
@@ -1975,7 +1975,8 @@ static int htb_change_class(struct Qdisc *sch, u32 classid,
 			/* turn parent into inner node */
 			qdisc_purge_queue(parent->leaf.q);
 			parent_qdisc = parent->leaf.q;
-			htb_deactivate(q, parent);
+			if (parent->prio_activity)
+				htb_deactivate(q, parent);
 
 			/* remove from evt list because of level change */
 			if (parent->cmode != HTB_CAN_SEND) {
