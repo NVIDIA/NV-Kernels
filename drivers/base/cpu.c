8c4a3c2873f3a619d323a03b059ed08a6625be14
Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
2025-09-17 14:22:49 +0200

    x86/vmscape: Enable the mitigation
    BugLink: https://bugs.launchpad.net/bugs/2124105
    
    Commit 556c1ad666ad90c50ec8fccb930dd5046cfbecfb upstream.
    
    Enable the previously added mitigation for VMscape. Add the cmdline
    vmscape={off|ibpb|force} and sysfs reporting.
    
    Signed-off-by: Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Reviewed-by: Borislav Petkov (AMD) <bp@alien8.de>
    Reviewed-by: Dave Hansen <dave.hansen@linux.intel.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (backported from commit 34e5667041050711a947e260fc9ebebe08bddee5 linux-6.6.y)
    [mpellizzer: context adjusted due to missing configs and code required
     by other CPU vulnerability mitigations (ITS and TSA) that have not been
     backported yet]
    CVE-2025-40300
    Signed-off-by: Massimiliano Pellizzer <massimiliano.pellizzer@canonical.com>
    Acked-by: Alessio Faina <alessio.faina@canonical.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Signed-off-by: Edoardo Canepa <edoardo.canepa@canonical.com>

--- drivers/base/cpu.c
+++ drivers/base/cpu.c
@@ -589,6 +589,7 @@ CPU_SHOW_VULN_FALLBACK(retbleed);
 CPU_SHOW_VULN_FALLBACK(spec_rstack_overflow);
 CPU_SHOW_VULN_FALLBACK(gds);
 CPU_SHOW_VULN_FALLBACK(reg_file_data_sampling);
+CPU_SHOW_VULN_FALLBACK(vmscape);
 
 static DEVICE_ATTR(meltdown, 0444, cpu_show_meltdown, NULL);
 static DEVICE_ATTR(spectre_v1, 0444, cpu_show_spectre_v1, NULL);
@@ -604,6 +605,7 @@ static DEVICE_ATTR(retbleed, 0444, cpu_show_retbleed, NULL);
 static DEVICE_ATTR(spec_rstack_overflow, 0444, cpu_show_spec_rstack_overflow, NULL);
 static DEVICE_ATTR(gather_data_sampling, 0444, cpu_show_gds, NULL);
 static DEVICE_ATTR(reg_file_data_sampling, 0444, cpu_show_reg_file_data_sampling, NULL);
+static DEVICE_ATTR(vmscape, 0444, cpu_show_vmscape, NULL);
 
 static struct attribute *cpu_root_vulnerabilities_attrs[] = {
 	&dev_attr_meltdown.attr,
@@ -620,6 +622,7 @@ static struct attribute *cpu_root_vulnerabilities_attrs[] = {
 	&dev_attr_spec_rstack_overflow.attr,
 	&dev_attr_gather_data_sampling.attr,
 	&dev_attr_reg_file_data_sampling.attr,
+	&dev_attr_vmscape.attr,
 	NULL
 };
 
