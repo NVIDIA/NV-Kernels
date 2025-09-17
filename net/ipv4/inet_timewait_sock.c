2c722ba4bbcca7402df09dcd68b43c4f596464ab
Jakub Kicinski <kuba@kernel.org>
2025-09-16 17:09:31 -0700

    tcp: add datapath logic for PSP with inline key exchange
    BugLink: https://bugs.launchpad.net/bugs/2124107
    
    Add validation points and state propagation to support PSP key
    exchange inline, on TCP connections. The expectation is that
    application will use some well established mechanism like TLS
    handshake to establish a secure channel over the connection and
    if both endpoints are PSP-capable - exchange and install PSP keys.
    Because the connection can existing in PSP-unsecured and PSP-secured
    state we need to make sure that there are no race conditions or
    retransmission leaks.
    
    On Tx - mark packets with the skb->decrypted bit when PSP key
    is at the enqueue time. Drivers should only encrypt packets with
    this bit set. This prevents retransmissions getting encrypted when
    original transmission was not. Similarly to TLS, we'll use
    sk->sk_validate_xmit_skb to make sure PSP skbs can't "escape"
    via a PSP-unaware device without being encrypted.
    
    On Rx - validation is done under socket lock. This moves the validation
    point later than xfrm, for example. Please see the documentation patch
    for more details on the flow of securing a connection, but for
    the purpose of this patch what's important is that we want to
    enforce the invariant that once connection is secured any skb
    in the receive queue has been encrypted with PSP.
    
    Add GRO and coalescing checks to prevent PSP authenticated data from
    being combined with cleartext data, or data with non-matching PSP
    state. On Rx, check skb's with psp_skb_coalesce_diff() at points
    before psp_sk_rx_policy_check(). After skb's are policy checked and on
    the socket receive queue, skb_cmp_decrypted() is sufficient for
    checking for coalescable PSP state. On Tx, tcp_write_collapse_fence()
    should be called when transitioning a socket into PSP Tx state to
    prevent data sent as cleartext from being coalesced with PSP
    encapsulated data.
    
    This change only adds the validation points, for ease of review.
    Subsequent change will add the ability to install keys, and flesh
    the enforcement logic out
    
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Co-developed-by: Daniel Zahka <daniel.zahka@gmail.com>
    Signed-off-by: Daniel Zahka <daniel.zahka@gmail.com>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Link: https://patch.msgid.link/20250917000954.859376-5-daniel.zahka@gmail.com
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    (backported from commit 659a2899a57da59f433182eba571881884d6323e)
    [bpoirier:
     Conflicts:
     - net/ipv4/inet_timewait_sock.c
     	Due to the absence of
     	2d842b6c670b tcp: Remove timewait_sock_ops.twsk_destructor(). (v6.18-rc1)
     	-> Adjust context
     - net/ipv4/tcp_output.c
     	Due to the absence of
     	30f5ca006243 tcp: ecn functions in separated include file (v6.18-rc1)
     	-> Adjust context]
    Signed-off-by: Benjamin Poirier <bpoirier@nvidia.com>
    Acked-by: Aya Levin <ayal@nvidia.com>
    Acked-by: Valentine Fatiev <valentinef@nvidia.com>
    Tested-by: Yoray Hammer <yhammer@nvidia.com>
    Tested-by: Shmuel Gutman <sgutman@nvidia.com>

--- net/ipv4/inet_timewait_sock.c
+++ net/ipv4/inet_timewait_sock.c
@@ -15,6 +15,7 @@
 #include <net/inet_hashtables.h>
 #include <net/inet_timewait_sock.h>
 #include <net/ip.h>
+#include <net/psp.h>
 
 
 /**
@@ -218,6 +219,7 @@ struct inet_timewait_sock *inet_twsk_alloc(const struct sock *sk,
 		refcount_set(&tw->tw_refcnt, 0);
 
 		__module_get(tw->tw_prot->owner);
+		psp_twsk_init(tw, sk);
 	}
 
 	return tw;
