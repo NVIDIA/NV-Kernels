951ca3840ce2eb2ce9f153d86ba90c2b109def03
Abdur Rahman <abdur.rahman@canonical.com>
2025-09-23 23:36:28 -0400

    Ubuntu: [Packaging] Initialize noble:linux-nvidia-6.17
    Ignore: yes
    Signed-off-by: Abdur Rahman <abdur.rahman@canonical.com>

---
+++ debian.nvidia-6.17/rules.d/arm64.mk
@@ -0,0 +1,27 @@
+build_arch	= arm64
+defconfig	= defconfig
+flavours	= nvidia nvidia-64k
+build_image	= vmlinuz.efi
+kernel_file	= arch/$(build_arch)/boot/vmlinuz.efi
+install_file	= vmlinuz
+no_dumpfile = true
+uefi_signed     = true
+
+vdso		= vdso_install
+
+do_extras_package = false
+do_tools_usbip  = true
+do_tools_cpupower = true
+do_tools_perf   = true
+do_tools_perf_jvmti = true
+do_tools_perf_python = true
+do_tools_bpftool = true
+do_tools_rtla = true
+
+do_dtbs		= true
+
+do_lib_rust             = true
+gcc                     = gcc-13
+rustc                   = rustc-1.80
+rustfmt                 = /usr/lib/rust-1.80/bin/rustfmt
+bindgen                 = bindgen-0.65
