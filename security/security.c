111a10a24e1038f30cfe6faac44b6e39e5428d2d
Juergen Gross <jgross@suse.com>
2025-10-14 13:28:15 +0200

    xen/privcmd: add boot control for restricted usage in domU
    BugLink: https://bugs.launchpad.net/bugs/2156550
    
    commit 1613462be621ad5103ec338a7b0ca0746ec4e5f1 upstream.
    
    When running in an unprivileged domU under Xen, the privcmd driver
    is restricted to allow only hypercalls against a target domain, for
    which the current domU is acting as a device model.
    
    Add a boot parameter "unrestricted" to allow all hypercalls (the
    hypervisor will still refuse destructive hypercalls affecting other
    guests).
    
    Make this new parameter effective only in case the domU wasn't started
    using secure boot, as otherwise hypercalls targeting the domU itself
    might result in violating the secure boot functionality.
    
    This is achieved by adding another lockdown reason, which can be
    tested to not being set when applying the "unrestricted" option.
    
    This is part of XSA-482
    
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    CVE-2026-31788
    [nwager: Context change due to missing commit
     eadb2f47a3ce ("lockdown: also lock down previous kgdb use").
     Instead of using that commit (from v5.15.42), we use
     11a6af597f8d4 ("UBUNTU: SAUCE: debug: Lock down kgdb")
     which fixes the same CVE but differs in content.
     Thus, context change for this patch.]
    Signed-off-by: Noah Wager <noah.wager@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- security/security.c
+++ security/security.c
@@ -61,6 +61,7 @@ const char *const lockdown_reasons[LOCKDOWN_CONFIDENTIALITY_MAX+1] = {
 	[LOCKDOWN_XMON_WR] = "xmon write access",
 	[LOCKDOWN_BPF_WRITE_USER] = "use of bpf to write user RAM",
 	[LOCKDOWN_KGDB] = "KDB or KGDB access",
+	[LOCKDOWN_XEN_USER_ACTIONS] = "Xen guest user action",
 	[LOCKDOWN_INTEGRITY_MAX] = "integrity",
 	[LOCKDOWN_KCORE] = "/proc/kcore access",
 	[LOCKDOWN_KPROBES] = "use of kprobes",
