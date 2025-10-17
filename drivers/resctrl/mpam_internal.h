63ed09a166128df370f8fc2e27a0dd56f6c11a35
James Morse <james.morse@arm.com>
2025-10-17 18:38:45 +0100

    NVIDIA: SAUCE: arm_mpam: Probe and reset the rest of the features
    BugLink: https://bugs.launchpad.net/bugs/2122432
    
    MPAM supports more features than are going to be exposed to resctrl.
    For partid other than 0, the reset values of these controls isn't
    known.
    Discover the rest of the features so they can be reset to avoid any
    side effects when resctrl is in use.
    PARTID narrowing allows MSC/RIS to support less configuration space than
    is usable. If this feature is found on a class of device we are likely
    to use, then reduce the partid_max to make it usable. This allows us
    to map a PARTID to itself.
    CC: Rohit Mathew <Rohit.Mathew@arm.com>
    CC: Zeng Heng <zengheng4@huawei.com>
    CC: Dave Martin <Dave.Martin@arm.com>
    Reviewed-by: Jonathan Cameron <jonathan.cameron@huawei.com>
    Tested-by: Fenghua Yu <fenghuay@nvidia.com>
    
    Signed-off-by: James Morse <james.morse@arm.com>
    (cherry picked from commit 342bfa69997131bebce86f273bc0b12014ffc519 https://git.kernel.org/pub/scm/linux/kernel/git/morse/linux.git)
    Signed-off-by: Fenghua Yu <fenghuay@nvidia.com>
    Acked-by: Jamie Nguyen <jamien@nvidia.com>
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>

--- drivers/resctrl/mpam_internal.h
+++ drivers/resctrl/mpam_internal.h
@@ -139,16 +139,30 @@ static inline void mpam_mon_sel_lock_init(struct mpam_msc *msc)
 
 /* Bits for mpam features bitmaps */
 enum mpam_device_features {
-	mpam_feat_cpor_part = 0,
+	mpam_feat_cmax_softlim,
+	mpam_feat_cmax_cmax,
+	mpam_feat_cmax_cmin,
+	mpam_feat_cmax_cassoc,
+	mpam_feat_cpor_part,
 	mpam_feat_mbw_part,
 	mpam_feat_mbw_min,
 	mpam_feat_mbw_max,
+	mpam_feat_mbw_prop,
+	mpam_feat_intpri_part,
+	mpam_feat_intpri_part_0_low,
+	mpam_feat_dspri_part,
+	mpam_feat_dspri_part_0_low,
 	mpam_feat_msmon,
 	mpam_feat_msmon_csu,
+	mpam_feat_msmon_csu_capture,
+	mpam_feat_msmon_csu_xcl,
 	mpam_feat_msmon_csu_hw_nrdy,
 	mpam_feat_msmon_mbwu,
+	mpam_feat_msmon_mbwu_capture,
+	mpam_feat_msmon_mbwu_rwbw,
 	mpam_feat_msmon_mbwu_hw_nrdy,
-	MPAM_FEATURE_LAST
+	mpam_feat_partid_nrw,
+	MPAM_FEATURE_LAST,
 };
 
 struct mpam_props {
@@ -157,6 +171,10 @@ struct mpam_props {
 	u16			cpbm_wd;
 	u16			mbw_pbm_bits;
 	u16			bwa_wd;
+	u16			cmax_wd;
+	u16			cassoc_wd;
+	u16			intpri_wd;
+	u16			dspri_wd;
 	u16			num_csu_mon;
 	u16			num_mbwu_mon;
 };
