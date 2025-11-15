6cc1b70f0868a01a3c245e90a9db86fb955ac7b6
Nathan Chen <nathanc@nvidia.com>
2025-11-15 03:55:16 +0000

    NVIDIA: SAUCE: vfio/nvgrace-egm: Prevent double-unregister of pfn_address_space
    BugLink: https://bugs.launchpad.net/bugs/2131582
    
    Check for the registration state of egm_region's pfn_address_space, preventing
    double-unregisters that can occur when using an external management app such
    as Libvirt to launch VMs with vEGM enabled. This addresses a null pointer
    dereference in __rb_erase_color when using shutting down a Libvirt VM with
    vEGM enabled.
    
    When launching a qemu + Libvirt VM with vEGM, the EGM regions on the host are
    opened and mmaped, populating the member at
    egm_region.pfn_address_space.node.__rb_parent_color. During VM shutdown,
    a close is issued and the region is unregistered. However, the extra
    conditional this commit introduces is required because after open_count goes to
    0 and the address space is unregistered, another open follows and the open_count
    goes back to 1. But there's no mmap so the address space is never registered.
    However, the unregister function gets called again on the following close,
    calling interval_tree_remove() which causes the __rb_erase_color null pointer
    dereference to occur when the pfn_address_space.node.__rb_parent_color member
    is a stale pointer from the previous registration:
    
    [217296.439595] Call trace:
    [217296.442180] __rb_erase_color+0xc4/0x2a8 (P)
    [217296.446632] interval_tree_remove+0x184/0x2e8
    [217296.451171] unregister_pfn_address_space+0x4c/0xc0
    [217296.456255] nvgrace_egm_release+0x98/0xd8 [nvgrace_egm]
    [217296.461780] __fput+0xe4/0x328
    [217296.464990] fput_close_sync+0x4c/0x138
    [217296.468995] __arm64_sys_close+0x44/0xa0
    [217296.473089] invoke_syscall.constprop.0+0x7c/0xf8
    [217296.477991] do_el0_svc+0x4c/0x100
    [217296.481553] el0_svc+0x48/0x200
    [217296.484848] el0t_64_sync_handler+0xc0/0x108
    [217296.489300] el0t_64_sync+0x1b8/0x1c0
    [217296.493132] Code: f9400674 eb03029f 54fffbe1 f9400a74 (f9400280)
    
    Signed-off-by: Nathan Chen <nathanc@nvidia.com>
    Acked-by: Carol L Soto <csoto@nvidia.com>
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Abdur Rahman <abdur.rahman@canonical.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Signed-off--by: Brad Figg <bfigg@nvidia.com>

--- drivers/vfio/pci/nvgrace-gpu/egm.c
+++ drivers/vfio/pci/nvgrace-gpu/egm.c
@@ -33,6 +33,7 @@ struct egm_region {
 	DECLARE_HASHTABLE(htbl, 0x10);
 #ifdef CONFIG_MEMORY_FAILURE
 	struct pfn_address_space pfn_address_space;
+	bool pfn_space_registered;
 #endif
 };
 
@@ -140,7 +141,10 @@ static int nvgrace_egm_release(struct inode *inode, struct file *file)
 
 	if (atomic_dec_and_test(&region->open_count)) {
 #ifdef CONFIG_MEMORY_FAILURE
-		unregister_pfn_address_space(&region->pfn_address_space);
+		if (region->pfn_space_registered) {
+			unregister_pfn_address_space(&region->pfn_address_space);
+			region->pfn_space_registered = false;
+		}
 #endif
 		file->private_data = NULL;
 	}
@@ -169,7 +173,10 @@ static int nvgrace_egm_mmap(struct file *file, struct vm_area_struct *vma)
 	vma->vm_ops = &nvgrace_egm_mmap_ops;
 
 	ret = nvgrace_egm_register_pfn_range(region, vma);
+	if (ret == 0)
+		region->pfn_space_registered = true;
 #endif
+
 	return ret;
 }
 
@@ -458,6 +465,9 @@ int register_egm_node(struct pci_dev *pdev)
 	region->egmpxm = egmpxm;
 
 	hash_init(region->htbl);
+#ifdef CONFIG_MEMORY_FAILURE
+	region->pfn_space_registered = false;
+#endif
 	INIT_LIST_HEAD(&region->gpus);
 
 	atomic_set(&region->open_count, 0);
