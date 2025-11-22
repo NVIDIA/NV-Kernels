44a281bcaeb57e994614ee3543f5e1ef13cdc19d
Ian Rogers <irogers@google.com>
2025-11-22 00:19:18 -0800

    perf hist: In init, ensure mem_info is put on error paths
    BugLink: https://bugs.launchpad.net/bugs/2142789
    
    [ Upstream commit f60efb4454b24cc944ff3eac164bb9dce9169f71 ]
    
    Rather than exit the internal map_symbols directly, put the mem-info
    that does this and also lowers the reference count on the mem-info
    itself otherwise the mem-info is being leaked.
    
    Fixes: 56e144fe98260a0f ("perf mem_info: Add and use map_symbol__exit and addr_map_symbol__exit")
    Signed-off-by: Ian Rogers <irogers@google.com>
    Reviewed-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Manuel Diewald <manuel.diewald@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- tools/perf/util/hist.c
+++ tools/perf/util/hist.c
@@ -519,10 +519,8 @@ err_infos:
 		map_symbol__exit(&he->branch_info->to.ms);
 		zfree(&he->branch_info);
 	}
-	if (he->mem_info) {
-		map_symbol__exit(&he->mem_info->iaddr.ms);
-		map_symbol__exit(&he->mem_info->daddr.ms);
-	}
+	if (he->mem_info)
+		mem_info__zput(he->mem_info);
 err:
 	map_symbol__exit(&he->ms);
 	zfree(&he->stat_acc);
