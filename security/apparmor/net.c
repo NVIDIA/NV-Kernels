45c94ba6606cbac96e139f21559c049f447bc457
John Johansen <john.johansen@canonical.com>
2025-11-24 15:07:42 -0800

    apparmor: fix NULL sock in aa_sock_file_perm
    BugLink: https://bugs.launchpad.net/bugs/2156272
    
    [ Upstream commit 00b67657535dfea56e84d11492f5c0f61d0af297 ]
    
    Deal with the potential that sock and sock-sk can be NULL during
    socket setup or teardown. This could lead to an oops. The fix for NULL
    pointer dereference in __unix_needs_revalidation shows this is at
    least possible for af_unix sockets. While the fix for af_unix sockets
    applies for newer mediation this is still the fall back path for older
    af_unix mediation and other sockets, so ensure it is covered.
    
    Fixes: 56974a6fcfef6 ("apparmor: add base infastructure for socket mediation")
    Reviewed-by: Georgia Garcia <georgia.garcia@canonical.com>
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    CVE-2026-45848
    Signed-off-by: Vinicius Peixoto <vinicius.peixoto@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- security/apparmor/net.c
+++ security/apparmor/net.c
@@ -248,8 +248,10 @@ int aa_sock_file_perm(struct aa_label *label, const char *op, u32 request,
 		      struct socket *sock)
 {
 	AA_BUG(!label);
-	AA_BUG(!sock);
-	AA_BUG(!sock->sk);
+
+	/* sock && sock->sk can be NULL for sockets being set up or torn down */
+	if (!sock || !sock->sk)
+		return 0;
 
 	return af_select(sock->sk->sk_family,
 			 file_perm(label, op, request, sock),
