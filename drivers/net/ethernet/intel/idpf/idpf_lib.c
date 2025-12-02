cdad0bca2f1a4dc108065293f4da50824ded928e
Sreedevi Joshi <sreedevi.joshi@intel.com>
2025-12-02 17:12:46 -0600

    idpf: Fix error handling in idpf_vport_open()
    BugLink: https://bugs.launchpad.net/bugs/2154749
    
    [ Upstream commit 87b8ee64685bc096a087af833d4594b2332bfdb1 ]
    
    Fix error handling to properly cleanup interrupts when
    idpf_vport_queue_ids_init() or idpf_rx_bufs_init_all() fail. Jump to
    'intr_deinit' instead of 'queues_rel' to ensure interrupts are cleaned up
    before releasing other resources.
    
    Fixes: d4d558718266 ("idpf: initialize interrupts and enable vport")
    Signed-off-by: Sreedevi Joshi <sreedevi.joshi@intel.com>
    Reviewed-by: Madhu Chittim <madhu.chittim@intel.com>
    Reviewed-by: Aleksandr Loktionov <aleksandr.loktionov@intel.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Tested-by: Samuel Salin <Samuel.salin@intel.com>
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    [bjamison: adjust context]
    Signed-off-by: Bethany Jamison <bethany.jamison@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- drivers/net/ethernet/intel/idpf/idpf_lib.c
+++ drivers/net/ethernet/intel/idpf/idpf_lib.c
@@ -1512,14 +1512,14 @@ static int idpf_vport_open(struct idpf_vport *vport, bool rtnl)
 	if (err) {
 		dev_err(&adapter->pdev->dev, "Failed to initialize interrupts for vport %u: %d\n",
 			vport->vport_id, err);
-		goto queues_rel;
+		goto intr_deinit;
 	}
 
 	err = idpf_rx_bufs_init_all(vport);
 	if (err) {
 		dev_err(&adapter->pdev->dev, "Failed to initialize RX buffers for vport %u: %d\n",
 			vport->vport_id, err);
-		goto queues_rel;
+		goto intr_deinit;
 	}
 
 	err = idpf_queue_reg_init(vport);
