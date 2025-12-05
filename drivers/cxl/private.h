d42c8094b4652141c005651324f63c610e7775a5
Dan Williams <dan.j.williams@intel.com>
2025-12-05 11:52:25 +0000

    NVIDIA: VR: SAUCE: cxl/port: Arrange for always synchronous endpoint attach
    Make it so that upon return from devm_cxl_add_endpoint() that cxl_mem_probe() can
    assume that the endpoint has had a chance to complete cxl_port_probe().
    I.e. cxl_port module loading has completed prior to device registration.
    
    MODULE_SOFTDEP() is not sufficient for this purpose, but a hard link-time
    dependency is reliable.
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    (backported from https://lore.kernel.org/linux-cxl/20251205115248.772945-1-alejandro.lucero-palau@amd.com/)
    Signed-off-by: Jiandi An <jan@nvidia.com>

--- drivers/cxl/private.h
+++ drivers/cxl/private.h
@@ -1,11 +1,15 @@
 // SPDX-License-Identifier: GPL-2.0
 /* Copyright(c) 2025 Intel Corporation. */
 
-/* Private interfaces betwen common drivers ("cxl_mem") and the cxl_core */
-
+/*
+ * Private interfaces betwen common drivers ("cxl_mem", "cxl_port") and
+ * the cxl_core.
+ */
 #ifndef __CXL_PRIVATE_H__
 #define __CXL_PRIVATE_H__
 struct cxl_memdev *cxl_memdev_alloc(struct cxl_dev_state *cxlds);
 struct cxl_memdev *devm_cxl_memdev_add_or_reset(struct device *host,
 						struct cxl_memdev *cxlmd);
+int devm_cxl_add_endpoint(struct device *host, struct cxl_memdev *cxlmd,
+			  struct cxl_dport *parent_dport);
 #endif /* __CXL_PRIVATE_H__ */
