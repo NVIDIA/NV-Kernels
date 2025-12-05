b17b6a228817b9ad8f0683effdf851adeac943b5
Alejandro Lucero <alucerop@amd.com>
2025-12-05 11:52:35 +0000

    NVIDIA: VR: SAUCE: cxl: Add function for obtaining region range
    A CXL region struct contains the physical address to work with.
    
    Type2 drivers can create a CXL region but have not access to the
    related struct as it is defined as private by the kernel CXL core.
    Add a function for getting the cxl region range to be used for mapping
    such memory range by a Type2 driver.
    
    Signed-off-by: Alejandro Lucero <alucerop@amd.com>
    Reviewed-by: Zhi Wang <zhiw@nvidia.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Reviewed-by: Dave Jiang <dave.jiang@intel.com>
    (backported from https://lore.kernel.org/linux-cxl/20251205115248.772945-1-alejandro.lucero-palau@amd.com/)
    Signed-off-by: Jiandi An <jan@nvidia.com>

--- include/cxl/cxl.h
+++ include/cxl/cxl.h
@@ -253,4 +253,6 @@ struct cxl_memdev *devm_cxl_add_memdev(struct device *host,
 struct cxl_region;
 struct cxl_endpoint_decoder *cxl_get_committed_decoder(struct cxl_memdev *cxlmd,
 						       struct cxl_region **cxlr);
+struct range;
+int cxl_get_region_range(struct cxl_region *region, struct range *range);
 #endif /* __CXL_CXL_H__ */
