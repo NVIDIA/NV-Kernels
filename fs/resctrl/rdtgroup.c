0e979da7337ccee4b02623d6cc54353349cd067a
Tony Luck <tony.luck@intel.com>
2025-12-17 09:21:15 -0800

    fs/resctrl: Move RMID initialization to first mount
    BugLink: https://bugs.launchpad.net/bugs/2146389
    
    L3 monitor features are enumerated during resctrl initialization and
    rmid_ptrs[] that tracks all RMIDs and depends on the number of supported
    RMIDs is allocated during this time.
    
    Telemetry monitor features are enumerated during first resctrl mount and
    may support a different number of RMIDs compared to L3 monitor features.
    
    Delay allocation and initialization of rmid_ptrs[] until first mount.
    Since the number of RMIDs cannot change on later mounts, keep the same set of
    rmid_ptrs[] until resctrl_exit(). This is required because the limbo handler
    keeps running after resctrl is unmounted and needs to access rmid_ptrs[]
    as it keeps tracking busy RMIDs after unmount.
    
    Rename routines to match what they now do:
    dom_data_init() -> setup_rmid_lru_list()
    dom_data_exit() -> free_rmid_lru_list()
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Reviewed-by: Reinette Chatre <reinette.chatre@intel.com>
    Link: https://lore.kernel.org/20251217172121.12030-1-tony.luck@intel.com
    (backported from commit d0891647fbc6e931f27517364cbc4ee1811d76db)
    Signed-off-by: Shanker Donthineni <sdonthineni@nvidia.com>
    [fenghuay: fix minor conflicts in setup_rmid_lru_list() and dom_data_exit()]
    Signed-off-by: Fenghua Yu <fenghuay@nvidia.com>
    Acked-by: Jamie Nguyen <jamien@nvidia.com>
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Signed-off-by: Brad Figg <bfigg@nvidia.com>

--- fs/resctrl/rdtgroup.c
+++ fs/resctrl/rdtgroup.c
@@ -2976,6 +2976,10 @@ static int rdt_get_tree(struct fs_context *fc)
 		goto out;
 	}
 
+	ret = setup_rmid_lru_list();
+	if (ret)
+		goto out;
+
 	ret = rdtgroup_setup_root(ctx);
 	if (ret)
 		goto out;
@@ -4848,4 +4852,5 @@ void resctrl_exit(void)
 	 */
 
 	resctrl_l3_mon_resource_exit();
+	free_rmid_lru_list();
 }
