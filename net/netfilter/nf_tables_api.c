4d96d083c39d9e593584a12a220eee49b6fb10af
Zilin Guan <zilin@seu.edu.cn>
2025-12-24 12:48:26 +0000

    netfilter: nf_tables: fix memory leak in nf_tables_newrule()
    BugLink: https://bugs.launchpad.net/bugs/2139704
    
    [ Upstream commit d077e8119ddbb4fca67540f1a52453631a47f221 ]
    
    In nf_tables_newrule(), if nft_use_inc() fails, the function jumps to
    the err_release_rule label without freeing the allocated flow, leading
    to a memory leak.
    
    Fix this by adding a new label err_destroy_flow and jumping to it when
    nft_use_inc() fails. This ensures that the flow is properly released
    in this error case.
    
    Fixes: 1689f25924ada ("netfilter: nf_tables: report use refcount overflow")
    Signed-off-by: Zilin Guan <zilin@seu.edu.cn>
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Manuel Diewald <manuel.diewald@canonical.com>
    Signed-off-by: Edoardo Canepa <edoardo.canepa@canonical.com>

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -3753,7 +3753,7 @@ static int nf_tables_newrule(struct sk_buff *skb, const struct nfnl_info *info,
 
 	if (!nft_use_inc(&chain->use)) {
 		err = -EMFILE;
-		goto err_release_rule;
+		goto err_destroy_flow;
 	}
 
 	if (info->nlh->nlmsg_flags & NLM_F_REPLACE) {
@@ -3803,6 +3803,7 @@ static int nf_tables_newrule(struct sk_buff *skb, const struct nfnl_info *info,
 
 err_destroy_flow_rule:
 	nft_use_dec_restore(&chain->use);
+err_destroy_flow:
 	if (flow)
 		nft_flow_rule_destroy(flow);
 err_release_rule:
