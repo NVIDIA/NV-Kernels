a85a8b4bf0085bcecb2e6597bc360fc74b21a2e2
Johan Hovold <johan@kernel.org>
2025-12-30 08:59:17 -0500

    serial: core: fix OF node leak
    BugLink: https://bugs.launchpad.net/bugs/2142789
    
    [ Upstream commit 273cc3406c8d4e830ed45967c70d08d20ca1380e ]
    
    Make sure to drop the OF node reference taken when initialising the
    control and port devices when the devices are later released.
    
    Fixes: d36f0e9a0002 ("serial: core: restore of_node information in sysfs")
    Cc: Aidan Stewart <astewart@tektelic.com>
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Link: https://lore.kernel.org/r/20250708085817.16070-1-johan@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Stable-dep-of: 24ec03cc5512 ("serial: core: Restore sysfs fwnode information")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Manuel Diewald <manuel.diewald@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- drivers/tty/serial/serial_base_bus.c
+++ drivers/tty/serial/serial_base_bus.c
@@ -12,6 +12,7 @@
 #include <linux/device.h>
 #include <linux/idr.h>
 #include <linux/module.h>
+#include <linux/of.h>
 #include <linux/serial_core.h>
 #include <linux/slab.h>
 #include <linux/spinlock.h>
@@ -92,6 +93,7 @@ static void serial_base_ctrl_release(struct device *dev)
 {
 	struct serial_ctrl_device *ctrl_dev = to_serial_base_ctrl_device(dev);
 
+	of_node_put(dev->of_node);
 	kfree(ctrl_dev);
 }
 
@@ -139,6 +141,7 @@ static void serial_base_port_release(struct device *dev)
 {
 	struct serial_port_device *port_dev = to_serial_base_port_device(dev);
 
+	of_node_put(dev->of_node);
 	kfree(port_dev);
 }
 
