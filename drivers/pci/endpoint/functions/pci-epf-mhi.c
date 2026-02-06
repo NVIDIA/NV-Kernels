1fc77c5fc07bd042a9639d6a8cbd6bd190722d0b
Daniel Hodges <git@danielhodges.dev>
2026-02-06 15:05:29 -0500

    PCI: epf-mhi: Return 0, not remaining timeout, when eDMA ops complete
    BugLink: https://bugs.launchpad.net/bugs/2156549
    
    commit 36bfc3642b19a98f1302aed4437c331df9b481f0 upstream.
    
    pci_epf_mhi_edma_read() and pci_epf_mhi_edma_write() start DMA
    operations and wait for completion with a timeout.
    
    On successful completion, they previously returned the remaining
    timeout, which callers may treat as an error.  In particular,
    mhi_ep_ring_add_element(), which calls pci_epf_mhi_edma_write() via
    mhi_cntrl->write_sync(), interprets any non-zero return value as
    failure.
    
    Return 0 on success instead of the remaining timeout to prevent
    mhi_ep_ring_add_element() from treating successful completion as an
    error.
    
    Fixes: 7b99aaaddabb ("PCI: epf-mhi: Add eDMA support")
    Signed-off-by: Daniel Hodges <git@danielhodges.dev>
    [mani: changed commit log as per https://lore.kernel.org/linux-pci/20260227191510.GA3904799@bhelgaas]
    Signed-off-by: Manivannan Sadhasivam <mani@kernel.org>
    Reviewed-by: Krishna Chaitanya Chundru <krishna.chundru@oss.qualcomm.com>
    Cc: stable@vger.kernel.org
    Link: https://patch.msgid.link/20260206200529.10784-1-git@danielhodges.dev
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Alice C. Munduruca <alice.munduruca@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- drivers/pci/endpoint/functions/pci-epf-mhi.c
+++ drivers/pci/endpoint/functions/pci-epf-mhi.c
@@ -350,6 +350,8 @@ static int pci_epf_mhi_edma_read(struct mhi_ep_cntrl *mhi_cntrl,
 		dev_err(dev, "DMA transfer timeout\n");
 		dmaengine_terminate_sync(chan);
 		ret = -ETIMEDOUT;
+	} else {
+		ret = 0;
 	}
 
 err_unmap:
@@ -421,6 +423,8 @@ static int pci_epf_mhi_edma_write(struct mhi_ep_cntrl *mhi_cntrl,
 		dev_err(dev, "DMA transfer timeout\n");
 		dmaengine_terminate_sync(chan);
 		ret = -ETIMEDOUT;
+	} else {
+		ret = 0;
 	}
 
 err_unmap:
