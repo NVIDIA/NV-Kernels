5495db8ca0fdcb40eb295658b98147874f1251ee
Jacob Martin <jacob.martin@canonical.com>
2026-02-27 14:50:31 -0600

    UBUNTU: [Packaging] Initialize resolute/linux-nvidia-bos
    Ignore: yes
    Signed-off-by: Jacob Martin <jacob.martin@canonical.com>

---
+++ debian.nvidia-bos/rules.d/arm64.mk
@@ -0,0 +1,20 @@
+build_arch	= arm64
+defconfig	= defconfig
+flavours	= nvidia nvidia-64k
+build_image	= vmlinuz.efi
+kernel_file	= arch/$(build_arch)/boot/vmlinuz.efi
+install_file	= vmlinuz
+no_dumpfile = true
+uefi_signed     = true
+
+vdso		= vdso_install
+
+do_tools_usbip  = true
+do_tools_cpupower = true
+do_tools_perf   = true
+do_tools_perf_jvmti = true
+do_tools_perf_python = true
+do_tools_bpftool = true
+do_tools_rtla = true
+
+do_dtbs		= true
