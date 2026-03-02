69cb91393c85e134c430491181bd62b2e82d95eb
Peter Yin <peteryin.openbmc@gmail.com>
2026-03-02 15:56:42 +0800

    i3c: master: dw-i3c: Fix missing of_node for virtual I2C adapter
    BugLink: https://bugs.launchpad.net/bugs/2155660
    
    [ Upstream commit f26ecaa0f0abfe5db173416214098a00d3b7db79 ]
    
    The DesignWare I3C master driver creates a virtual I2C adapter to
    provide backward compatibility with I2C devices. However, the current
    implementation does not associate this virtual adapter with any
    Device Tree node.
    
    Propagate the of_node from the I3C master platform device to the
    virtual I2C adapter's device structure. This ensures that standard
    I2C aliases are correctly resolved and bus numbering remains consistent.
    
    Signed-off-by: Peter Yin <peteryin.openbmc@gmail.com>
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Link: https://patch.msgid.link/20260302075645.1492766-1-peteryin.openbmc@gmail.com
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Alice C. Munduruca <alice.munduruca@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- drivers/i3c/master/dw-i3c-master.c
+++ drivers/i3c/master/dw-i3c-master.c
@@ -1512,6 +1512,8 @@ int dw_i3c_common_probe(struct dw_i3c_master *master,
 		ops = &dw_mipi_i3c_ibi_ops;
 
 	INIT_WORK(&master->hj_work, dw_i3c_hj_work);
+
+	device_set_of_node_from_dev(&master->base.i2c.dev, &pdev->dev);
 	ret = i3c_master_register(&master->base, &pdev->dev, ops, false);
 	if (ret)
 		goto err_assert_rst;
