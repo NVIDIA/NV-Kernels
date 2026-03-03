7cdde7e1384346125000c4275f638107ddacdb15
Marios Makassikis <mmakassikis@freebox.fr>
2026-03-03 11:14:32 +0100

    smb: server: fix use-after-free in smb2_open()
    commit 1e689a56173827669a35da7cb2a3c78ed5c53680 upstream.
    
    The opinfo pointer obtained via rcu_dereference(fp->f_opinfo) is
    dereferenced after rcu_read_unlock(), creating a use-after-free
    window.
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Marios Makassikis <mmakassikis@freebox.fr>
    Acked-by: Namjae Jeon <linkinjeon@kernel.org>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    CVE-2026-43378
    [cremfuelled: adjust context]
    Signed-off-by: Alice C. Munduruca <alice.munduruca@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- fs/smb/server/smb2pdu.c
+++ fs/smb/server/smb2pdu.c
@@ -3346,10 +3346,8 @@ int smb2_open(struct ksmbd_work *work)
 	memcpy(fp->client_guid, conn->ClientGUID, SMB2_CLIENT_GUID_SIZE);
 
 	rsp->StructureSize = cpu_to_le16(89);
-	rcu_read_lock();
-	opinfo = rcu_dereference(fp->f_opinfo);
+	opinfo = opinfo_get(fp);
 	rsp->OplockLevel = opinfo != NULL ? opinfo->level : 0;
-	rcu_read_unlock();
 	rsp->Flags = 0;
 	rsp->CreateAction = cpu_to_le32(file_info);
 	rsp->CreationTime = cpu_to_le64(fp->create_time);
@@ -3390,6 +3388,7 @@ int smb2_open(struct ksmbd_work *work)
 		next_ptr = &lease_ccontext->Next;
 		next_off = conn->vals->create_lease_size;
 	}
+	opinfo_put(opinfo);
 
 	if (maximal_access_ctxt) {
 		struct create_context *mxac_ccontext;
