feb0620980bd47af83676d172e08de349bae611b
Koichiro Den <den@valinux.co.jp>
2026-03-06 00:10:50 +0900

    PCI: dwc: rcar-gen4: Change EPC BAR alignment to 4K as per the documentation
    BugLink: https://bugs.launchpad.net/bugs/2156956
    
    [ Upstream commit 13f55a7ca773c731a1e645934c1ae48577f48785 ]
    
    R-Car S4 Series (R8A779F[4-7]*) EP controller uses a 4K minimum iATU region
    size (CX_ATU_MIN_REGION_SIZE = 4K) as per R19UH0161EJ0130 Rev.1.30. Also,
    the controller itself can only be configured in the range 4 KB to 64 KB, so
    the current 1 MB alignment requirement is incorrect.
    
    Hence, change the alignment to the min size 4K as per the documentation.
    
    This also fixes needless unusability of BAR4 on this platform when the
    target address is fixed, such as for doorbell targets.
    
    Fixes: e311b3834dfa ("PCI: rcar-gen4: Add endpoint mode support")
    Signed-off-by: Koichiro Den <den@valinux.co.jp>
    [mani: commit log]
    Signed-off-by: Manivannan Sadhasivam <mani@kernel.org>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Niklas Cassel <cassel@kernel.org>
    Link: https://patch.msgid.link/20260305151050.1834007-1-den@valinux.co.jp
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Alice C. Munduruca <alice.munduruca@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- drivers/pci/controller/dwc/pcie-rcar-gen4.c
+++ drivers/pci/controller/dwc/pcie-rcar-gen4.c
@@ -406,7 +406,7 @@ static const struct pci_epc_features rcar_gen4_pcie_epc_features = {
 	.msi_capable = true,
 	.msix_capable = false,
 	.reserved_bar = 1 << BAR_1 | 1 << BAR_3 | 1 << BAR_5,
-	.align = SZ_1M,
+	.align = SZ_4K,
 };
 
 static const struct pci_epc_features*
