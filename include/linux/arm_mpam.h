d7af0e2907b86e342e010a6b9da8095d0e394290
Fenghua Yu <fenghuay@nvidia.com>
2026-03-11 23:05:38 +0000

    Revert "NVIDIA: SAUCE: untested: arm_mpam: resctrl: Allow resctrl to enable NUMA nid as MB domain-id"
    BugLink: https://bugs.launchpad.net/bugs/2146389
    
    This reverts commit 448da3e0e4a3f779a02072996770318b9b8ecce5.
    
    The commit will be replaced by newer numa code.
    
    Signed-off-by: Fenghua Yu <fenghuay@nvidia.com>
    Acked-by: Jamie Nguyen <jamien@nvidia.com>
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Signed-off-by: Brad Figg <bfigg@nvidia.com>

--- include/linux/arm_mpam.h
+++ include/linux/arm_mpam.h
@@ -78,8 +78,15 @@ struct rdt_resource;
 void *resctrl_arch_mon_ctx_alloc(struct rdt_resource *r, enum resctrl_event_id evtid);
 void resctrl_arch_mon_ctx_free(struct rdt_resource *r, enum resctrl_event_id evtid, void *ctx);
 
-bool resctrl_arch_get_mb_uses_numa_nid(void);
-int resctrl_arch_set_mb_uses_numa_nid(bool enabled);
+static inline bool resctrl_arch_get_mb_uses_numa_nid(void)
+{
+	return false;
+}
+
+static inline bool resctrl_arch_set_mb_uses_numa_nid(bool enabled)
+{
+	return false;
+}
 
 /*
  * The CPU configuration for MPAM is cheap to write, and is only written if it
