45dc819bdcaf8895574627d09326db95b7b34295
Val Packett <val@packett.cool>
2026-03-12 08:12:08 -0300

    clk: qcom: gcc-sc8180x: Use retention for USB power domains
    BugLink: https://bugs.launchpad.net/bugs/2156956
    
    [ Upstream commit 25bc96f26cd6c19dde13a0b9859183e531d6fbfc ]
    
    The USB subsystem does not expect to lose its state on suspend:
    
        xhci-hcd xhci-hcd.0.auto: xHC error in resume, USBSTS 0x401, Reinit
        usb usb1: root hub lost power or was reset
    
    (The reinitialization usually succeeds, but it does slow down resume.)
    
    To maintain state during suspend, the relevant GDSCs need to stay in
    retention mode, like they do on other similar SoCs. Change the mode to
    PWRSTS_RET_ON to fix.
    
    Fixes: 4433594bbe5d ("clk: qcom: gcc: Add global clock controller driver for SC8180x")
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
    Reviewed-by: Konrad Dybcio <konrad.dybcio@oss.qualcomm.com>
    Signed-off-by: Val Packett <val@packett.cool>
    Link: https://lore.kernel.org/r/20260312112321.370983-4-val@packett.cool
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Alice C. Munduruca <alice.munduruca@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- drivers/clk/qcom/gcc-sc8180x.c
+++ drivers/clk/qcom/gcc-sc8180x.c
@@ -4146,7 +4146,7 @@ static struct gdsc usb30_sec_gdsc = {
 	.pd = {
 		.name = "usb30_sec_gdsc",
 	},
-	.pwrsts = PWRSTS_OFF_ON,
+	.pwrsts = PWRSTS_RET_ON,
 	.flags = POLL_CFG_GDSCR,
 };
 
@@ -4164,7 +4164,7 @@ static struct gdsc usb30_prim_gdsc = {
 	.pd = {
 		.name = "usb30_prim_gdsc",
 	},
-	.pwrsts = PWRSTS_OFF_ON,
+	.pwrsts = PWRSTS_RET_ON,
 	.flags = POLL_CFG_GDSCR,
 };
 
@@ -4236,7 +4236,7 @@ static struct gdsc usb30_mp_gdsc = {
 	.pd = {
 		.name = "usb30_mp_gdsc",
 	},
-	.pwrsts = PWRSTS_OFF_ON,
+	.pwrsts = PWRSTS_RET_ON,
 	.flags = POLL_CFG_GDSCR,
 };
 
