e6d9d4fd24ab9b38fbbd175e146a583e17a01f25
Sean Christopherson <seanjc@google.com>
2026-03-16 13:19:56 -0400

    KVM: x86: Co-locate initialization of feature MSRs in kvm_arch_vcpu_create()
    BugLink: https://bugs.launchpad.net/bugs/2155660
    
    [ Upstream commit 2142ac663a6a72ac868d0768681b1355e3a703eb ]
    
    Bunch all of the feature MSR initialization in kvm_arch_vcpu_create() so
    that it can be easily quirked in a future patch.
    
    No functional change intended.
    
    Link: https://lore.kernel.org/r/20240802185511.305849-2-seanjc@google.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Stable-dep-of: e2ffe85b6d2b ("KVM: x86: Introduce KVM_X86_QUIRK_VMCS12_ALLOW_FREEZE_IN_SMM")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    [cremfuelled: adjust context]
    Signed-off-by: Alice C. Munduruca <alice.munduruca@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -12206,6 +12206,8 @@ int kvm_arch_vcpu_create(struct kvm_vcpu *vcpu)
 
 	kvm_async_pf_hash_reset(vcpu);
 
+	vcpu->arch.arch_capabilities = kvm_get_arch_capabilities();
+	vcpu->arch.msr_platform_info = MSR_PLATFORM_INFO_CPUID_FAULT;
 	vcpu->arch.perf_capabilities = kvm_caps.supported_perf_cap;
 	kvm_pmu_init(vcpu);
 
@@ -12220,8 +12222,6 @@ int kvm_arch_vcpu_create(struct kvm_vcpu *vcpu)
 	if (r)
 		goto free_guest_fpu;
 
-	vcpu->arch.arch_capabilities = kvm_get_arch_capabilities();
-	vcpu->arch.msr_platform_info = MSR_PLATFORM_INFO_CPUID_FAULT;
 	kvm_xen_init_vcpu(vcpu);
 	kvm_vcpu_mtrr_init(vcpu);
 	vcpu_load(vcpu);
