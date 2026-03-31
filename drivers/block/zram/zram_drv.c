e223cd6bad249bd7edd76bc528d753f171f3b4d2
Sergey Senozhatsky <senozhatsky@chromium.org>
2026-03-31 16:42:44 +0900

    zram: do not forget to endio for partial discard requests
    BugLink: https://bugs.launchpad.net/bugs/2156549
    
    commit e3668b371329ea036ff022ce8ecc82f8befcf003 upstream.
    
    As reported by Qu Wenruo and Avinesh Kumar, the following
    
     getconf PAGESIZE
     65536
     blkdiscard -p 4k /dev/zram0
    
    takes literally forever to complete.  zram doesn't support partial
    discards and just returns immediately w/o doing any discard work in such
    cases.  The problem is that we forget to endio on our way out, so
    blkdiscard sleeps forever in submit_bio_wait().  Fix this by jumping to
    end_bio label, which does bio_endio().
    
    Link: https://lore.kernel.org/20260331074255.777019-1-senozhatsky@chromium.org
    Fixes: 0120dd6e4e20 ("zram: make zram_bio_discard more self-contained")
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Reported-by: Qu Wenruo <wqu@suse.com>
    Closes: https://lore.kernel.org/linux-block/92361cd3-fb8b-482e-bc89-15ff1acb9a59@suse.com
    Tested-by: Qu Wenruo <wqu@suse.com>
    Reported-by: Avinesh Kumar <avinesh.kumar@suse.com>
    Closes: https://bugzilla.suse.com/show_bug.cgi?id=1256530
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Cc: Brian Geffon <bgeffon@google.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    CVE-2026-46089
    Signed-off-by: Alice C. Munduruca <alice.munduruca@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1891,7 +1891,7 @@ static void zram_bio_discard(struct zram *zram, struct bio *bio)
 	 */
 	if (offset) {
 		if (n <= (PAGE_SIZE - offset))
-			return;
+			goto end_bio;
 
 		n -= (PAGE_SIZE - offset);
 		index++;
@@ -1906,6 +1906,7 @@ static void zram_bio_discard(struct zram *zram, struct bio *bio)
 		n -= PAGE_SIZE;
 	}
 
+end_bio:
 	bio_endio(bio);
 }
 
