NVIDIA/NV-Kernels

commit bb89f8b44e66a6769543de2cbaddb3cec0ed1be0

tegra: bpmp: Add BPMP client driver for T410
On T410, the PCIe/GPU-UPHY is unable to throttle down to the configured
MPAM bandwidth level.
This issue can be addressed by writing to the relevant IMPDEF/CREG
registers. However, the kernel does not have access to the CREG registers.
This patch introduces a new BPMP client layer for T410, allowing userspace
to send IMPDEF/CREG programming requests to BPMP-FW via sysfs.
debugfs cannot serve as the userspace interface since this feature is
intended to be accessible during runtime in production environments.

Bug 5415122

Change-Id: Ic410df8e12bae09e332dd80519ec46f9a86410f8
Signed-off-by: Aniruddha Rao <anrao@nvidia.com>
Signed-off-by: Shanker Donthineni <sdonthineni@nvidia.com>

drivers/firmware/tegra/bpmp-tegra410.c
@@ -1,0 +1,308 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2026, NVIDIA CORPORATION.
+ */
+
+#include <linux/acpi.h>
+#include <linux/device.h>
+
+#include <soc/tegra/bpmp.h>
+#include <soc/tegra/bpmp-abi.h>
+
+#include "bpmp-private.h"
+
+struct bpmp_export {
+	struct device child;
+	struct tegra_bpmp *bpmp;
+	u32 instance;
+	u32 vc_type;
+	bool vc_type_set;
+};
+
+static struct bpmp_export *child_to_bpmp_export(struct device *child)
+{
+	return container_of(child, struct bpmp_export, child);
+}
+
+static struct tegra_bpmp *child_to_tegra_bpmp(struct device *child)
+{
+	struct bpmp_export *export = child_to_bpmp_export(child);
+
+	return export->bpmp;
+}
+
+static void bpmp_export_release(struct device *child)
+{
+	struct bpmp_export *export = child_to_bpmp_export(child);
+
+	kfree(export);
+}
+
+static int tegra_sochub_set_mbwt(struct tegra_bpmp *bpmp,
+				 unsigned int instance,
+				 unsigned int vc_type,
+				 unsigned int bandwidth)
+{
+	struct mrq_sochub_mbwt_request request;
+	struct tegra_bpmp_message msg;
+	int err;
+
+	memset(&request, 0, sizeof(request));
+	request.cmd = CMD_SOCHUB_MBWT_SET_BW;
+	request.set_bw.instance = instance;
+	request.set_bw.vc_type = vc_type;
+	request.set_bw.bw = bandwidth;
+
+	memset(&msg, 0, sizeof(msg));
+	msg.mrq = MRQ_SOCHUB_MBWT;
+	msg.tx.data = &request;
+	msg.tx.size = sizeof(request);
+
+	err = tegra_bpmp_transfer(bpmp, &msg);
+
+	if (err)
+		dev_err(bpmp->dev,
+			"Failed setting up the SocHub MBWT with error %d\n",
+			err);
+	return err;
+}
+
+static int tegra_sochub_get_mbwt(struct tegra_bpmp *bpmp,
+				 unsigned int instance,
+				 unsigned int vc_type,
+				 struct cmd_sochub_mbwt_get_bw_resp *bw)
+{
+	struct mrq_sochub_mbwt_request request;
+	struct mrq_sochub_mbwt_response response;
+	struct tegra_bpmp_message msg;
+	int err;
+
+	memset(&request, 0, sizeof(request));
+	request.cmd = CMD_SOCHUB_MBWT_GET_BW;
+	request.get_bw.instance = instance;
+	request.get_bw.vc_type = vc_type;
+
+	memset(&response, 0, sizeof(response));
+
+	memset(&msg, 0, sizeof(msg));
+	msg.mrq = MRQ_SOCHUB_MBWT;
+	msg.tx.data = &request;
+	msg.tx.size = sizeof(request);
+	msg.rx.data = &response;
+	msg.rx.size = sizeof(response);
+
+	err = tegra_bpmp_transfer(bpmp, &msg);
+	if (err) {
+		dev_err(bpmp->dev,
+			"Failed reading the SocHub MBWT with error %d\n",
+			err);
+		return err;
+	}
+	if (msg.rx.ret < 0)
+		return -EINVAL;
+
+	memcpy(bw, &response.get_bw, sizeof(response.get_bw));
+
+	return 0;
+}
+
+
+static ssize_t vc_type_store(struct device *child,
+			    struct device_attribute *attr,
+			    const char *buf, size_t size)
+{
+	struct bpmp_export *export = child_to_bpmp_export(child);
+	u32 val;
+	int ret;
+
+	ret = kstrtou32(buf, 0, &val);
+	if (ret)
+		return ret;
+
+	export->vc_type = val;
+	export->vc_type_set = true;
+	return ret ? : size;
+
+}
+static DEVICE_ATTR_WO(vc_type);
+
+static ssize_t bandwidth_show(struct device *child,
+			      struct device_attribute *attr,
+			      char *buf)
+{
+	struct bpmp_export *export = child_to_bpmp_export(child);
+	struct cmd_sochub_mbwt_get_bw_resp mbwt;
+	struct tegra_bpmp *bpmp = export->bpmp;
+	int ret;
+
+	if (!export->vc_type_set) {
+		dev_err(bpmp->dev,
+			"SoCHub VC type not set\n");
+		return -EINVAL;
+	}
+
+	ret = tegra_sochub_get_mbwt(bpmp, export->instance,
+				    export->vc_type, &mbwt);
+	/*
+	 * Clear vc_type to make sure the user sets it
+	 * everytime before reading/writing to the bandwidth node
+	 */
+	export->vc_type_set = false;
+
+	return sysfs_emit(buf, "%u\n", mbwt.bw);
+
+}
+
+static ssize_t bandwidth_store(struct device *child,
+			       struct device_attribute *attr,
+			       const char *buf, size_t size)
+{
+	struct bpmp_export *export = child_to_bpmp_export(child);
+	struct tegra_bpmp *bpmp = export->bpmp;
+	u32 val;
+	int ret;
+
+	ret = kstrtou32(buf, 0, &val);
+	if (ret)
+		return ret;
+
+	if (!export->vc_type_set) {
+		dev_err(bpmp->dev,
+			"SoCHub VC type not set\n");
+		return val;
+	}
+
+	ret = tegra_sochub_set_mbwt(bpmp, export->instance,
+				    export->vc_type, val);
+
+	/*
+	 * Clear vc_type to make sure the user sets it
+	 * everytime before reading/writing to the bandwidth node
+	 */
+	export->vc_type_set = false;
+
+	return ret ? : size;
+
+}
+static DEVICE_ATTR_RW(bandwidth);
+
+static struct attribute *bpmp_membw_attrs[] = {
+	&dev_attr_vc_type.attr,
+	&dev_attr_bandwidth.attr,
+	NULL,
+};
+ATTRIBUTE_GROUPS(bpmp_membw);
+
+
+static ssize_t export_store(struct device *parent,
+			    struct device_attribute *attr,
+			    const char *buf, size_t len)
+{
+	int ret;
+	struct tegra_bpmp *bpmp = dev_get_drvdata(parent);
+	struct bpmp_export *export;
+	u32 instance;
+
+	ret = kstrtouint(buf, 0, &instance);
+	if (ret < 0)
+		return ret;
+
+	export = kzalloc(sizeof(*export), GFP_KERNEL);
+	if (!export)
+		return -ENOMEM;
+
+	export->bpmp = bpmp;
+
+	export->child.release = bpmp_export_release;
+	export->child.parent = parent;
+	export->child.devt = MKDEV(0, 0);
+	export->child.groups = bpmp_membw_groups;
+	export->instance = instance;
+	dev_set_name(&export->child, "mbwt%u", instance);
+
+	ret = device_register(&export->child);
+	if (ret) {
+		put_device(&export->child);
+		export = NULL;
+		return ret;
+	}
+
+	return ret ? : len;
+}
+static DEVICE_ATTR_WO(export);
+
+
+static int bpmp_unexport_match(struct device *child, const void *data)
+{
+	struct bpmp_export *export = child_to_bpmp_export(child);
+
+	return export->instance == *(unsigned int *)data;
+}
+
+static ssize_t unexport_store(struct device *parent,
+			    struct device_attribute *attr,
+			    const char *buf, size_t len)
+{
+	struct tegra_bpmp *bpmp = dev_get_drvdata(parent);
+	unsigned int instance;
+	struct device *child;
+	int ret;
+
+	ret = kstrtouint(buf, 0, &instance);
+	if (ret < 0)
+		return ret;
+
+	child = device_find_child(parent, &instance, bpmp_unexport_match);
+	if (!child)
+		return -ENODEV;
+
+	put_device(child);
+	device_unregister(child);
+
+	return ret ? : len;
+}
+static DEVICE_ATTR_WO(unexport);
+
+static struct attribute *bpmp_attrs[] = {
+	&dev_attr_export.attr,
+	&dev_attr_unexport.attr,
+	NULL,
+};
+ATTRIBUTE_GROUPS(bpmp);
+
+static struct class bpmp_class = {
+	.name = "bpmp_mbwt",
+	.dev_groups = bpmp_groups,
+};
+
+static int bpmp_sysfs_export(struct tegra_bpmp *bpmp)
+{
+	struct device *parent;
+
+	if (!ACPI_HANDLE(bpmp->dev))
+		return -EFAULT;
+
+	parent = device_create(&bpmp_class, NULL, MKDEV(0, 0), bpmp,
+			       "mbwt_control");
+	if (IS_ERR(parent)) {
+		dev_warn(bpmp->dev,
+			 "failed creating device for bpmp sysfs export\n");
+		return PTR_ERR(parent);
+	}
+
+	return 0;
+}
+
+static int tegra410_bpmp_init(struct tegra_bpmp *bpmp)
+{
+	return bpmp_sysfs_export(bpmp);
+}
+
+const struct tegra_bpmp_ops tegra410_bpmp_ops = {
+	.init = tegra410_bpmp_init,
+};
+static int __init bpmp_sysfs_init(void)
+{
+	return class_register(&bpmp_class);
+}
+subsys_initcall(bpmp_sysfs_init);

drivers/firmware/tegra/Makefile
@@ -5,6 +5,7 @@ tegra-bpmp-$(CONFIG_ARCH_TEGRA_186_SOC)	+= bpmp-tegra186.o
 tegra-bpmp-$(CONFIG_ARCH_TEGRA_194_SOC)	+= bpmp-tegra186.o
 tegra-bpmp-$(CONFIG_ARCH_TEGRA_234_SOC)	+= bpmp-tegra186.o
 tegra-bpmp-$(CONFIG_ARCH_TEGRA_264_SOC)	+= bpmp-tegra186.o
+tegra-bpmp-$(CONFIG_ARCH_TEGRA_241_SOC)	+= bpmp-tegra410.o
 tegra-bpmp-$(CONFIG_DEBUG_FS)	+= bpmp-debugfs.o
 obj-$(CONFIG_TEGRA_BPMP)	+= tegra-bpmp.o
 obj-$(CONFIG_TEGRA_IVC)		+= ivc.o

drivers/firmware/tegra/bpmp-private.h
@@ -25,5 +25,8 @@ struct tegra_bpmp_ops {
 
 extern const struct tegra_bpmp_ops tegra186_bpmp_ops;
 extern const struct tegra_bpmp_ops tegra210_bpmp_ops;
+#if IS_ENABLED(CONFIG_ARCH_TEGRA_241_SOC)
+extern const struct tegra_bpmp_ops tegra410_bpmp_ops;
+#endif
 
 #endif

drivers/firmware/tegra/bpmp.c
@@ -999,6 +999,13 @@ static const struct tegra_bpmp_soc tegra210_soc = {
 };
 #endif
 
+#if IS_ENABLED(CONFIG_ARCH_TEGRA_241_SOC)
+static const struct tegra_bpmp_soc tegra410_soc = {
+	.ops = &tegra410_bpmp_ops,
+};
+#endif
+
+
 static const struct of_device_id tegra_bpmp_match[] = {
 #if IS_ENABLED(CONFIG_ARCH_TEGRA_186_SOC) || \
     IS_ENABLED(CONFIG_ARCH_TEGRA_194_SOC) || \
@@ -1012,10 +1019,18 @@ static const struct of_device_id tegra_bpmp_match[] = {
 	{ }
 };
 
+static const struct acpi_device_id tegra_bpmp_acpi_match[] = {
+#if IS_ENABLED(CONFIG_ARCH_TEGRA_241_SOC)
+	{.id = "NVDA3001", .driver_data = (kernel_ulong_t)&tegra410_soc},
+#endif
+	{ }
+};
+
 static struct platform_driver tegra_bpmp_driver = {
 	.driver = {
 		.name = "tegra-bpmp",
 		.of_match_table = tegra_bpmp_match,
+		.acpi_match_table = tegra_bpmp_acpi_match,
 		.pm = &tegra_bpmp_pm_ops,
 		.suppress_bind_attrs = true,
 	},

include/soc/tegra/bpmp-abi.h
@@ -341,6 +341,7 @@ struct mrq_response {
 #define MRQ_GEARS		82U
 #define MRQ_BWMGR_INT		83U
 #define MRQ_OC_STATUS		84U
+#define MRQ_SOCHUB_MBWT		96U
 
 /** @cond DEPRECATED */
 #define MRQ_RESERVED_2		2U
@@ -374,7 +375,7 @@ struct mrq_response {
  * @brief Maximum MRQ code to be sent by CPU software to
  * BPMP. Subject to change in future
  */
-#define MAX_CPU_MRQ_ID		84U
+#define MAX_CPU_MRQ_ID		96U
 
 /**
  * @addtogroup MRQ_Payloads
@@ -3863,6 +3864,137 @@ struct mrq_pwr_limit_response {
 /** @} PwrLimit */
 /** @endcond bpmp_th500 */
 
+/** @cond (bpmp_tb500)
+ * @ingroup MRQ_Codes
+ * @def MRQ_SOCHUB_MBWT
+ * @brief Get/set per-virtual-channel bandwidth for SoC Hub using MBWT.
+ *
+ * * Initiators: Any
+ * * Targets: BPMP
+ * * Request Payload: @ref mrq_sochub_mbwt_request
+ * * Response Payload: @ref mrq_sochub_mbwt_response
+ *
+ * @addtogroup SOCHUB_MBWT
+ * @{
+ */
+
+/**
+ * @brief Sub-command identifiers for #MRQ_SOCHUB_MBWT.
+ */
+enum mrq_sochub_mbwt_cmd {
+	/**
+	 * @brief Check whether the BPMP-FW supports the specified
+	 * #MRQ_SOCHUB_MBWT sub-command.
+	 *
+	 * mrq_sochub_mbwt_response:err is 0 if the specified request is
+	 * supported and -#BPMP_ENOTSUP otherwise.
+	 */
+	CMD_SOCHUB_MBWT_QUERY_ABI = 0,
+	/**
+	 * @brief Get per-virtual-channel bandwidth configured for SoC Hub
+	 * using MBWT.
+	 *
+	 * mrq_sochub_mbwt_response:err is defined as:
+	 *
+	 * | Value          | Description                                    |
+	 * |----------------|------------------------------------------------|
+	 * | 0              | Success                                        |
+	 * | -#BPMP_ENOTSUP | #MRQ_SOCHUB_MBWT is not supported by BPMP-FW.  |
+	 * | -#BPMP_EINVAL  | Invalid request parameters.                    |
+	 */
+	CMD_SOCHUB_MBWT_GET_BW = 1,
+	/**
+	 * @brief Set per-virtual-channel bandwidth for SoC Hub using MBWT.
+	 *
+	 * mrq_sochub_mbwt_response:err is defined as:
+	 *
+	 * | Value          | Description                                    |
+	 * |----------------|------------------------------------------------|
+	 * | 0              | Success                                        |
+	 * | -#BPMP_ENOTSUP | #MRQ_SOCHUB_MBWT is not supported by BPMP-FW.  |
+	 * | -#BPMP_EINVAL  | Invalid request parameters.                    |
+	 */
+	CMD_SOCHUB_MBWT_SET_BW = 2,
+};
+
+/**
+ * @brief Request data for #MRQ_SOCHUB_MBWT sub-command
+ *        #CMD_SOCHUB_MBWT_QUERY_ABI
+ */
+struct cmd_sochub_mbwt_query_abi_req {
+	/** @brief Sub-command identifier from @ref mrq_sochub_mbwt_cmd */
+	uint32_t cmd_code;
+} BPMP_ABI_PACKED;
+
+/**
+ * @brief Request data for #MRQ_SOCHUB_MBWT sub-command
+ *        #CMD_SOCHUB_MBWT_GET_BW
+ */
+struct cmd_sochub_mbwt_get_bw_req {
+	/** @brief SoC Hub instance */
+	uint32_t instance;
+	/** @brief Type of the SoC Hub virtual channel */
+	uint32_t vc_type;
+} BPMP_ABI_PACKED;
+
+/**
+ * @brief Request data for #MRQ_SOCHUB_MBWT sub-command
+ *        #CMD_SOCHUB_MBWT_SET_BW
+ */
+struct cmd_sochub_mbwt_set_bw_req {
+	/** @brief SoC Hub instance */
+	uint32_t instance;
+	/** @brief Type of the SoC Hub virtual channel */
+	uint32_t vc_type;
+	/** @brief Bandwidth in GB/s */
+	uint32_t bw;
+} BPMP_ABI_PACKED;
+
+/**
+ * @brief Response data for #MRQ_SOCHUB_MBWT sub-command
+ *        #CMD_SOCHUB_MBWT_GET_BW
+ */
+struct cmd_sochub_mbwt_get_bw_resp {
+	/** @brief Bandwidth in GB/s */
+	uint32_t bw;
+} BPMP_ABI_PACKED;
+
+/**
+ * @brief Request payload for the #MRQ_SOCHUB_MBWT -command
+ *
+ * | Sub-command                   | Request payload                        |
+ * |-------------------------------|----------------------------------------|
+ * | #CMD_SOCHUB_MBWT_QUERY_ABI    | #cmd_sochub_mbwt_query_abi_req         |
+ * | #CMD_SOCHUB_MBWT_GET_BW       | #cmd_sochub_mbwt_get_bw_req            |
+ * | #CMD_SOCHUB_MBWT_SET_BW       | #cmd_sochub_mbwt_set_bw_req            |
+ */
+struct mrq_sochub_mbwt_request {
+	/** @brief Sub-command ID from @ref mrq_sochub_mbwt_cmd. */
+	uint32_t cmd;
+	union {
+		struct cmd_sochub_mbwt_query_abi_req query_abi;
+		struct cmd_sochub_mbwt_get_bw_req get_bw;
+		struct cmd_sochub_mbwt_set_bw_req set_bw;
+	} BPMP_UNION_ANON;
+} BPMP_ABI_PACKED;
+
+/**
+ * @brief Response payload for the #MRQ_SOCHUB_MBWT -command.
+ *
+ * | Sub-command                   | Response payload                       |
+ * |-------------------------------|----------------------------------------|
+ * | #CMD_SOCHUB_MBWT_QUERY_ABI    | -                                      |
+ * | #CMD_SOCHUB_MBWT_GET_BW       | #cmd_sochub_mbwt_get_bw_resp           |
+ * | #CMD_SOCHUB_MBWT_SET_BW       | -                                      |
+ */
+struct mrq_sochub_mbwt_response {
+	union {
+		struct cmd_sochub_mbwt_get_bw_resp get_bw;
+	} BPMP_UNION_ANON;
+} BPMP_ABI_PACKED;
+
+/** @} SOCHUB_MBWT */
+/** @endcond */
 
 /**
  * @ingroup MRQ_Codes

commit 1583392470db32ee66b9983318355c29d024a043

NVIDIA: VR: SAUCE: firmware: tegra: bpmp-tegra410: reject duplicate mbwt export
export_store() blindly calls device_register() without checking if the
requested instance is already exported, causing repeated kobject
"don't try to register things with the same name" errors when userspace
writes the same instance number more than once.

Add a device_find_child() guard before allocation to return -EBUSY
early, reusing the existing bpmp_unexport_match() callback. Also clean
up dead code: drop the pointless 'export = NULL' on the error path and
replace 'ret ? : len' with a direct 'return len' since ret is always 0
at that point.

Fixes: bb89f8b44e66 ("tegra: bpmp: Add BPMP client driver for T410")
Signed-off-by: Koba Ko <kobak@nvidia.com>

drivers/firmware/tegra/bpmp-tegra410.c
@@ -195,6 +195,13 @@ static struct attribute *bpmp_membw_attrs[] = {
 ATTRIBUTE_GROUPS(bpmp_membw);
 
 
+static int bpmp_unexport_match(struct device *child, const void *data)
+{
+	struct bpmp_export *export = child_to_bpmp_export(child);
+
+	return export->instance == *(unsigned int *)data;
+}
+
 static ssize_t export_store(struct device *parent,
 			    struct device_attribute *attr,
 			    const char *buf, size_t len)
@@ -202,11 +209,18 @@ static ssize_t export_store(struct device *parent,
 	int ret;
 	struct tegra_bpmp *bpmp = dev_get_drvdata(parent);
 	struct bpmp_export *export;
+	struct device *existing;
 	u32 instance;
 
 	ret = kstrtouint(buf, 0, &instance);
 	if (ret < 0)
 		return ret;
+
+	existing = device_find_child(parent, &instance, bpmp_unexport_match);
+	if (existing) {
+		put_device(existing);
+		return -EBUSY;
+	}
 
 	export = kzalloc(sizeof(*export), GFP_KERNEL);
 	if (!export)
@@ -224,21 +238,12 @@ static ssize_t export_store(struct device *parent,
 	ret = device_register(&export->child);
 	if (ret) {
 		put_device(&export->child);
-		export = NULL;
 		return ret;
 	}
 
-	return ret ? : len;
+	return len;
 }
 static DEVICE_ATTR_WO(export);
-
-
-static int bpmp_unexport_match(struct device *child, const void *data)
-{
-	struct bpmp_export *export = child_to_bpmp_export(child);
-
-	return export->instance == *(unsigned int *)data;
-}
 
 static ssize_t unexport_store(struct device *parent,
 			    struct device_attribute *attr,