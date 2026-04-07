c06abbb45ad6a3d000764fb4c2e5c86ffc3be776
Reinette Chatre <reinette.chatre@intel.com>
2026-04-07 09:01:59 -0700

    fs/resctrl: Add missing return value descriptions
    Using the stricter "./tools/docs/kernel-doc -Wall -v" to verify proper
    formatting of documentation comments includes warnings related to return
    markup on functions that are omitted during the default verification
    checks. This stricter verification reports a couple of missing return
    descriptions in resctrl:
    
        Warning: .../fs/resctrl/rdtgroup.c:1536 No description found for return value of 'rdtgroup_cbm_to_size'
        Warning: .../fs/resctrl/rdtgroup.c:3131 No description found for return value of 'mon_get_kn_priv'
        Warning: .../fs/resctrl/rdtgroup.c:3523 No description found for return value of 'cbm_ensure_valid'
        Warning: .../fs/resctrl/monitor.c:238 No description found for return value of 'resctrl_find_cleanest_closid'
    
    Add the missing return descriptions.
    
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Link: https://patch.msgid.link/1c50b9f7c73251c007133590986f127e1af57780.1775576382.git.reinette.chatre@intel.com
    (cherry picked from commit 79727019ce3da234d877ec0cb6a3985f001e2b2d)
    Signed-off-by: Lee Trager <ltrager@nvidia.com>
    Acked-by: Jamie Nguyen <jamien@nvidia.com>
    Acked-by: Carol L Soto <csoto@nvidia.com>
    Acked-by: Seth Forshee <sforshee@nvidia.com>
    Acked-by: Fenghua Yu <fenghuay@nvidia.com>

--- fs/resctrl/rdtgroup.c
+++ fs/resctrl/rdtgroup.c
@@ -1521,6 +1521,8 @@ out:
  *
  * @cbm is unsigned long, even if only 32 bits are used to make the
  * bitmap functions work correctly.
+ *
+ * Return: Size (in bytes) of cache portion represented by CBM, 0 on failure.
  */
 unsigned int rdtgroup_cbm_to_size(struct rdt_resource *r,
 				  struct rdt_ctrl_domain *d, unsigned long cbm)
@@ -3117,6 +3119,8 @@ static void rmdir_all_sub(void)
  * @mevt:   The type of event file being created.
  * @do_sum: Whether SNC summing monitors are being created. Only set
  *	    when @rid == RDT_RESOURCE_L3.
+ *
+ * Return: Pointer to mon_data private data of the event, NULL on failure.
  */
 static struct mon_data *mon_get_kn_priv(enum resctrl_res_level rid, int domid,
 					struct mon_evt *mevt,
@@ -3511,6 +3515,8 @@ out_destroy:
  * resource group is initialized. The user can follow this with a
  * modification to the CBM if the default does not satisfy the
  * requirements.
+ *
+ * Return: A CBM that is valid for resource @r.
  */
 static u32 cbm_ensure_valid(u32 _val, struct rdt_resource *r)
 {
