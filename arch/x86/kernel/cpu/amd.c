24e820cd4d9039a7d9487d0cb624fd8a6991eed9
Borislav Petkov (AMD) <bp@alien8.de>
2026-04-07 11:40:03 +0200

    x86/CPU: Fix FPDSS on Zen1
    BugLink: https://bugs.launchpad.net/bugs/2156550
    
    commit e55d98e7756135f32150b9b8f75d580d0d4b2dd3 upstream.
    
    Zen1's hardware divider can leave, under certain circumstances, partial
    results from previous operations.  Those results can be leaked by
    another, attacker thread.
    
    Fix that with a chicken bit.
    
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    CVE-2025-54505
    CVE-2026-31628
    [nwager: adjust context]
    Signed-off-by: Noah Wager <noah.wager@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- arch/x86/kernel/cpu/amd.c
+++ arch/x86/kernel/cpu/amd.c
@@ -1104,6 +1104,9 @@ static void init_amd_zen3(struct cpuinfo_x86 *c)
 		if (!cpu_has(c, X86_FEATURE_BTC_NO))
 			set_cpu_cap(c, X86_FEATURE_BTC_NO);
 	}
+
+	pr_notice_once("AMD Zen1 FPDSS bug detected, enabling mitigation.\n");
+	msr_set_bit(MSR_AMD64_FP_CFG, MSR_AMD64_FP_CFG_ZEN1_DENORM_FIX_BIT);
 }
 
 static void init_amd_zen4(struct cpuinfo_x86 *c)
