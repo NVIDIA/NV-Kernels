402a5b6f03a5bfafa856e345b671344eaab99698
Abdur Rahman <abdur.rahman@canonical.com>
2026-04-07 16:01:07 -0400

    Initialize noble:linux-nvidia-7.0
    Signed-off-by: Abdur Rahman <abdur.rahman@canonical.com>

---
+++ debian.nvidia-7.0/rules.d/arm64.mk
@@ -0,0 +1,26 @@
+build_arch	= arm64
+defconfig	= defconfig
+flavours	= nvidia nvidia-64k
+build_image	= Image.gz
+kernel_file	= arch/$(build_arch)/boot/Image.gz
+install_file	= vmlinuz
+no_dumpfile = true
+uefi_signed     = true
+
+vdso		= vdso_install
+
+do_tools_usbip  = true
+do_tools_cpupower = true
+do_tools_perf   = true
+do_tools_perf_jvmti = true
+do_tools_perf_python = true
+do_tools_bpftool = true
+do_tools_rtla = true
+
+do_dtbs		= true
+
+do_lib_rust             = false
+gcc                     = gcc-13
+rustc                   = rustc-1.80
+rustfmt                 = /usr/lib/rust-1.80/bin/rustfmt
+bindgen                 = bindgen-0.65
