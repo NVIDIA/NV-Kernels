25bbbea476e7ffc29307e9380a0095cff0fec7f8
Johan Hovold <johan@kernel.org>
2026-04-14 15:43:14 +0200

    spi: mpc52xx: fix controller deregistration
    BugLink: https://bugs.launchpad.net/bugs/2156619
    
    commit 0f997fdae819a8c2cc83bd4ff7d935ad76c727c9 upstream.
    
    Make sure to deregister the controller before disabling and releasing
    underlying resources like interrupts and gpios during driver unbind.
    
    Fixes: 42bbb70980f3 ("powerpc/5200: Add mpc5200-spi (non-PSC) device driver")
    Fixes: b8d4e2ce60b6 ("mpc52xx_spi: add gpio chipselect")
    Cc: stable@vger.kernel.org	# 2.6.33
    Cc: Grant Likely <grant.likely@secretlab.ca>
    Cc: Luotao Fu <l.fu@pengutronix.de>
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Link: https://patch.msgid.link/20260414134319.978196-4-johan@kernel.org
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    CVE-2026-46200
    [cremfuelled: adjust context]
    Signed-off-by: Alice C. Munduruca <alice.munduruca@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- drivers/spi/spi-mpc52xx.c
+++ drivers/spi/spi-mpc52xx.c
@@ -526,13 +526,14 @@ static void mpc52xx_spi_remove(struct platform_device *op)
 	free_irq(ms->irq0, ms);
 	free_irq(ms->irq1, ms);
 
+	spi_unregister_controller(host);
+
 	cancel_work_sync(&ms->work);
 
 	for (i = 0; i < ms->gpio_cs_count; i++)
 		gpiod_put(ms->gpio_cs[i]);
 
 	kfree(ms->gpio_cs);
-	spi_unregister_controller(host);
 	iounmap(ms->regs);
 	spi_controller_put(host);
 }
