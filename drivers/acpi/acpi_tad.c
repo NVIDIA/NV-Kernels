1089f6ff3df7c68bc6f68e9e1b0e134d18d1693b
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2026-04-22 17:25:46 +0200

    ACPI: TAD: Use devres for all driver cleanup
    The code in acpi_tad_remove() needs to run after the unregistration of
    the devres-managed RTC class device so that it doesn't race with the
    class callbacks of the latter.
    
    To make that happen, pass it to devm_add_action_or_reset() before
    registering the RTC class device.
    
    Fixes: 7572dcabe38d ("ACPI: TAD: Add alarm support to the RTC class device interface")
    Fixes: 8a1e7f4b1764 ("ACPI: TAD: Add RTC class device interface")
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Link: https://patch.msgid.link/14001754.uLZWGnKmhe@rafael.j.wysocki
    
    (backported from commit 77dd14ab1b575328745644136ba758d394e10fbc)
    [abhsahu: Resolved context conflict in acpi_tad_probe()]
    Signed-off-by: Abhishek Sahu <abhsahu@nvidia.com>

--- drivers/acpi/acpi_tad.c
+++ drivers/acpi/acpi_tad.c
@@ -653,9 +653,9 @@ static int acpi_tad_disable_timer(struct device *dev, u32 timer_id)
 	return acpi_tad_wake_set(dev, "_STV", timer_id, ACPI_TAD_WAKE_DISABLED);
 }
 
-static void acpi_tad_remove(struct platform_device *pdev)
+static void acpi_tad_remove(void *data)
 {
-	struct device *dev = &pdev->dev;
+	struct device *dev = data;
 	struct acpi_tad_driver_data *dd = dev_get_drvdata(dev);
 
 	device_init_wakeup(dev, false);
@@ -735,11 +735,17 @@ static int acpi_tad_probe(struct platform_device *pdev)
 	pm_runtime_enable(dev);
 	pm_runtime_suspend(dev);
 
-	ret = sysfs_create_group(&dev->kobj, &acpi_tad_attr_group);
-	if (ret) {
-		acpi_tad_remove(pdev);
+	/*
+	 * acpi_tad_remove() needs to run after unregistering the RTC class
+	 * device to avoid racing with the latter's callbacks.
+	 */
+	ret = devm_add_action_or_reset(&pdev->dev, acpi_tad_remove, &pdev->dev);
+	if (ret)
 		return ret;
-	}
+
+	ret = sysfs_create_group(&dev->kobj, &acpi_tad_attr_group);
+	if (ret)
+		return ret;
 
 	if (caps & ACPI_TAD_RT)
 		acpi_tad_register_rtc(dev);
@@ -758,7 +764,6 @@ static struct platform_driver acpi_tad_driver = {
 		.acpi_match_table = acpi_tad_ids,
 	},
 	.probe = acpi_tad_probe,
-	.remove = acpi_tad_remove,
 };
 MODULE_DEVICE_TABLE(acpi, acpi_tad_ids);
 
