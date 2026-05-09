8099dc54530f4e5bd8e5665299fbd6a565c08b30
Andrea Righi <arighi@nvidia.com>
2026-05-09 20:07:25 +0200

    sched/fair: Drop redundant RCU read lock in NOHZ kick path
    nohz_balancer_kick() is reached from sched_balance_trigger(), which is
    called from sched_tick(). sched_tick() runs with IRQs disabled, so the
    additional rcu_read_lock/unlock() used around sched_domain accesses in
    this path is redundant. Rely on the existing IRQ-disabled context (and
    the rcu_dereference_all() checking) instead.
    
    The same applies to set_cpu_sd_state_idle(), called from the idle entry
    path with IRQs disabled, and to set_cpu_sd_state_busy(), reachable via
    nohz_balance_exit_idle() from two contexts: nohz_balancer_kick() (IRQs
    disabled, as above) and sched_cpu_deactivate() (the CPUHP_AP_ACTIVE
    teardown, which runs under cpus_write_lock(), so it cannot race with
    sched-domain rebuilds). In both cases the rcu_dereference_all()
    validation is sufficient.
    
    No functional change intended.
    
    Suggested-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Signed-off-by: Andrea Righi <arighi@nvidia.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://patch.msgid.link/20260509180955.1840064-2-arighi@nvidia.com
    (backported from commit c9d93a73ce871ca32caf9308562501290b64b955)
    kelseys: Resolve the scheduler context conflicts by retaining the branch's
    existing sd->shared NULL checks in set_cpu_sd_state_busy() and
    set_cpu_sd_state_idle() while removing the redundant RCU read locks.
    Signed-off-by: Kelsey Steele <kelseys@nvidia.com>
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Carol L Soto <csoto@nvidia.com>
    Signed-off-by: Kelsey Steele <kelseys@nvidia.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -12828,8 +12828,6 @@ static void nohz_balancer_kick(struct rq *rq)
 		goto out;
 	}
 
-	rcu_read_lock();
-
 	sd = rcu_dereference_all(rq->sd);
 	if (sd) {
 		/*
@@ -12837,8 +12835,8 @@ static void nohz_balancer_kick(struct rq *rq)
 		 * capacity, kick the ILB to see if there's a better CPU to run on:
 		 */
 		if (rq->cfs.h_nr_runnable >= 1 && check_cpu_capacity(rq, sd)) {
-			flags = NOHZ_STATS_KICK | NOHZ_BALANCE_KICK;
-			goto unlock;
+			flags |= NOHZ_STATS_KICK | NOHZ_BALANCE_KICK;
+			goto out;
 		}
 	}
 
@@ -12854,8 +12852,8 @@ static void nohz_balancer_kick(struct rq *rq)
 		 */
 		for_each_cpu_and(i, sched_domain_span(sd), nohz.idle_cpus_mask) {
 			if (sched_asym(sd, i, cpu)) {
-				flags = NOHZ_STATS_KICK | NOHZ_BALANCE_KICK;
-				goto unlock;
+				flags |= NOHZ_STATS_KICK | NOHZ_BALANCE_KICK;
+				goto out;
 			}
 		}
 	}
@@ -12866,10 +12864,8 @@ static void nohz_balancer_kick(struct rq *rq)
 		 * When ASYM_CPUCAPACITY; see if there's a higher capacity CPU
 		 * to run the misfit task on.
 		 */
-		if (check_misfit_status(rq)) {
-			flags = NOHZ_STATS_KICK | NOHZ_BALANCE_KICK;
-			goto unlock;
-		}
+		if (check_misfit_status(rq))
+			flags |= NOHZ_STATS_KICK | NOHZ_BALANCE_KICK;
 
 		/*
 		 * For asymmetric systems, we do not want to nicely balance
@@ -12878,7 +12874,7 @@ static void nohz_balancer_kick(struct rq *rq)
 		 *
 		 * Skip the LLC logic because it's not relevant in that case.
 		 */
-		goto unlock;
+		goto out;
 	}
 
 	sds = rcu_dereference_all(per_cpu(sd_llc_shared, cpu));
@@ -12893,13 +12889,9 @@ static void nohz_balancer_kick(struct rq *rq)
 		 * like this LLC domain has tasks we could move.
 		 */
 		nr_busy = atomic_read(&sds->nr_busy_cpus);
-		if (nr_busy > 1) {
-			flags = NOHZ_STATS_KICK | NOHZ_BALANCE_KICK;
-			goto unlock;
-		}
+		if (nr_busy > 1)
+			flags |= NOHZ_STATS_KICK | NOHZ_BALANCE_KICK;
 	}
-unlock:
-	rcu_read_unlock();
 out:
 	if (READ_ONCE(nohz.needs_update))
 		flags |= NOHZ_NEXT_KICK;
@@ -12911,18 +12903,14 @@ out:
 static void set_cpu_sd_state_busy(int cpu)
 {
 	struct sched_domain *sd;
-
-	rcu_read_lock();
 	sd = rcu_dereference_all(per_cpu(sd_llc, cpu));
 
 	if (!sd || !sd->nohz_idle)
-		goto unlock;
+		return;
 	sd->nohz_idle = 0;
 
 	if (sd->shared)
 		atomic_inc(&sd->shared->nr_busy_cpus);
-unlock:
-	rcu_read_unlock();
 }
 
 void nohz_balance_exit_idle(struct rq *rq)
@@ -12942,18 +12930,14 @@ void nohz_balance_exit_idle(struct rq *rq)
 static void set_cpu_sd_state_idle(int cpu)
 {
 	struct sched_domain *sd;
-
-	rcu_read_lock();
 	sd = rcu_dereference_all(per_cpu(sd_llc, cpu));
 
 	if (!sd || sd->nohz_idle)
-		goto unlock;
+		return;
 	sd->nohz_idle = 1;
 
 	if (sd->shared)
 		atomic_dec(&sd->shared->nr_busy_cpus);
-unlock:
-	rcu_read_unlock();
 }
 
 /*
