af9a3b263f1a4c8ea291f337b27184143f3f3c20
Ankit Nautiyal <ankit.k.nautiyal@intel.com>
2026-06-10 14:08:10 +0800

    drm/i915/alpm: Allow LOBF only for platform that have Always on VRR TG
    BugLink: https://bugs.launchpad.net/bugs/2156312
    
    As per bspec the LOBF is allowed when running in fixed refresh rate mode
    i.e. when flipline = vmin = vmax and when window1 is non zero. This
    implies that we can allow LOBF only when VRR timing generator is running
    in the fixed refresh rate mode.
    
    Use the check intel_vrr_always_use_vrr_tg() to avoid LOBF with legacy
    timing generator. Also use intel_vrr_is_fixed_rr() to check for fixed
    refresh rate mode.
    
    v2: Modify commit message to clarify window1 requirement for LOBF.
    (Ville)
    
    Signed-off-by: Ankit Nautiyal <ankit.k.nautiyal@intel.com>
    Reviewed-by: Michał Grzelak <michal.grzelak@intel.com>
    Link: https://patch.msgid.link/20260204050250.762718-4-ankit.k.nautiyal@intel.com
    (backported from commit 8a517b1e6d8bdde298a7006234c8188c6fb43747)
    [AceLan: a missing #include "intel_vrr.h" in intel_alpm.c]
    Signed-off-by: Chia-Lin Kao (AceLan) <acelan.kao@canonical.com>
    Acked-by: Changwei Zou via kernel-team <kernel-team@lists.ubuntu.com>
    Acked-by: Ross Porter via kernel-team <kernel-team@lists.ubuntu.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- drivers/gpu/drm/i915/display/intel_alpm.c
+++ drivers/gpu/drm/i915/display/intel_alpm.c
@@ -15,6 +15,7 @@
 #include "intel_dp_aux.h"
 #include "intel_psr.h"
 #include "intel_psr_regs.h"
+#include "intel_vrr.h"
 
 bool intel_alpm_aux_wake_supported(struct intel_dp *intel_dp)
 {
@@ -307,8 +308,8 @@ void intel_alpm_lobf_compute_config(struct intel_dp *intel_dp,
 	if (crtc_state->has_psr)
 		return;
 
-	if (crtc_state->vrr.vmin != crtc_state->vrr.vmax ||
-	    crtc_state->vrr.vmin != crtc_state->vrr.flipline)
+	if (!intel_vrr_always_use_vrr_tg(display) ||
+	    !intel_vrr_is_fixed_rr(crtc_state))
 		return;
 
 	if (!(intel_alpm_aux_wake_supported(intel_dp) ||
