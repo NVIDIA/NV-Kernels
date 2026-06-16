bf966b47d7ab2fe1a842eb4c44fa211c1884536f
Noah Wager <noah.wager@canonical.com>
2026-06-15 17:53:38 -0700

    UBUNTU: SAUCE: Revert "fs/ntfs3: Sequential field availability check in mi_enum_attr()"
    BugLink: https://bugs.launchpad.net/bugs/2156901
    
    This reverts commit 115d76576b6d5d17867ae2f591838e224ca3a9d6.
    
    Reverted to apply a7accf181a47 ("fs/ntfs3: Add more attributes checks in
    mi_enum_attr()") from v5.15.209. The upstream version of this patch will
    be applied after.
    
    Signed-off-by: Noah Wager <noah.wager@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- fs/ntfs3/record.c
+++ fs/ntfs3/record.c
@@ -228,7 +228,6 @@ struct ATTRIB *mi_enum_attr(struct mft_inode *mi, struct ATTRIB *attr)
 	}
 
 	/* Can we use the first field (attr->type). */
-	/* NOTE: this code also checks attr->size availability. */
 	if (off + 8 > used) {
 		static_assert(ALIGN(sizeof(enum ATTR_TYPE), 8) == 8);
 		return NULL;
@@ -245,6 +244,10 @@ struct ATTRIB *mi_enum_attr(struct mft_inode *mi, struct ATTRIB *attr)
 		return NULL;
 
 	asize = le32_to_cpu(attr->size);
+	if (asize < SIZEOF_RESIDENT) {
+		/* Impossible 'cause we should not return such attribute. */
+		return NULL;
+	}
 
 	/* Check overflow and boundary. */
 	if (off + asize < off || off + asize > used)
@@ -273,10 +276,6 @@ struct ATTRIB *mi_enum_attr(struct mft_inode *mi, struct ATTRIB *attr)
 	}
 
 	/* Check some nonresident fields. */
-	/* Can we use memory including attr->nres.valid_size? */
-	if (asize < SIZEOF_NONRESIDENT)
-		return NULL;
-
 	if (attr->name_len &&
 	    le16_to_cpu(attr->name_off) + sizeof(short) * attr->name_len >
 		    le16_to_cpu(attr->nres.run_off)) {
@@ -284,9 +283,12 @@ struct ATTRIB *mi_enum_attr(struct mft_inode *mi, struct ATTRIB *attr)
 	}
 
 	if (attr->nres.svcn || !is_attr_ext(attr)) {
+		if (asize + 8 < SIZEOF_NONRESIDENT)
+			return NULL;
+
 		if (attr->nres.c_unit)
 			return NULL;
-	} else if (asize < SIZEOF_NONRESIDENT_EX)
+	} else if (asize + 8 < SIZEOF_NONRESIDENT_EX)
 		return NULL;
 
 	return attr;
