9c61712c18cb6518999df23c5126752813e3f028
Hangbin Liu <liuhangbin@gmail.com>
2026-06-24 17:13:23 +0800

    selftests: net: move wait_local_port_listen to lib.sh
    BugLink: https://bugs.launchpad.net/bugs/2142613
    
    The function wait_local_port_listen() is the only function defined in
    net_helper.sh. Since some tests source both lib.sh and net_helper.sh,
    we can simplify the setup by moving wait_local_port_listen() to lib.sh.
    
    With this change, net_helper.sh becomes redundant and can be removed.
    
    Signed-off-by: Hangbin Liu <liuhangbin@gmail.com>
    Reviewed-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>
    Link: https://patch.msgid.link/20250526014600.9128-1-liuhangbin@gmail.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (backported from commit d9d836bfa5e6e255c411733b4b1ce7a1f8346c54)
    [an: resolved conflicts because Noble lacks newer selftests/drivers/net files
     and newer MPTCP selftest include changes. Applied the helper move only
     to Noble files which sourced net_helper.sh and kept Noble MPTCP files
     unchanged.]
    Signed-off-by: ChunAn Wu <an.wu@canonical.com>
    Acked-by: Hui Wang <hui.wang@canonical.com>
    Acked-by: Jian Hui Lee <jianhui.lee@canonical.com>
    [ecanepa: merged tools/testing/selftests/net/lib.sh]
    Signed-off-by: Edoardo Canepa <edoardo.canepa@canonical.com>

--- tools/testing/selftests/net/lib.sh
+++ tools/testing/selftests/net/lib.sh
@@ -206,6 +206,27 @@ tc_rule_handle_stats_get()
 		  .options.actions[0].stats$selector"
 }
 
+wait_local_port_listen()
+{
+	local listener_ns="${1}"
+	local port="${2}"
+	local protocol="${3}"
+	local pattern
+	local i
+
+	pattern=":$(printf "%04X" "${port}") "
+
+	# for tcp protocol additionally check the socket state
+	[ ${protocol} = "tcp" ] && pattern="${pattern}0A"
+	for i in $(seq 10); do
+		if ip netns exec "${listener_ns}" awk '{print $2" "$4}' \
+		   /proc/net/"${protocol}"* | grep -q "${pattern}"; then
+			break
+		fi
+		sleep 0.1
+	done
+}
+
 ret_set_ksft_status()
 {
 	local ksft_status=$1; shift
