173d390e832410dc622b494278744b03ee99cb2e
Jiandi An <jan@nvidia.com>
2026-07-14 00:48:57 -0500

    Revert "NVIDIA: VR: SAUCE: vfio/cxl: Provide opt-out for CXL feature"
    BugLink: https://bugs.launchpad.net/bugs/2163076
    
    This reverts commit 18be666350c5239e2b3fc56407bb4a4616caa522.
    
    Signed-off-by: Jiandi An <jan@nvidia.com>
    Acked-by: Seth Forshee <sforshee@nvidia.com>
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Signed-off-by: Brad Figg <bfigg@nvidia.com>

--- include/linux/vfio_pci_core.h
+++ include/linux/vfio_pci_core.h
@@ -134,9 +134,6 @@ struct vfio_pci_core_device {
 	bool			pm_intx_masked;
 	bool			pm_runtime_engaged;
 	bool			sriov_active;
-#if IS_ENABLED(CONFIG_VFIO_CXL_CORE)
-	bool			disable_cxl:1;
-#endif
 	struct pci_saved_state	*pci_saved_state;
 	struct pci_saved_state	*pm_save;
 	int			ioeventfds_nr;
