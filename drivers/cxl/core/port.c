724ed9e515b51d018e5b4a5ac3ed58664cf22404
Jiandi An <jan@nvidia.com>
2026-07-14 00:49:29 -0500

    Revert "NVIDIA: VR: SAUCE: cxl: Add cxl_reset sysfs interface for PCI devices"
    BugLink: https://bugs.launchpad.net/bugs/2163076
    
    This reverts commit dfe9c1018e0f808ea9ec7883a77ff88191a45596.
    
    Signed-off-by: Jiandi An <jan@nvidia.com>
    Acked-by: Seth Forshee <sforshee@nvidia.com>
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Signed-off-by: Brad Figg <bfigg@nvidia.com>

--- drivers/cxl/core/port.c
+++ drivers/cxl/core/port.c
@@ -2531,8 +2531,6 @@ static __init int cxl_core_init(void)
 	if (rc)
 		goto err_ras;
 
-	cxl_reset_sysfs_init();
-
 	return 0;
 
 err_ras:
@@ -2548,7 +2546,6 @@ err_wq:
 
 static void cxl_core_exit(void)
 {
-	cxl_reset_sysfs_exit();
 	cxl_ras_exit();
 	cxl_region_exit();
 	bus_unregister(&cxl_bus_type);
