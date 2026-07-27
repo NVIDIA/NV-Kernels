aca7189a9954b48e01aa11e19450f25b3fa9e99a
Lee Trager <ltrager@nvidia.com>
2026-07-27 21:00:28 +0000

    NVIDIA: SAUCE: Revert PCI/MSI v20 memory-decoding fix
    This reverts commit 7d8ffe076143ea2c4e0c9793d867b2b8a96ed8da.
    
    Signed-off-by: Lee Trager <ltrager@nvidia.com>

--- drivers/pci/msi/msi.c
+++ drivers/pci/msi/msi.c
@@ -860,7 +860,6 @@ void __pci_restore_msix_state(struct pci_dev *dev)
 {
 	struct msi_desc *entry;
 	bool write_msg;
-	u16 cmd;
 
 	if (!dev->msix_enabled)
 		return;
@@ -869,8 +868,6 @@ void __pci_restore_msix_state(struct pci_dev *dev)
 	pci_intx_for_msi(dev, 0);
 	pci_msix_clear_and_set_ctrl(dev, 0,
 				PCI_MSIX_FLAGS_ENABLE | PCI_MSIX_FLAGS_MASKALL);
-	pci_read_config_word(dev, PCI_COMMAND, &cmd);
-	pci_write_config_word(dev, PCI_COMMAND, cmd | PCI_COMMAND_MEMORY);
 
 	write_msg = arch_restore_msi_irqs(dev);
 
@@ -882,7 +879,6 @@ void __pci_restore_msix_state(struct pci_dev *dev)
 		}
 	}
 
-	pci_write_config_word(dev, PCI_COMMAND, cmd);
 	pci_msix_clear_and_set_ctrl(dev, PCI_MSIX_FLAGS_MASKALL, 0);
 }
 
