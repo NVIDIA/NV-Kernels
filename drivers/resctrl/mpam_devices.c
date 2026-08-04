b0f49a65b20471b5d6fc5b70ac54dab0666ba636
Andre Przywara <andre.przywara@arm.com>
2026-08-04 12:06:26 +0200

    NVIDIA: SAUCE: arm_mpam: change MPAM-Fb error IRQ to use a threaded IRQ handler
    When an MPAM MSC gets into an error condition, it can trigger an error
    IRQ. We cannot really do much about those errors, but we at least query
    and log the error, then disable MPAM functionality.
    
    This error report relies on reading the MSC's error status register
    (ESR) in the current hard-IRQ handler, which is not possible for MPAM-Fb
    based MSC accesses, since they involve mailbox routines that might sleep.
    The same is true for clearing the interrupt at the source, which requires
    an MSC access as well.
    
    When an MSC is using MPAM-Fb, change the error IRQ to use a threaded IRQ
    handler, with an empty hard IRQ routine, and doing all the MSC accesses
    (to access the status and disable the IRQ line) in the threaded part.
    Also forbid per-CPU interrupts (PPIs) for MPAM-Fb, as we cannot use a
    threaded IRQ here.
    
    The change in the actual IRQ handler is minimal, we just check for the
    first MSC access error and bail out early. MMIO based MSCs keep using a
    hard-IRQ handler, since they must be at least non-migrate-able when doing
    MSC accesses, for the CPU affinity check to work.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>
    Tested-by: Srivathsa L Rao <srivathsa.rao@oss.qualcomm.com>
    (backported from https://lore.kernel.org/r/20260804100627.3217149-11-andre.przywara@arm.com)
    [ltrager: Retain downstream PARTID/PMG programming-error handling in
    the threaded IRQ path.]
    Signed-off-by: Lee Trager <ltrager@nvidia.com>
    Acked-by: Nirmoy Das <nirmoyd@nvidia.com>
    Acked-by: Seth Forshee <sforshee@nvidia.com>

--- drivers/resctrl/mpam_devices.c
+++ drivers/resctrl/mpam_devices.c
@@ -2793,24 +2793,42 @@ static int mpam_disable_msc_ecr(void *_msc)
 	return 0;
 }
 
+/*
+ * This will run as the threaded IRQ handler part when using MPAM-Fb, but
+ * as the sole hard-IRQ handler for MMIO based accesses.
+ */
 static irqreturn_t __mpam_irq_handler(int irq, struct mpam_msc *msc)
 {
 	u64 reg;
+	int ret;
 	u16 partid;
 	u8 errcode, pmg, ris;
 
-	if (WARN_ON_ONCE(!msc) ||
+	if (WARN_ON_ONCE(!msc))
+		return IRQ_NONE;
+
+	if (msc->iface == MPAM_IFACE_MMIO &&
 	    WARN_ON_ONCE(!cpumask_test_cpu(smp_processor_id(),
 					   &msc->accessibility)))
 		return IRQ_NONE;
 
-	mpam_msc_read_esr(msc, &reg);
+	ret = mpam_msc_read_esr(msc, &reg);
+	if (ret) {
+		pr_err_ratelimited("unknown error irq from msc:%u\n", msc->id);
+
+		/* Try out best here ... */
+		goto out_disable;
+	}
 
 	errcode = FIELD_GET(MPAMF_ESR_ERRCODE, reg);
 	if (!errcode)
 		return IRQ_NONE;
 
-	/* Clear level triggered irq */
+	/*
+	 * Clear the level triggered IRQ. If that fails, we cannot do anything
+	 * about it, so ignore any errors. We will disable the IRQ either on
+	 * the device side or on the irqchip level next anyway.
+	 */
 	mpam_msc_clear_esr(msc);
 
 	partid = FIELD_GET(MPAMF_ESR_PARTID_MON, reg);
@@ -2826,16 +2844,24 @@ static irqreturn_t __mpam_irq_handler(int irq, struct mpam_msc *msc)
 	    (errcode != MPAM_ERRCODE_REQ_PMG_RANGE))
 		return IRQ_HANDLED;
 
-	/* Disable this interrupt. */
-	mpam_disable_msc_ecr(msc);
+out_disable:
+	/*
+	 * Disable this interrupt on the device side. If that fails, disable
+	 * the IRQ on the irqchip level, as we must prevent further handler
+	 * invocations. We only take the interrupt once anyway, as we
+	 * are going to free the IRQ next, in mpam_disable().
+	 */
+	ret = mpam_disable_msc_ecr(msc);
+	if (ret)
+		disable_irq_nosync(irq);
 
-	/* Are we racing with the thread disabling MPAM? */
+	/* Check whether we are racing with the thread disabling MPAM. */
 	if (!mpam_is_enabled())
 		return IRQ_HANDLED;
 
 	/*
-	 * Schedule the teardown work. Don't use a threaded IRQ as we can't
-	 * unregister the interrupt from the threaded part of the handler.
+	 * Schedule the teardown work. We have to defer it as we can't
+	 * unregister the interrupt from the threaded part of a handler.
 	 */
 	mpam_disable_reason = "hardware error interrupt";
 	schedule_work(&mpam_broken_work);
@@ -2874,6 +2900,11 @@ static int mpam_register_irqs(void)
 		/* The MPAM spec says the interrupt can be SPI, PPI or LPI */
 		/* We anticipate sharing the interrupt with other MSCs */
 		if (irq_is_percpu(irq)) {
+			if (msc->iface != MPAM_IFACE_MMIO) {
+				dev_err(&msc->pdev->dev,
+					"Only MMIO MSCs can use per-CPU interrupts\n");
+				return -EINVAL;
+			}
 			err = request_percpu_irq(irq, &mpam_ppi_handler,
 						 "mpam:msc:error",
 						 msc->error_dev_id);
@@ -2884,10 +2915,17 @@ static int mpam_register_irqs(void)
 			smp_call_function_many(&msc->accessibility,
 					       &_enable_percpu_irq, &irq,
 					       true);
-		} else {
+		} else if (msc->iface == MPAM_IFACE_MMIO) {
 			err = devm_request_irq(&msc->pdev->dev, irq,
 					       &mpam_spi_handler, IRQF_SHARED,
 					       "mpam:msc:error", msc);
+			if (err)
+				return err;
+		} else {
+			err = devm_request_threaded_irq(&msc->pdev->dev, irq,
+							NULL, &mpam_spi_handler,
+							IRQF_SHARED | IRQF_ONESHOT,
+							"mpam:msc:error", msc);
 			if (err)
 				return err;
 		}
