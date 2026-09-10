a79fd080547f2b19e0e53e2b79fbf891d4d91395
Srirangan Madhavan <smadhavan@nvidia.com>
2026-09-10 18:24:53 +0000

    NVIDIA: VR: SAUCE: cxl: Restore CXL Device DVSEC enable state
    BugLink: https://bugs.launchpad.net/bugs/2167292
    
    Cache the CXL Device DVSEC control value alongside the early HDM
    snapshot. Restore the CXL.cache and CXL.mem enable bits after PCI
    reset. Refresh the cached value when CXL core changes memory
    enablement.
    
    Signed-off-by: Srirangan Madhavan <smadhavan@nvidia.com>
    Signed-off-by: Jiandi An <jan@nvidia.com>
    Acked-by: Nirmoy Das <nirmoyd@nvidia.com>
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Carol L Soto <csoto@nvidia.com>
    Signed-off-by: Brad Figg <bfigg@nvidia.com>

--- drivers/cxl/core/pci.c
+++ drivers/cxl/core/pci.c
@@ -205,6 +205,16 @@ int cxl_await_media_ready(struct cxl_dev_state *cxlds)
 }
 EXPORT_SYMBOL_NS_GPL(cxl_await_media_ready, "CXL");
 
+static void cxl_hdm_cache_dvsec_ctrl(struct pci_dev *pdev, u16 ctrl)
+{
+	guard(rwsem_write)(&cxl_rwsem.dpa);
+	if (!pdev->hdm)
+		return;
+
+	pdev->hdm->dvsec_ctrl = ctrl;
+	pdev->hdm->dvsec_ctrl_valid = true;
+}
+
 static int cxl_set_mem_enable(struct cxl_dev_state *cxlds, u16 val)
 {
 	struct pci_dev *pdev = to_pci_dev(cxlds->dev);
@@ -216,8 +226,10 @@ static int cxl_set_mem_enable(struct cxl_dev_state *cxlds, u16 val)
 	if (rc)
 		return pcibios_err_to_errno(rc);
 
-	if ((ctrl & PCI_DVSEC_CXL_MEM_ENABLE) == val)
+	if ((ctrl & PCI_DVSEC_CXL_MEM_ENABLE) == val) {
+		cxl_hdm_cache_dvsec_ctrl(pdev, ctrl);
 		return 1;
+	}
 	ctrl &= ~PCI_DVSEC_CXL_MEM_ENABLE;
 	ctrl |= val;
 
@@ -225,6 +237,7 @@ static int cxl_set_mem_enable(struct cxl_dev_state *cxlds, u16 val)
 	if (rc)
 		return pcibios_err_to_errno(rc);
 
+	cxl_hdm_cache_dvsec_ctrl(pdev, ctrl);
 	return 0;
 }
 
